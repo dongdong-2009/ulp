4cc4feb81ef8ba64966cce7e807a1eb174dd2555
miaofng <miaofng@126.com>
2009-11-29 21:42:13 +0800

    BUG of clk: 12M x 6=72Mhz Signed-off-by: miaofng <miaofng@gmail.com>

--- main.c
+++ main.c
@@ -108,7 +108,7 @@ void RCC_Configuration(void)
     RCC_PCLK1Config(RCC_HCLK_Div2);
 
     /* PLLCLK = 8MHz * 9 = 72 MHz */
-    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
+    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_6);
 
     /* Enable PLL */ 
     RCC_PLLCmd(ENABLE);
