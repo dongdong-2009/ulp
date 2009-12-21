955c58e469e4a4235117a7550aaa9c2d5b79ec89
miaofng <miaofng@gmail.com>
2009-12-21 11:01:11 +0800

    [hurry] console phy device change to usart2
    Signed-off-by: miaofng <miaofng@gmail.com>

--- boards/hurry/src/console.c
+++ boards/hurry/src/console.c
@@ -13,14 +13,14 @@ void console_init(void)
 	
 	/* Enable GPIOA clock */
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
-	/*configure PA9<uart1.tx>, PA10<uart1.rx>*/
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	/*configure PA2<uart2.tx>, PA3<uart2.rx>*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
@@ -28,17 +28,17 @@ void console_init(void)
 	/*init serial port*/
 	USART_StructInit(&uartinfo);
 	uartinfo.USART_BaudRate = 115200;
-	USART_Init(USART1, &uartinfo);
-	USART_Cmd(USART1, ENABLE);
+	USART_Init(USART2, &uartinfo);
+	USART_Cmd(USART2, ENABLE);
 }
 
 int console_putchar(char c)
 {		
-	USART_SendData(USART1, c);
-	while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+	USART_SendData(USART2, c);
+	while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
 	if(c == '\n') {
-		USART_SendData(USART1, '\r');
-		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+		USART_SendData(USART2, '\r');
+		while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
 	}
 	return 0;
 }
@@ -54,12 +54,12 @@ int console_getchar(void)
 	}
 
 	/*read and echo back*/
-	ret = USART_ReceiveData(USART1);
+	ret = USART_ReceiveData(USART2);
 	console_putchar((char)ret);
 	return ret;
 }
 
 int console_IsNotEmpty()
 {
-	return (int) USART_GetFlagStatus(USART1, USART_FLAG_RXNE);
+	return (int) USART_GetFlagStatus(USART2, USART_FLAG_RXNE);
 }
