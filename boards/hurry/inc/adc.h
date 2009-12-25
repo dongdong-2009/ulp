90a204e8ba4ff4d42f4f39844f1419cfef1d04c1
dusk <daoweifan@126.com>
2009-12-26 00:42:55 +0800

    [hurry] change ADC functions to support VSM
    Signed-off-by: dusk <daoweifan@126.com>

--- boards/hurry/inc/adc.h
+++ boards/hurry/inc/adc.h
@@ -6,28 +6,30 @@
 
 #include "stm32f10x.h"
 
+#define NB_CONVERSIONS	16
+
 #undef ADC_MODE_NORMAL
 #define ADC_MODE_INJECT
 #undef ADC_MODE_INJECT_DUAL
 
-#define	U_CHANNEL	ADC_Channel_4
-#define V_CHANNEL	ADC_Channel_5
-#define W_CHANNEL	ADC_Channel_14
-#define TOTAL_CHANNEL	ADC_Channel_15
-#define VDC_CHANNEL	ADC_Channel_11
+#define PHASE_U_ADC_CHANNEL     ADC_Channel_4
+#define PHASE_V_ADC_CHANNEL     ADC_Channel_5
+#define PHASE_W_ADC_CHANNEL     ADC_Channel_14
+#define TOTAL_CHANNEL			ADC_Channel_15
+#define VDC_CHANNEL				ADC_Channel_11
 
-void adc_init(void);
-/*zf32 board: PA1/ADC_IN1, JP1 R7 POT_WHEEL 0~3v3*/
 /*unit: mv*/
 #define ADC_COUNT_TO_VOLTAGE(CNT) (CNT*3300/4096)
-int adc_getvolt1(void); 
 
-int adc_getvolt(uint8_t ch);
+void adc_Init(void);
+void ad_Update(void);
+void adc_GetCalibration(uint16_t* pPhaseAOffset,uint16_t* pPhaseBOffset,uint16_t* pPhaseCOffset);
+void adc_SetChannel(ADC_TypeDef* ADCx,uint8_t ch);
 
-/*temperature sensor para*/
-/*V25 = 1400 mV,avg_slope = 4.478 mV/C*/
+int adc_GetVolt(uint8_t ch);
+
+/*temperature sensor para V25 = 1400 mV,avg_slope = 4.478 mV/C*/
 #define V25 1400
 #define AVG_SLOPE 4.478
-float adc_gettemp(void);
-//void ad_update(void);
+float adc_GetTemp(void);
 #endif /*__LED_H_*/
