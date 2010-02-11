b28afc79edbab33d0d447a83c54511ab1363e98e
miaofng <miaofng@gmail.com>
2010-02-11 15:05:05 +0800

    [hurry] NOR_RES/IND float support
    note:
      float calculation can only reside in main thread!!
    
    Signed-off-by: miaofng <miaofng@gmail.com>

--- boards/hurry/inc/normalize.h
+++ boards/hurry/inc/normalize.h
@@ -18,12 +18,12 @@
 
 /*note: NOR_XX must be the maximum allowable value*/
 #define NOR_RES_VAL	(1<<10) /*unit: Ohm*/
-#define NOR_RES(x) ((int)x << (15 -10)) /*y =  (x/nor_res)*32768*/
-#define _RES(y) ((int)y >> (15 - 10)) /* res= (y/32768)*nor_res */
+#define NOR_RES(x) (int)(x / NOR_RES_VAL * midShort) /*y =  (x/nor_res)*32768*/
+#define _RES(y) ((float)y * NOR_RES_VAL / midShort) /* res= (y/32768)*nor_res */
 
 #define NOR_IND_VAL	(1 << 10)/*unit: mH*/
-#define NOR_IND(x) ((int)x << (15 - 10)) /*y =  (x/nor_ind)*32768*/
-#define _IND(y) ((int)y >> (15 - 10)) /* ind= (y/32768)*nor_ind */
+#define NOR_IND(x) (int)(x / NOR_IND_VAL * midShort) /*y =  (x/nor_ind)*32768*/
+#define _IND(y) ((float)y * NOR_IND_VAL / midShort) /* ind= (y/32768)*nor_ind */
 
 #define NOR_VOL_VAL	(1 << 20) /*unit: mV*/
 #define NOR_VOL(x) ((int)x >> (20 - 15)) /*y =  (x/nor_vol)*32768*/
