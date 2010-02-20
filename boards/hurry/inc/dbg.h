72ce0ea70364d12587d906d333bc4ddc345b7267
miaofng <miaofng@gmail.com>
2010-02-20 15:45:55 +0800

    [hurry] rename pwm.x to dbg.x, provide dbg gpio/pwm resource
    Signed-off-by: miaofng <miaofng@gmail.com>

---
+++ boards/hurry/inc/dbg.h
@@ -0,0 +1,38 @@
+/* pwm.h
+ * 	miaofng@2009 initial version
+ */
+#ifndef __DBG_H_
+#define __DBG_H_
+
+#include "stm32f10x.h"
+
+/*pwm debug*/
+#define pwm1_Set(val) do { TIM3->CCR1 = val; } while(0)
+#define pwm2_Set(val) do { TIM3->CCR2 = val; } while(0)
+#define pwm3_Set(val) do { TIM3->CCR3 = val; } while(0)
+#define pwm4_Set(val) do { TIM3->CCR4 = val; } while(0)
+
+/*gpio debug*/
+/* pin map:
+	GPIO_DEBUG1	PB11
+	GPIO_DEBUG2	PB10
+*/
+#define BIT_DBG1 (1 << 11)
+#define BIT_DBG2 (1 << 10)
+
+#define _SDBG1 BIT_DBG1
+#define _SDBG2 BIT_DBG2
+
+#define _RDBG1 (BIT_DBG1 << 16)
+#define _RDBG2 (BIT_DBG2 << 16)
+
+#define dbg1_Set()	do { GPIOB->BSRR = _SDBG1; } while(0)
+#define dbg1_Reset()	do { GPIOB->BSRR = _RDBG1; } while(0)
+#define dbg1_Inv()	do { GPIOB->BSRR = (GPIOB->ODR & BIT_DBG1)?_RDBG1 : _SDBG1; } while(0)
+#define dbg2_Set()	do { GPIOB->BSRR = _SDBG2; } while(0)
+#define dbg2_Reset()	do { GPIOB->BSRR = _RDBG2; } while(0)
+#define dbg2_Inv()	do { GPIOB->BSRR = (GPIOB->ODR & BIT_DBG2)?_RDBG2 : _SDBG2; } while(0)
+
+void dbg_Init(void);
+
+#endif /*__DBG_H_*/
---
+++ boards/hurry/src/dbg.c
@@ -0,0 +1,69 @@
+/* pwm.c
+ * 	miaofng@2009 initial version
+ */
+
+#include "config.h"
+#include "stm32f10x.h"
+#include "dbg.h"
+#include "vsm.h"
+
+void dbg_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+
+	/*step 0, clk configuration*/
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+	/*step 1, pwm debug init*/
+	/* pin map:
+		PWM_DEBUG1	PA6
+		PWM_DEBUG2	PA7
+		PWM_DEBUG3	PB0
+		PWM_DEBUG4	PB1
+	*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	TIM_TimeBaseStructure.TIM_Period = T;
+	TIM_TimeBaseStructure.TIM_Prescaler = 0;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = (T >> 2);
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+	TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+	TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+
+	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+	TIM_ARRPreloadConfig(TIM3, ENABLE);
+	TIM_Cmd(TIM3, ENABLE);
+
+	/*step 2, gpio debug init*/
+	/* pin map:
+		GPIO_DEBUG1	PB11
+		GPIO_DEBUG2	PB10
+	*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
