2ceec1d005fe6acaf37a2e58efab0794e49eb54b
miaofng <miaofng@gmail.com>
2010-02-26 15:54:39 +0800

    add Makefiles
    Signed-off-by: miaofng <miaofng@gmail.com>

--- src/include/config.h
+++ src/include/config.h
@@ -7,4 +7,16 @@
 
 #include "autoconfig.h"
 
+#ifdef CONFIG_STM32F10X_MD
+#define STM32F10X_MD
+#endif
+
+#ifdef CONFIG_USE_STDPERIPH_DRIVER
+#define USE_STDPERIPH_DRIVER
+#endif
+
+#ifdef CONFIG_HSE_VALUE
+#define HSE_Value CONFIG_HSE_VALUE
+#endif
+
 #endif /*__CONFIG_H_*/
