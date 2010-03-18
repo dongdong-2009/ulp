4eece97912f80c6f5ad20b298838abaff1481bc5
miaofng <miaofng@atom.linktronsys.com>
2010-03-18 14:44:56 +0800

    [vvt] add driver for chip ad9833
    Signed-off-by: miaofng <miaofng@gmail.com>

---
+++ src/drivers/ad9833.c
@@ -0,0 +1,66 @@
+/*
+*	miaofng@2010 initial version
+ *		ad9833, dds, max mclk=25MHz, 28bit phase resolution, 10bit DAC
+ *		interface: spi, 40Mhz, 16bit, CPOL=0(sck low idle), CPHA=1(latch at 2nd edge of sck)
+*/
+
+#include "ad9833.h"
+//#include "normalize.h"
+
+#define PHASE_RESOLUTION 28
+
+#define REG_CTRL(val) ((0x00 << 14) | val)
+#define REG_FREQ0(val) ((0x01 << 14) | val)
+#define REG_FREQ1(val) ((0x02 << 14) | val))
+#define REG_PHASE0(val) ((0x03 << 14) | (0x00 << 13) | val)
+#define REG_PHASE1(val) ((0x03 << 14) | (0x01 << 13) | val)
+
+/*ctrl reg bits*/
+#define B28 (1 << 13) /*1, seq write 14LSB + 14MSB*/
+#define HLB (1 << 12)
+#define FSELECT (1 << 11) /*write freq0/1*/
+#define PSELECT (1 << 10) /*write phase0/1*/
+#define RESET (1 << 8)
+#define SLEEP1 (1 << 7)
+#define SLEEP12 (1 << 6)
+
+void ad9833_Init(ad9833_t *chip)
+{
+	int priv = chip->option;
+	chip->io.write_reg(0, REG_CTRL(RESET));
+	
+	priv |= B28;
+	chip->io.write_reg(0, REG_CTRL(priv));
+	chip->priv = priv;
+}
+
+int ad9833_SetFreq(ad9833_t *chip, unsigned freq)
+{
+	unsigned short msb, lsb;
+	int priv = chip->priv;
+	
+	if((priv & AD9833_OPT_DIV) && (priv & AD9833_OPT_OUT_SQU))
+		freq >>= (32 - PHASE_RESOLUTION - 1);
+	else
+		freq >>= (32 - PHASE_RESOLUTION);
+	
+	lsb = (unsigned short)(freq);
+	freq >>= 16;
+	msb = (unsigned short)(freq);
+	
+	if(priv & FSELECT) { /*current freq1 works*/
+		chip->io.write_reg(0, REG_FREQ0(lsb));
+		chip->io.write_reg(0, REG_FREQ0(msb));
+		priv &= ~FSELECT;
+		chip->io.write_reg(0, REG_CTRL(priv));
+	}
+	else { /*current freq0 works*/
+		chip->io.write_reg(1, REG_FREQ0(lsb));
+		chip->io.write_reg(1, REG_FREQ0(msb));
+		priv |= FSELECT;
+		chip->io.write_reg(0, REG_CTRL(priv));
+	}
+	
+	chip->priv = priv;
+	return 0;
+}
---
+++ src/include/ad9833.h
@@ -0,0 +1,26 @@
+/*
+ * 	miaofng@2010 initial version
+ *		ad9833, dds, max mclk=25MHz, 28bit phase resolution, 10bit DAC
+ *		interface: spi, 40Mhz, 16bit, CPOL=0(sck low idle), CPHA=1(latch at 2nd edge of sck)
+ */
+#ifndef __AD9833_H_
+#define __AD9833_H_
+
+#include "device.h"
+
+#define AD9833_OPT_OUT_SIN ((0 << 1) | (0 << 5)) /*sin waveform out enable*/
+#define AD9833_OPT_OUT_TRI ((1 << 1) | (0 << 5)) /*triangle waveform out enable*/
+#define AD9833_OPT_OUT_SQU ((0 << 1) | (1 << 5)) /*square waveform out enable, f/2*/
+#define AD9833_OPT_DIV (1 << 3) /*Fout = f/2*/
+
+typedef struct {
+	dev_io_t io;
+	int option;
+	int priv;
+} ad9833_t;
+
+void ad9833_Init(ad9833_t *chip);
+/*para 'freq' is the freq tuning word in case of 32bit phase resolution*/
+int ad9833_SetFreq(ad9833_t *chip, unsigned freq);
+
+#endif /*__AD9833_H_*/
