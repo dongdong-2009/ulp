3211ed26a3a8e797cb77b3dba40efd2d7909fa97
miaofng <miaofng@atom.linktronsys.com>
2010-03-18 15:39:02 +0800

    [vvt] add driver for Position and Speed Sensor simulation
    Signed-off-by: miaofng <miaofng@gmail.com>

---
+++ src/drivers/pss.c
@@ -0,0 +1,28 @@
+/*
+*	miaofng@2010 initial version
+ *		vvt position and speed sensor signal generator
+  *		provide vvt signal: ne58x cam1x cam4x_in cam4x_ext vss
+*/
+
+#include "stm32f10x.h"
+#include "pss.h"
+#include "ad9833.h"
+
+void pss_Init(void)
+{
+}
+
+void pss_Update(void)
+{
+}
+
+int pss_SetSpeed(short hz)
+{
+	return 0;
+}
+
+int pss_SetVolt(pss_ch_t ch, short mv)
+{
+	return 0;
+}
+
---
+++ src/include/pss.h
@@ -0,0 +1,24 @@
+/*
+ * 	miaofng@2010 initial version
+ *		vvt position and speed sensor signal generator
+ *		provide vvt signal: ne58x cam1x cam4x_in cam4x_ext vss
+ */
+#ifndef __PSS_H_
+#define __PSS_H_
+
+typedef enum {
+	NE58X = 0,
+	CAM1X,
+	CAM4X_IN,
+	CAM4X_EXT,
+	PSS_CH_NR
+} pss_ch_t;
+
+void pss_Init(void);
+void pss_Update(void);
+int pss_SetSpeed(short hz); /*1hz speed -> 2hz dsso -> 1rpm*/
+int pss_SetVolt(pss_ch_t ch, short mv); /*on/off?*/
+int pss_SetVssSpeed(short hz);
+int pss_SetVssVolt(short mv);
+
+#endif /*__PSS_H_*/
