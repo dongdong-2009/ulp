390aa933704e2d2954c67e2a61f05901b90f5b7a
miaofng <miaofng@atom.linktronsys.com>
2010-03-18 15:47:11 +0800

    [vvt] initial add task of vvt
    Signed-off-by: miaofng <miaofng@gmail.com>

---
+++ src/include/vvt/vvt.h
@@ -0,0 +1,13 @@
+/*
+ * 	miaofng@2010 initial version
+ */
+#ifndef __VVT_H_
+#define __VVT_H_
+
+/*shared with command shell*/
+
+void vvt_Init(void);
+void vvt_Update(void);
+void vvt_isr(void);
+#endif /*__VVT_H_*/
+
---
+++ src/lib/vvt/vvt.c
@@ -0,0 +1,21 @@
+/*
+ * 	miaofng@2009 initial version
+ */
+
+#include "config.h"
+#include "vvt/vvt.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void vvt_Init(void)
+{
+}
+
+void vvt_Update(void)
+{
+}
+
+void vvt_isr(void)
+{
+}
+
