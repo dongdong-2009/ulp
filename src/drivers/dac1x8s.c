833c9cb085e6ad0c13f1a04b5a61df326bf80295
miaofng <miaofng@126.com>
2010-04-11 01:09:25 +0800

    add multi channle dac chip dac1x8s driver
    Signed-off-by: miaofng <miaofng@126.com>

---
+++ src/drivers/dac1x8s.c
@@ -0,0 +1,30 @@
+/*
+*	miaofng@2010 initial version
+ *		dac1x8s085,  8ch,8bit/10bit/12bit, 6uS settling time, rail-to-rail
+ *		interface: spi, 40Mhz, 16bit, CPOL=1(sck high idle), CPHA=0(latch at 1st edge/falling edge of sck), msb first
+*/
+
+#include "config.h"
+#include "dac1x8s.h"
+
+#define CMD_MOD_WRM		0x8000
+#define CMD_MOD_WTM		0x9000
+#define CMD_UPD_SEL		0xa000
+#define CMD_UPD_CHA		0xb000
+#define CMD_UPD_ALL		0xc000
+#define CMD_OUT_HIZ		0xd000
+#define CMD_OUT_100K	0xe000
+#define CMD_OUT_2p5K	0xf000
+
+void dac1x8s_Init(const dac1x8s_t *chip)
+{
+	chip->io.write_reg(0, CMD_MOD_WTM);
+}
+
+void dac1x8s_SetVolt(const dac1x8s_t *chip, int ch, int cnt)
+{
+	ch &= 0x0007;
+	cnt &= 0x0fff;
+	cnt |= ch << 12;
+	chip->io.write_reg(0, cnt);
+}
---
+++ src/include/dac1x8s.h
@@ -0,0 +1,17 @@
+/*
+ * 	miaofng@2010 initial version
+ */
+#ifndef __DAC1X8S_H_
+#define __DAC1X8S_H_
+
+#include "device.h"
+
+typedef struct {
+	dev_io_t io;
+} dac1x8s_t;
+
+void dac1x8s_Init(const dac1x8s_t *chip);
+/*ch range: 0~7, val range: 0~4095*/
+void dac1x8s_SetVolt(const dac1x8s_t *chip, int ch, int cnt);
+
+#endif /*__DAC1X8S_H_*/
