5908d067ba49aa784aa27dfe7e61e3dce0975dfc
daowei.fan <dusk@atom.linktronsys.com>
2010-04-20 11:49:03 +0800

    add flash operation module
    Signed-off-by: dusk <daoweifan@126.com

---
+++ src/drivers/flash.c
@@ -0,0 +1,62 @@
+/* flash.c
+ * 	dusk@2010 initial version
+ */
+
+#include "config.h"
+#include "stm32f10x.h"
+#include "flash.h"
+#include "time.h"
+
+
+/*
+ *adopt FLASH_ProgramHalfWord function,so where the data_size is odd,full with zero
+ *para:OffsetAddress should be even
+ */
+FlashOpStatus flash_Write(uint32_t OffsetAddress,uint8_t * sour_addr, uint8_t data_size)
+{
+	uint32_t page_addr;
+	uint16_t half_word = 0;
+	volatile FLASH_Status flash_status = FLASH_COMPLETE;
+	uint32_t i;	
+
+	/*get page address*/
+	page_addr = USER_FLASH_STARTADDR + (OffsetAddress / FLASH_PAGE_SIZE) * FLASH_PAGE_SIZE;
+		
+	/* Unlock the Flash Program Erase controller */
+	FLASH_Unlock();
+
+	/* Clear All pending flags */
+	FLASH_ClearFlag(FLASH_FLAG_BSY | FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPRTERR);	
+  
+	/* Erase the FLASH pages */
+	if(FLASH_ErasePage( page_addr ) != FLASH_COMPLETE)
+		return FAILED;
+    
+	for(i=0;i<data_size;i=i+2){
+		if((i+2)>data_size)
+			half_word = 0;
+		else
+			half_word |= *(sour_addr+i+1);
+			
+		half_word <<= 8;
+		half_word |= *(sour_addr+i);		
+		
+		if(FLASH_ProgramHalfWord(USER_FLASH_STARTADDR + OffsetAddress + i, half_word) != FLASH_COMPLETE)
+			return FAILED;
+		half_word = 0;
+	}
+			
+	FLASH_Lock();
+	
+	return PASSED;	
+}
+
+FlashOpStatus flash_Read(uint32_t OffsetAddress,uint8_t * dest_addr,uint32_t data_size)
+{
+	uint32_t i;
+	for(i=0;i<data_size;i++){
+		* (dest_addr + i) = *(__IO uint8_t*) (USER_FLASH_STARTADDR + OffsetAddress + i);
+	}
+	
+	return PASSED;	
+}
---
+++ src/include/flash.h
@@ -0,0 +1,42 @@
+/* flash.h
+ * 	dusk@2010 initial version
+ */
+#ifndef __FLASH_H_
+#define __FLASH_H_
+
+#include "stm32f10x.h"
+
+/* Private define */
+/* Define the STM32F10x FLASH Page Size depending on the used STM32 device */
+#ifdef STM32F10X_LD
+  #define FLASH_PAGE_SIZE    ((uint16_t)0x400)
+#elif defined STM32F10X_MD
+  #define FLASH_PAGE_SIZE    ((uint16_t)0x400)
+#elif defined STM32F10X_HD
+  #define FLASH_PAGE_SIZE    ((uint16_t)0x800)
+#elif defined STM32F10X_CL
+  #define FLASH_PAGE_SIZE    ((uint16_t)0x800)  
+#endif /* STM32F10X_LD */
+
+/*page flash*/
+#define USER_FLASH_PAGESIZE	FLASH_PAGE_SIZE
+
+/*STM32 flash start address*/
+#define STM32_FLASH_STARTADDR 0x08000000
+
+/*user's flash size*/
+#define USER_FLASH_SIZE (CONFIG_FLASH_USER_SIZE*1024)
+
+
+/*user's flash start address*/
+#define USER_FLASH_STARTADDR STM32_FLASH_STARTADDR + (CONFIG_FLASH_WHOLE_SIZE - CONFIG_FLASH_USER_SIZE)*1024
+
+typedef enum {
+	FAILED = 0,
+	PASSED = !FAILED
+} FlashOpStatus;
+
+FlashOpStatus flash_Write(uint32_t OffsetAddress,uint8_t * sour_addr, uint8_t data_size);
+FlashOpStatus flash_Read(uint32_t OffsetAddress,uint8_t * dest_addr,uint32_t data_size);
+
+#endif /*__FLASH_H_*/
