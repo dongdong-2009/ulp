10b9ffca29f61e3d69d4566f07f46cf4ddb12ea6
miaofng <feng.ni@linktronsys.com>
2010-04-26 17:05:26 +0800

    add key.h for common key handling
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

---
+++ src/include/key.h
@@ -0,0 +1,17 @@
+/*
+ * 	miaofng@2010 initial version
+ */
+#ifndef __KEY_H_
+#define __KEY_H_
+
+//virtual key code table
+enum {
+	KEY_RESET,
+	KEY_LEFT,
+	KEY_RIGHT,
+	KEY_ENTER,
+	KEY_UP,
+	KEY_DOWN,
+};
+
+#endif /*__KEY_H_*/
