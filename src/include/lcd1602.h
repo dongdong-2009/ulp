9bc45e932b1b64f4c4fa95af59d1284160b6e133
dusk <daowei.fan@linktronsys.com>
2010-04-26 16:20:07 +0800

    add lcd1602 driver
    Signed-off-by: dusk <daowei.fan@linktronsys.com>

--- src/include/lcd1602.h
+++ src/include/lcd1602.h
@@ -4,17 +4,22 @@
 #ifndef __LCD1602_H_
 #define __LCD1602_H_
 
-#include <stddef.h>
+#include "stm32f10x.h"
 
-typedef struct{
-	uint8_t addr;
-	uint8_t busy;
-}lcd1602_status;
+#define LCD1602_RW	GPIO_Pin_8
+#define LCD1602_RS	GPIO_Pin_9
+#define LCD1602_E	GPIO_Pin_11
+
+#define LCD1602_PORT (GPIO_Pin_All&0x00ff)
+
+#define LCD1602_COMMAND_OFFSCREEN 0X08
+#define LCD1602_COMMAND_CLRSCREEN 0X01
+#define LCD1602_COMMAND_SETMODE   0X38
+#define LCD1602_COMMAND_SETPTMOVE 0X06
+#define LCD1602_COMMAND_SETCUSOR  0X0C
 
 int lcd1602_Init(void);
 int lcd1602_WriteChar(uint8_t row,uint8_t column,int8_t ch);
-int lcd1602_WriteString(uint8_t row,uint8_t offset,char *s);
-int lcd1602_ReadChar(uint8_t row,uint8_t column,int8_t &ch);
-int lcd1602_ReadString(uint8_t row,uint8_t offset,char *s);
+int lcd1602_WriteString(uint8_t row,uint8_t column,char *s);
 
 #endif /*__LCD1602_H_*/
