8c1b277d6be22bb3a5a1092a87126e842b28a246
dusk <daowei.fan@linktronsys.com>
2010-04-26 17:37:19 +0800

    [drivers] add l6208 header file
    Signed-off-by: dusk <daowei.fan@linktronsys.com>

---
+++ src/include/l6208.h
@@ -0,0 +1,40 @@
+/* L6208.h
+ * 	dusk@2010 initial version
+ */
+#ifndef __L6208_H_
+#define __L6208_H_
+
+#include "stm32f10x.h"
+
+#define L6208_VREF	GPIO_Pin_0
+#define L6208_RST	GPIO_Pin_11
+#define L6208_VENA	GPIO_Pin_12
+#define L6208_CTRL	GPIO_Pin_13
+#define L6208_HALF	GPIO_Pin_14
+#define L6208_DIR	GPIO_Pin_15
+
+//this clock from dds
+#define L6208_CLK	GPIO_Pin_8
+
+typedef enum{
+	Clockwise = 0,
+	CounterClockwise
+}l6208_dir;
+
+typedef enum{
+	HalfMode = 0,
+	FullMode
+}l6208_stepmode;
+
+typedef enum{
+	HalfMode = 0,
+	FullMode
+}l6208_controlmode;
+
+void l6208_Init(void);
+void l6208_SetRotationDirection(l6208_dir dir);
+void l6208_SelectMode(l6208_stepmode stepmode);
+void l6208_ResetDisable(void);
+void l6208_SetControlMode(l6208_controlmode controlmode);
+void l6208_Start(FunctionalState NewState);
+#endif /*__L6208_H_*/
