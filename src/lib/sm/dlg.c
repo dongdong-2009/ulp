4584f10d10417ab4bc69e018c17fed77c73f5716
miaofng <feng.ni@linktronsys.com>
2010-04-29 16:10:40 +0800

    [stepmotor] osd menu update
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/lib/sm/dlg.c
+++ src/lib/sm/dlg.c
@@ -26,61 +26,60 @@ const char str_cfg[] = "config";
 const char str_steps[] = "steps";
 
 const osd_item_t items_status[] = {
-	{5, 1, 6, 1, (int)str_status, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
-	{0, 1, 3, 1, (int)str_rpm, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
+	{5, 0, 6, 1, (int)str_status, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
 	{0, 1, 8, 1, (int)dlg_GetSteps, ITEM_DRAW_INT, ITEM_ALIGN_RIGHT, ITEM_UPDATE_AFTERCOMMAND, ITEM_RUNTIME_V},
 	{12, 1, 4, 1, (int)dlg_GetMode, ITEM_DRAW_TXT, ITEM_ALIGN_RIGHT, ITEM_UPDATE_AFTERCOMMAND, ITEM_RUNTIME_V},
 	NULL,
 };
 
 const osd_item_t items_rpm[] = {
-	{5, 1, 6, 1, (int)str_cfg, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
-	{0, 1, 3, 1, (int)str_rpm, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
-	{12, 1, 4, 1, (int)dlg_GetRPM, ITEM_DRAW_INT, ITEM_ALIGN_RIGHT, ITEM_UPDATE_AFTERCOMMAND, ITEM_RUNTIME_V},
+	{5, 2, 6, 1, (int)str_cfg, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
+	{0, 3, 3, 1, (int)str_rpm, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
+	{12, 3, 4, 1, (int)dlg_GetRPM, ITEM_DRAW_INT, ITEM_ALIGN_RIGHT, ITEM_UPDATE_AFTERCOMMAND, ITEM_RUNTIME_V},
 	NULL,
 };
 
 const osd_item_t items_steps[] = {
-	{5, 1, 6, 1, (int)str_cfg, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
-	{0, 1, 5, 1, (int)str_steps, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
-	{8, 1, 8, 1, (int)dlg_GetAutoSteps, ITEM_DRAW_INT, ITEM_ALIGN_RIGHT, ITEM_UPDATE_AFTERCOMMAND, ITEM_RUNTIME_V},
+	{5, 4, 6, 1, (int)str_cfg, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
+	{0, 5, 5, 1, (int)str_steps, ITEM_DRAW_TXT, ITEM_ALIGN_LEFT, ITEM_UPDATE_NEVER, ITEM_RUNTIME_NONE},
+	{8, 5, 8, 1, (int)dlg_GetAutoSteps, ITEM_DRAW_INT, ITEM_ALIGN_RIGHT, ITEM_UPDATE_AFTERCOMMAND, ITEM_RUNTIME_V},
 	NULL,
 };
 
 const osd_command_t cmds_status[] = {
-	{.cmd = KEY_UP, .func = dlg_SelectGroup},
-	{.cmd = KEY_DOWN, .func = dlg_SelectGroup},
-	{.cmd = KEY_RIGHT, .func = dlg_Run},
-	{.cmd = KEY_LEFT, .func = dlg_Run},
-	{.cmd = KEY_RESET, .func = dlg_ResetStep},
-	{.cmd = KEY_ENTER, .func = dlg_ChangeRunMode},
+	{.event = KEY_UP, .func = dlg_SelectGroup},
+	{.event = KEY_DOWN, .func = dlg_SelectGroup},
+	{.event = KEY_RIGHT, .func = dlg_Run},
+	{.event = KEY_LEFT, .func = dlg_Run},
+	{.event = KEY_RESET, .func = dlg_ResetStep},
+	{.event = KEY_ENTER, .func = dlg_ChangeRunMode},
 	NULL,
 };
 
 const osd_command_t cmds_rpm[] = {
-	{.cmd = KEY_UP, .func = dlg_SelectGroup},
-	{.cmd = KEY_DOWN, .func = dlg_SelectGroup},
-	{.cmd = KEY_RIGHT, .func = dlg_ChangeRPM},
-	{.cmd = KEY_LEFT, .func = dlg_ChangeRPM},
-	{.cmd = KEY_RESET, .func = dlg_ChangeRPM},
-	{.cmd = KEY_ENTER, .func = dlg_ChangeRPM},
+	{.event = KEY_UP, .func = dlg_SelectGroup},
+	{.event = KEY_DOWN, .func = dlg_SelectGroup},
+	{.event = KEY_RIGHT, .func = dlg_ChangeRPM},
+	{.event = KEY_LEFT, .func = dlg_ChangeRPM},
+	{.event = KEY_RESET, .func = dlg_ChangeRPM},
+	{.event = KEY_ENTER, .func = dlg_ChangeRPM},
 	NULL,
 };
 
 const osd_command_t cmds_steps[] = {
-	{.cmd = KEY_UP, .func = dlg_SelectGroup},
-	{.cmd = KEY_DOWN, .func = dlg_SelectGroup},
-	{.cmd = KEY_RIGHT, .func = dlg_ChangeAutoSteps},
-	{.cmd = KEY_LEFT, .func = dlg_ChangeAutoSteps},
-	{.cmd = KEY_RESET, .func = dlg_ChangeAutoSteps},
-	{.cmd = KEY_ENTER, .func = dlg_ChangeAutoSteps},
+	{.event = KEY_UP, .func = dlg_SelectGroup},
+	{.event = KEY_DOWN, .func = dlg_SelectGroup},
+	{.event = KEY_RIGHT, .func = dlg_ChangeAutoSteps},
+	{.event = KEY_LEFT, .func = dlg_ChangeAutoSteps},
+	{.event = KEY_RESET, .func = dlg_ChangeAutoSteps},
+	{.event = KEY_ENTER, .func = dlg_ChangeAutoSteps},
 	NULL,
 };
 
 const osd_group_t sm_grps[] = {
-	{.items = items_status, .cmds = cmds_status, .status = 0, .option = 0},
-	{.items = items_rpm, .cmds = cmds_rpm, .status = 1, .option = 0},
-	{.items = items_steps, .cmds = cmds_steps, .status = 2, .option = 0},
+	{.items = items_status, .cmds = cmds_status, .order = 0, .option = GROUP_DRAW_FULLSCREEN},
+	{.items = items_rpm, .cmds = cmds_rpm, .order = 1, .option = GROUP_DRAW_FULLSCREEN},
+	{.items = items_steps, .cmds = cmds_steps, .order = 2, .option = GROUP_DRAW_FULLSCREEN},
 	NULL,
 };
 
@@ -88,7 +87,7 @@ const osd_dialog_t sm_dlg = {
 	.grps = sm_grps,
 	.cmds = NULL,
 	.func = NULL,
-	.max_grps = 1,
+	.option = 0,
 };
 
 static int dlg_GetSteps(void)
