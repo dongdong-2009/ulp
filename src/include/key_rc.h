0825a2b380734b0d60237ee32b74deb3e7c6146c
miaofng <feng.ni@linktronsys.com>
2010-05-04 15:26:08 +0800

    add general remote control support -rc5 protocol is supported
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

---
+++ src/include/key_rc.h
@@ -0,0 +1,11 @@
+/*
+ * 	miaofng@2010 initial version
+ */
+#ifndef __KEY_RC_H_
+#define __KEY_RC_H_
+
+int rckey_init(void);
+key_t rckey_getkey(void);
+void rckey_isr(void);
+
+#endif /*__KEY_RC_H_*/
