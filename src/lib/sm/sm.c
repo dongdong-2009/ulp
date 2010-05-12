664014546d69148a07ca4d9ff874b27f5284604d
miaofng <feng.ni@linktronsys.com>
2010-05-12 11:14:15 +0800

    [stepmotor] improve sm main state machine
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/lib/sm/sm.c
+++ src/lib/sm/sm.c
@@ -14,17 +14,16 @@
 
 //private data read from or store to flash
 typedef struct{
-	int sm_rpm;//unit: rpm
-	int sm_autosteps;
+	short rpm;//unit: rpm
+	short autosteps;
+	int dir : 1;
+	int runmode : 1;
 }sm_config_t;
 
 //private varibles
 static int sm_status;
-static int sm_runmode;
 static sm_config_t sm_config;
-static bool sm_startevent;
-static bool sm_stopevent;
-static time_t manual_overflow;
+static time_t sm_stoptimer;
 
 static const int keymap[] = {
 	KEY_UP,
@@ -35,42 +34,6 @@ static const int keymap[] = {
 	KEY_LEFT,	
 	KEY_NONE
 };
-
-static bool sm_Process(void);
-
-static bool sm_Process(void)
-{
-	bool result;
-	
-	switch(sm_status){
-	case SM_IDLE:
-		if(sm_startevent){
-			sm_status = SM_RUNNING;
-			if(sm_runmode == SM_RUNMODE_MANUAL)
-				capture_SetAutoReload(65535);
-			else
-				capture_SetAutoReload(sm_config.sm_autosteps);
-			sm_startevent = FALSE;
-			result = TRUE;
-		}
-		else
-			result = FALSE;
-		break;
-	case SM_RUNNING:
-		if(sm_stopevent){
-			sm_status = SM_IDLE;
-			sm_stopevent = FALSE;
-			result = TRUE;
-		}
-		else
-			result = FALSE;
-		break;
-	default:
-		break;
-	}
-
-	return result;
-}
 
 void sm_Init(void)
 {
@@ -88,56 +51,66 @@ void sm_Init(void)
 	capture_Init();
 		
 	//read config from flash and config device
-	sm_GetRPMFromFlash(&sm_config.sm_rpm);
-	sm_GetAutostepFromFlash(&sm_config.sm_autosteps);
+	sm_GetRPMFromFlash(&sm_config.rpm);
+	sm_GetAutostepFromFlash(&sm_config.autosteps);
 
-	capture_SetAutoRelaod(sm_config.sm_autosteps);
-	sm_SetRPM(sm_config.sm_rpm);
+	capture_SetAutoRelaod(sm_config.autosteps);
+	sm_SetRPM(sm_config.rpm);
 	
 	//init for variables
 	sm_status = SM_IDLE;
-	sm_runmode = SM_RUNMODE_MANUAL;
-	sm_startevent = FALSE;
-	sm_stopevent = FALSE;
+	sm_config.runmode = SM_RUNMODE_MANUAL;
 }
 
 void sm_Update(void)
 {
-	if(sm_runmode == SM_RUNMODE_MANUAL){
-		if(time_left(manual_overflow) < 0)
+	int left, reload, mode, dir;
+
+	left = time_left(sm_stoptimer);
+	mode = sm_config.runmode;
+	reload = (mode == SM_RUNMODE_MANUAL) ? 65535 : sm_config.autosteps;
+	dir = sm_config.dir;
+	
+	switch (sm_status) {
+	case SM_IDLE:
+		if(left > 0) {
+			capture_SetAutoReload(reload);
+			smctrl_SetRotationDirection(dir);
+			smctrl_Start();
+			sm_status = SM_RUNNING;
+		}
+		break;
+
+	case SM_RUNNING:
+		if((mode == SM_RUNMODE_MANUAL) && (left <= 0)) {
 			sm_StopMotor();
+		}
+		break;
+
+	default:
+		sm_status = SM_IDLE;
 	}
 }
 
 int sm_StartMotor(bool clockwise)
 {
-	// there should be a state machine operation here ....
-	sm_startevent = TRUE;
-	if(sm_Process()){
-		smctrl_SetRotationDirection(clockwise);
-		smctrl_Start();
-	}
-
-	if(sm_status == SM_RUNMODE_MANUAL)
-		manual_overflow = time_get(20); //20ms delay
-		
+	sm_config.dir = clockwise;
+	sm_stoptimer = time_get(20); //20ms delay
 	return 0;
 }
 
 void sm_StopMotor(void)
 {
-	// there should be a state machine operation here ....
-	sm_stopevent = TRUE;
-	if(sm_Process()){
-		smctrl_Stop();
-	}
+	smctrl_Stop();
+	sm_status = SM_IDLE;
+	sm_stoptimer = 0;
 }
 
 int sm_SetRPM(int rpm)
 {
 	/*rpm min/max limit*/
 	rpm = (rpm < 1) ? 1 : rpm;	
-	sm_config.sm_rpm = rpm;	
+	sm_config.rpm = rpm;	
 	smctrl_SetRPM(rpm);
 
 	return 0;
@@ -145,7 +118,7 @@ int sm_SetRPM(int rpm)
 
 int sm_GetRPM(void)
 {
-	return sm_config.sm_rpm;
+	return sm_config.rpm;
 }
 
 int sm_SetAutoSteps(int steps)
@@ -155,7 +128,7 @@ int sm_SetAutoSteps(int steps)
 	steps = (steps < 1) ? 1 : steps;
 	
 	if(sm_status == SM_IDLE) {
-		sm_config.sm_autosteps = steps;
+		sm_config.autosteps = steps;
 		result = 0;
 	}
 	
@@ -164,7 +137,7 @@ int sm_SetAutoSteps(int steps)
 
 int sm_GetAutoSteps(void)
 {
-	return sm_config.sm_autosteps;
+	return sm_config.autosteps;
 }
 
 unsigned short sm_GetSteps(void)
@@ -180,18 +153,18 @@ void sm_ResetStep(void)
 
 int sm_GetRunMode(void)
 {
-	return sm_runmode;
+	return sm_config.runmode;
 }
 
 int sm_SetRunMode(int newmode)
 {
 	if(sm_status == SM_IDLE) {
 		if((newmode >= SM_RUNMODE_MANUAL) && (newmode < SM_RUNMODE_INVALID))
-			sm_runmode = newmode;
+			sm_config.runmode = newmode;
 	}
 	
 	/*success return 0*/
-	return (newmode != sm_runmode);
+	return (newmode != sm_config.runmode);
 }
 
 int sm_GetRPMFromFlash(int *rpm)
@@ -218,9 +191,6 @@ int sm_SaveConfigToFlash(void)
 
 void sm_isr(void)
 {
-	sm_stopevent = TRUE;
-	if(sm_Process()){
-		smctrl_Stop();
-	}
+	sm_StopMotor();
 }
 
