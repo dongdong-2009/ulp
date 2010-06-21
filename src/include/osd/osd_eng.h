2c3569cd17c45fe9a4f7854b8b85948cbc71ae83
miaofng <feng.ni@linktronsys.com>
2010-06-21 14:49:01 +0800

    [osd] redirect osd_eng_xxx to general lcd driver
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

---
+++ src/include/osd/osd_eng.h
@@ -0,0 +1,16 @@
+/*
+ * 	miaofng@2010 initial version
+ */
+#ifndef __OSD_ENG_H_
+#define __OSD_ENG_H_
+
+#include "lcd.h"
+
+#define osd_engine_t		lcd_t
+#define osd_eng_init		lcd_init
+#define osd_eng_puts		lcd_puts
+#define osd_eng_clear_all	lcd_clear_all
+#define osd_eng_clear_rect	lcd_clear_rect
+#define osd_eng_scroll		lcd_scroll
+
+#endif /*__OSD_ENG_H_*/
