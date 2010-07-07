b05221568fed4b51d20934ada08681f31cb3dfc6
dusk <daowei.fan@linktronsys.com>
2010-07-07 15:41:45 +0800

    [driver] add stm32 usart1 support.config it in the xconfig
    Signed-off-by: dusk <daowei.fan@linktronsys.com>

--- src/drivers/console/console_stm32.c
+++ src/drivers/console/console_stm32.c
@@ -10,8 +10,9 @@ void console_Init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	USART_InitTypeDef uartinfo;
-	
+
 	/* Enable GPIOA clock */
+#ifndef CONFIG_DRIVER_STM32_USART1
 #ifdef STM32F10X_CL
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO, ENABLE);
 	GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
@@ -19,7 +20,12 @@ void console_Init(void)
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 #endif
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+#else
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+#endif
 
+#ifndef CONFIG_DRIVER_STM32_USART1
 #ifdef STM32F10X_CL
 	/*configure PD5<uart2.tx>, PD6<uart2.rx>*/
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
@@ -41,22 +47,47 @@ void console_Init(void)
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 #endif
+#else
+	/*configure PA9<uart1.tx>, PA10<uart1.rx>*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+#endif
 
 	/*init serial port*/
 	USART_StructInit(&uartinfo);
 	uartinfo.USART_BaudRate = 115200;
+#ifndef CONFIG_DRIVER_STM32_USART1
 	USART_Init(USART2, &uartinfo);
 	USART_Cmd(USART2, ENABLE);
+#else
+	USART_Init(USART1, &uartinfo);
+	USART_Cmd(USART1, ENABLE);
+#endif
 }
 
 int console_putchar(char c)
-{		
+{
+#ifndef CONFIG_DRIVER_STM32_USART1
 	USART_SendData(USART2, c);
 	while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
 	if(c == '\n') {
 		USART_SendData(USART2, '\r');
 		while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
 	}
+#else
+	USART_SendData(USART1, c);
+	while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+	if(c == '\n') {
+		USART_SendData(USART1, '\r');
+		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+	}
+#endif
 	return 0;
 }
 
@@ -71,7 +102,11 @@ int console_getch(void)
 	}
 
 	/*read and echo back*/
+#ifndef CONFIG_DRIVER_STM32_USART1
 	ret = USART_ReceiveData(USART2);
+#else
+	ret = USART_ReceiveData(USART1);
+#endif
 	return ret;
 }
 
@@ -84,5 +119,9 @@ int console_getchar(void)
 
 int console_IsNotEmpty()
 {
+#ifndef CONFIG_DRIVER_STM32_USART1
 	return (int) USART_GetFlagStatus(USART2, USART_FLAG_RXNE);
+#else
+	return (int) USART_GetFlagStatus(USART1, USART_FLAG_RXNE);
+#endif
 }
