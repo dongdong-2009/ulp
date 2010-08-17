ceaf98fced2a6d12bc7497e476540e3970290161
miaofng <feng.ni@linktronsys.com>
2010-08-17 12:23:30 +0800

    [console] simplify stm32 console driver configuration
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/drivers/console/console_stm32.c
+++ src/drivers/console/console_stm32.c
@@ -6,28 +6,27 @@
 #include "stm32f10x.h"
 #include "console.h"
 
+#if CONFIG_CONSOLE_UART1 == 1
+	#define uart USART1
+#elif CONFIG_CONSOLE_UART2 == 1
+	#define uart USART2
+#else 
+	//default to uart2
+	#define CONFIG_CONSOLE_UART2 1
+	#define uart USART2
+#endif
+
 void console_Init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	USART_InitTypeDef uartinfo;
 
-	/* Enable GPIOA clock */
-#ifndef CONFIG_DRIVER_STM32_USART1
-#ifdef STM32F10X_CL
+#ifdef CONFIG_CONSOLE_UART2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+#ifdef CONFIG_STM32F10X_CL
+	/*configure PD5<uart2.tx>, PD6<uart2.rx>*/
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO, ENABLE);
 	GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
-#else
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-#endif
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-#else
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-#endif
-
-#ifndef CONFIG_DRIVER_STM32_USART1
-#ifdef STM32F10X_CL
-	/*configure PD5<uart2.tx>, PD6<uart2.rx>*/
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
@@ -38,6 +37,7 @@ void console_Init(void)
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 #else
 	/*configure PA2<uart2.tx>, PA3<uart2.rx>*/
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
@@ -47,8 +47,12 @@ void console_Init(void)
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 #endif
-#else
+#endif /*CONFIG_CONSOLE_UART2*/
+
+#if CONFIG_CONSOLE_UART1
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 	/*configure PA9<uart1.tx>, PA10<uart1.rx>*/
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
@@ -57,37 +61,23 @@ void console_Init(void)
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
-#endif
+#endif /*CONFIG_CONSOLE_UART1*/
 
 	/*init serial port*/
 	USART_StructInit(&uartinfo);
 	uartinfo.USART_BaudRate = 115200;
-#ifndef CONFIG_DRIVER_STM32_USART1
-	USART_Init(USART2, &uartinfo);
-	USART_Cmd(USART2, ENABLE);
-#else
-	USART_Init(USART1, &uartinfo);
-	USART_Cmd(USART1, ENABLE);
-#endif
+	USART_Init(uart, &uartinfo);
+	USART_Cmd(uart, ENABLE);
 }
 
 int console_putchar(char c)
 {
-#ifndef CONFIG_DRIVER_STM32_USART1
-	USART_SendData(USART2, c);
-	while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+	USART_SendData(uart, c);
+	while(USART_GetFlagStatus(uart, USART_FLAG_TXE) == RESET);
 	if(c == '\n') {
-		USART_SendData(USART2, '\r');
-		while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+		USART_SendData(uart, '\r');
+		while(USART_GetFlagStatus(uart, USART_FLAG_TXE) == RESET);
 	}
-#else
-	USART_SendData(USART1, c);
-	while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-	if(c == '\n') {
-		USART_SendData(USART1, '\r');
-		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-	}
-#endif
 	return 0;
 }
 
@@ -102,11 +92,7 @@ int console_getch(void)
 	}
 
 	/*read and echo back*/
-#ifndef CONFIG_DRIVER_STM32_USART1
-	ret = USART_ReceiveData(USART2);
-#else
-	ret = USART_ReceiveData(USART1);
-#endif
+	ret = USART_ReceiveData(uart);
 	return ret;
 }
 
@@ -119,9 +105,5 @@ int console_getchar(void)
 
 int console_IsNotEmpty()
 {
-#ifndef CONFIG_DRIVER_STM32_USART1
-	return (int) USART_GetFlagStatus(USART2, USART_FLAG_RXNE);
-#else
-	return (int) USART_GetFlagStatus(USART1, USART_FLAG_RXNE);
-#endif
+	return (int) USART_GetFlagStatus(uart, USART_FLAG_RXNE);
 }
