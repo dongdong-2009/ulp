119ec25f1080d6eddbacf85b3b998bb8e91a9204
dusk <daowei.fan@linktronsys.com>
2010-08-19 16:21:16 +0800

    [vvt]move code to this file from knock and pss file
    Signed-off-by: dusk <daowei.fan@linktronsys.com>

--- src/include/vvt/vvt_pulse.h
+++ src/include/vvt/vvt_pulse.h
@@ -4,13 +4,29 @@
 #ifndef __VVT_H_
 #define __VVT_H_
 
-#define GPIO_KNOCK_PATTERN	(GPIO_Pin_All & 0x003f)
 #define CH_NE58X			ADC_Channel_6
 #define CH_VSS				ADC_Channel_7
 #define CH_WSS				ADC_Channel_8
 #define CH_KNOCK_FRQ		ADC_Channel_9
 #define CH_MISFIRE_STREN	ADC_Channel_10
-#define ADC3_DR_Address    ((uint32_t)0x40013C4C)
+
+#define GPIO_KNOCK_PATTERN		(GPIO_Pin_All & 0x003f)
+#define GPIO_MISFIRE_PATTERN	(GPIO_Pin_All & 0x003f)
+
+void vvt_pulse_Init(void);
+
+void pss_Update(void);
+void pss_Enable(int on);
+void pss_SetSpeed(short hz); /*1hz speed -> 2hz dsso -> 1rpm*/
+void pss_SetVolt(pss_ch_t ch, short mv); /*on/off?*/
+void pss_SetVssSpeed(short hz);
+void pss_SetVssVolt(short mv);
+
+void knock_Update(void);
+void knock_SetFreq(short hz);
+void knock_SetVolt(knock_ch_t ch, short mv);
+int knock_GetPattern(void);
+void knock_Enable(int en);
 
 #endif /*__VVT_H_*/
 
