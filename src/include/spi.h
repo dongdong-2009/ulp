c13c48b215c437af8101fdcda5819d9df44511db
miaofng <feng.ni@linktronsys.com>
2010-08-20 16:49:03 +0800

    [spi] change spi driver api to bus_t type
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/include/spi.h
+++ src/include/spi.h
@@ -4,20 +4,19 @@
 #ifndef __SPI_H_
 #define __SPI_H_
 
-#define SPI_MODE_POL_0 (0 << 0)
-#define SPI_MODE_POL_1 (1 << 0)
-#define SPI_MODE_PHA_0 (0 << 1)
-#define SPI_MODE_PHA_1 (1 << 1)
-#define SPI_MODE_BW_8 (0 << 2)
-#define SPI_MODE_BW_16 (1 << 2)
-#define SPI_MODE_MSB (0 << 3)
-#define SPI_MODE_LSB (1 << 3)
-#define SPI_MODE_PRESCALER_2 (0<<4)
-#define SPI_MODE_PRESCALER_4 (1<<4)
+#include <stddef.h>
+#include "device.h"
 
-/*bus,range: 1, 2, ...*/
-int spi_Init(int bus, int mode);
-int spi_Write(int bus, int val);
-int spi_DMA_Write(int bus, unsigned char *pbuf, int len);
+typedef struct {
+	unsigned cpol : 1; /*clock polarity, 0-> idle low level*/
+	unsigned cpha : 1; /*clock phase, 0-> first edge active*/
+	unsigned bits : 5; /*bits of a frame, 0~31*/
+	unsigned bseq : 1; /*bit sequency, 0->lsb*/
+} spi_cfg_t;
+
+extern bus_t spi1;
+extern bus_t spi2;
+extern bus_t spi3;
+
 #endif /*__SPI_H_*/
 
--- src/lib/sys/system.c
+++ src/lib/sys/system.c
@@ -17,36 +17,6 @@ void sys_Init(void)
 #if (CONFIG_IAR_REDIRECT == 1) || (CONFIG_TASK_SHELL == 1)
 	console_Init();
 #endif
-
-#if CONFIG_SYSTEM_SPI1 == 1
-	int stm32_spi1_mode = 0;
-#ifdef CONFIG_STM32_SPI1_LEN_8
-	stm32_spi1_mode |= SPI_MODE_BW_8;	//data len 8 bits
-#else
-	stm32_spi1_mode |= SPI_MODE_BW_16;	//default 16 bits
-#endif
-#ifdef CONFIG_STM32_SPI1_PRESCALER_2
-	stm32_spi1_mode |= SPI_MODE_PRESCALER_2;	//prescaler_2
-#else
-	stm32_spi1_mode |= SPI_MODE_PRESCALER_4;	//default prescaler_4
-#endif
-	spi_Init(1, SPI_MODE_POL_1| SPI_MODE_PHA_0| stm32_spi1_mode | SPI_MODE_MSB);
-#endif
-
-#if CONFIG_SYSTEM_SPI2 == 1
-	int stm32_spi2_mode = 0;
-#ifdef CONFIG_STM32_SPI2_LEN_8
-	stm32_spi2_mode |= SPI_MODE_BW_8;	//data len 8 bits
-#else
-	stm32_spi2_mode |= SPI_MODE_BW_16;	//default 16 bits
-#endif
-#ifdef CONFIG_STM32_SPI2_PRESCALER_2
-	stm32_spi2_mode |= SPI_MODE_PRESCALER_2;	//prescaler_2
-#else
-	stm32_spi2_mode |= SPI_MODE_PRESCALER_4;	//default prescaler_4
-#endif
-	spi_Init(2, SPI_MODE_POL_0| SPI_MODE_PHA_0| stm32_spi2_mode | SPI_MODE_MSB);
-#endif
 }
 
 void sys_Update(void)
