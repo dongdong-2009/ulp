e1714e26d5c8babb1b2458c83076c0a4a9bcdbda
miaofng <feng.ni@linktronsys.com>
2010-08-20 18:03:15 +0800

    [mcp41x] dev_io_t -> bus_t update
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/drivers/mcp41x.c
+++ src/drivers/mcp41x.c
@@ -2,7 +2,9 @@
 *	miaofng@2010 initial version
 */
 
+#include "config.h"
 #include "mcp41x.h"
+#include "spi.h"
 
 #define CMD_WRITE_DATA (0x01 << 12)
 #define CMD_SHUT_DOWN (0x02 << 12)
@@ -12,10 +14,17 @@
 
 void mcp41x_Init(mcp41x_t *chip)
 {
+	spi_cfg_t cfg = {
+		.cpol = 0,
+		.cpha = 0,
+		.bits = 16,
+		.bseq = 1,
+	};
+	chip->bus->init(&cfg);
 }
 
 void mcp41x_SetPos(mcp41x_t *chip, short pos) /*0~255*/
 {
 	pos &= 0xff;
-	chip->io.write_reg(0, CMD_WRITE_DATA | SEL_POT_0 | pos);
+	chip->bus->wreg(chip->idx, CMD_WRITE_DATA | SEL_POT_0 | pos);
 }
