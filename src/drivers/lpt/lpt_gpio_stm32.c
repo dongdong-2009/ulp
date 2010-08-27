f3216f4cf02e7a8579828743dbfd321c63b8f9ee
miaofng <feng.ni@linktronsys.com>
2010-08-27 11:06:41 +0800

    [lpt] add pinmap for zf32 board
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/drivers/lpt/lpt_gpio_stm32.c
+++ src/drivers/lpt/lpt_gpio_stm32.c
@@ -17,18 +17,39 @@
 #define H Bit_SET
 #define L Bit_RESET
 
+#ifdef CONFIG_LPT_PINMAP_DEF8
 /* pinmap:
-	signal		8bit		16bit
-	data bus		PC0~7	PC0~15
+	data bus		PC0~7
 	addr bus		PC9
 	we/wr		PC8
 	oe			PC13
 	cs/enable		PC11
 */
-
+#define CONFIG_LPT_WIDTH_8BIT
 #define cs_set(level) GPIO_WriteBit(GPIOC, GPIO_Pin_11, level)
 #define we_set(level) GPIO_WriteBit(GPIOC, GPIO_Pin_8, level)
 #define oe_set(level) GPIO_WriteBit(GPIOC, GPIO_Pin_13, level)
+#endif
+
+#ifdef CONFIG_LPT_PINMAP_ZF32
+/* pinmap:
+	data bus		PC0~7
+	addr bus		PC10
+	we/wr		PC9
+	oe			PC8
+	cs/enable		PC12
+	rst			PC11
+*/
+#define CONFIG_LPT_WIDTH_8BIT
+#define cs_set(level) GPIO_WriteBit(GPIOC, GPIO_Pin_12, level)
+#define we_set(level) GPIO_WriteBit(GPIOC, GPIO_Pin_9, level)
+#define oe_set(level) GPIO_WriteBit(GPIOC, GPIO_Pin_8, level)
+#define rst_set(level) GPIO_WriteBit(GPIOC, GPIO_Pin_11, level)
+#endif 
+
+#define ndelay(ns) do { \
+	for(int i = ((ns) >> 3); i > 0 ; i -- ); \
+} while(0)
 
 static int lpt_init(void)
 {
@@ -58,7 +79,7 @@ static int lpt_init(void)
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-#ifdef CONFIG_LPT_WIDTH_8BIT
+#ifdef CONFIG_LPT_PINMAP_DEF8
 	//config addr bus as pushpull out
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
@@ -70,6 +91,28 @@ static int lpt_init(void)
 	GPIO_InitStructure.GPIO_Pin |= GPIO_Pin_13;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
+#endif
+
+#ifdef CONFIG_LPT_PINMAP_ZF32
+	//config addr bus as pushpull out
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+	//config ctrl pins as pushpull out
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Pin |= GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Pin |= GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Pin |= GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+#endif
+
+	//bus reset
+#ifdef rst_set
+	rst_set(L);
+	ndelay(10000);
+	rst_set(H);
 #endif
 
 	return 0;
@@ -104,10 +147,6 @@ static int lpt_getdata(void)
 
 	return value;
 }
-
-#define ndelay(ns) do { \
-	for(int i = ((ns) >> 3); i > 0 ; i -- ); \
-} while(0)
 
 static int lpt_write(int addr, int data)
 {
