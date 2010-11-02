4f26c6931b6fb16aa5e432b38621400e08af6392
miaofng <feng.ni@linktronsys.com>
2010-11-02 18:42:21 +0800

    [pd] add core routine
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/include/pd.h
+++ src/include/pd.h
@@ -8,6 +8,22 @@
 #include "config.h"
 #include "common/glib.h"
 
+#ifndef CONFIG_PD_RX
+#define CONFIG_PD_RX 600 //unit; Ohm
+#endif
+
+#ifndef CONFIG_PD_DX
+#define CONFIG_PD_DX	(12800)
+#endif
+
+#ifndef CONFIG_PD_DY
+#define CONFIG_PD_DY	(12800)
+#endif
+
+#ifndef CONFIG_PD_ZL
+#define CONFIG_PD_ZL	(3000)
+#endif
+
 //supported event
 enum {
 	PDE_NONE,
@@ -18,8 +34,18 @@ enum {
 	PDE_DRAG,
 };
 
+extern int pd_dx; //Delta-X limit
+extern int pd_dy; //Delta-Y limit
+extern int pd_zl; //RZ-limit
+
 int pd_Init(void);
 int pd_SetMargin(const rect_t *r);
 int pd_GetEvent(dot_t *p);
+
+/* get position and pressure info from bottom touch screen chip driver
+* x, y pos info, 15bit
+* return pressure resistor info, unit: Ohm
+*/
+int pdd_get(int *px, int *py);
 
 #endif /*__PD_H_*/
