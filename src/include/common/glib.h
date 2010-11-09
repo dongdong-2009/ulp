ea59aab39408f4e21ac1c006003d533ff35adb8c
miaofng <feng.ni@linktronsys.com>
2010-11-09 13:35:51 +0800

    [common] fixed glib bug
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/include/common/glib.h
+++ src/include/common/glib.h
@@ -44,10 +44,10 @@ static inline void rect_get(const rect_t *r, int *x, int *y, int *w, int *h)
 
 static inline int rect_zoom(rect_t *r, int xfactor, int yfactor)
 {
-	r -> x1 << xfactor;
-	r -> x2 << xfactor;
-	r -> y1 << yfactor;
-	r -> y2 << yfactor;
+	r -> x1 <<= xfactor;
+	r -> x2 <<= xfactor;
+	r -> y1 <<= yfactor;
+	r -> y2 <<= yfactor;
 	return 0;
 }
 
