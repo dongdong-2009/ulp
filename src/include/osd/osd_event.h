62bea5a22042df13c7caeb1191d98e4c02664fe9
miaofng <feng.ni@linktronsys.com>
2010-11-17 15:44:47 +0800

    [osd] osd_event pd support
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/include/osd/osd_event.h
+++ src/include/osd/osd_event.h
@@ -6,8 +6,21 @@
 
 #include <stddef.h>
 #include "key.h"
+#include "common/glib.h"
+
+enum {
+	OSDE_NONE = KEY_NONE,
+	//...KEY EVENTS
+	OSDE_DLG_QUIT = KEY_DUMMY,
+	OSDE_GRP_FOCUS,
+};
 
 int osd_event_init(void);
 int osd_event_get(void);
+
+//return 0 if pd event outside the rect, note:
+//para rc is char based coordinated system
+//para pp is pixel based coordinated system
+int osd_event_try(const rect_t *rc, const dot_t *pp);
 
 #endif /*__OSD_EVENT _H_*/
