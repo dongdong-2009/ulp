8a43ff905246d856af7a698e78a83b79a3fc25d4
miaofng <feng.ni@linktronsys.com>
2010-11-22 13:40:13 +0800

    [led] add an option for led driver enable/disable
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/lib/sys/system.c
+++ src/lib/sys/system.c
@@ -14,7 +14,9 @@ void sys_Init(void)
 	
 	SystemInit();
 	time_Init();
+#if CONFIG_DRIVER_LED == 1
 	led_Init();
+#endif
 #if (CONFIG_IAR_REDIRECT == 1) || (CONFIG_TASK_SHELL == 1)
 	console_Init();
 #endif
@@ -25,6 +27,7 @@ void sys_Init(void)
 
 void sys_Update(void)
 {
+#if CONFIG_DRIVER_LED == 1
 	led_Update();
-	time_Update();
+#endif
 }
