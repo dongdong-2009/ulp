d1a0bda2c952bce5df503ea1b38ec14e6660bfd5
daoweifan <daoweifan@126.com>
2010-12-08 14:48:13 +0800

    [vvt]improve md204l driver, remove the "redundant" bytes
    Signed-off-by: daoweifan <daoweifan@126.com>

--- src/drivers/uart/md204l.c
+++ src/drivers/uart/md204l.c
@@ -6,32 +6,29 @@
 #include "stm32f10x.h"
 #include "string.h"
 #include "md204l.h"
-#include "usart.h"
-#include "time.h"
 
-/*global members*/
-short md204l_read[MD204L_READ_LEN];
+static int md204l_GetByte(md204l_t *chip);
 
-/*private members*/
-static md204l_req_t md204l_req;
-static md204l_ack_t md204l_ack;
-static time_t md204l_deadtime;
-static int md204l_GetByte(void);
-
-void md204l_Init(void)
+void md204l_Init(md204l_t *chip)
 {
-	md204l_req.station = 1;
-	usart_Init();
+	uart_cfg_t cfg = { //UART_CFG_DEF;
+		.baud = 19200,
+	};
+
+	chip->bus->init(&cfg);
 }
 
-int md204l_Write(int addr, short *pbuf, int count)
+int md204l_Write(md204l_t *chip, int addr, short *pbuf, int count)
 {
 	int i;
 	int temp = 0;
+	md204l_req_t md204l_req;
+	md204l_ack_t md204l_ack;
 
+	md204l_req.station = chip->station;
 	md204l_req.cmd = CMD_MD204L_WRITE;
 	md204l_req.addr = (unsigned char)addr;
-	md204l_req.len = (unsigned char)count;
+	md204l_req.len = count;
 
 	/*calculate check sum*/
 	for (i = 0; i < count; i++) {
@@ -47,100 +44,103 @@ int md204l_Write(int addr, short *pbuf, int count)
 	md204l_req.cksum = (unsigned char)(temp%0x100);
 
 	/*send data by usart*/
-	usart_putchar(md204l_req.station);
-	usart_putchar(md204l_req.cmd);
-	usart_putchar(md204l_req.addr);
-	usart_putchar(md204l_req.len);
+	chip->bus->putchar(md204l_req.station);
+	chip->bus->putchar(md204l_req.cmd);
+	chip->bus->putchar(md204l_req.addr);
+	chip->bus->putchar(md204l_req.len);
 	for (i = 0; i < count; i++) {
-		usart_putchar(pbuf[i] >> 8);
-		usart_putchar(pbuf[i] & 0x00ff);
+		chip->bus->putchar(pbuf[i] >> 8);
+		chip->bus->putchar(pbuf[i] & 0x00ff);
 	}
-	usart_putchar(md204l_req.cksum);
+	chip->bus->putchar(md204l_req.cksum);
 
 	/*receive ack data*/
-	md204l_ack.station = (unsigned char)md204l_GetByte();
-	md204l_ack.status = (unsigned char)md204l_GetByte();
-	if(md204l_ack.status != MD204L_OK)
+	md204l_ack.station = (unsigned char)md204l_GetByte(chip);
+	md204l_ack.status = (unsigned char)md204l_GetByte(chip);
+	md204l_GetByte(chip);
+	if (md204l_ack.status != MD204L_OK)
 		return 1;
 
 	return 0;
 }
 
-int md204l_Read(int addr, short *pbuf, int count)
+int md204l_Read(md204l_t *chip, int addr, short *pbuf, int count)
 {
-	int temp = 0;
 	int i;
+	int temp = 0;
+	md204l_req_t md204l_req;
+	md204l_ack_t md204l_ack;
 
+	md204l_req.station = chip->station;
 	md204l_req.cmd = CMD_MD204L_READ;
-#ifndef MD204L_READ_ADDR
 	md204l_req.addr = (unsigned char)addr;
-#else
-	md204l_req.addr = MD204L_READ_ADDR;
-#endif
-
-#ifndef MD204L_READ_LEN
 	md204l_req.len = count;
-#else
-	md204l_req.len = MD204L_READ_LEN;
-#endif
 
 	temp += md204l_req.station;
 	temp += md204l_req.cmd;
 	temp += md204l_req.addr;
 	temp += md204l_req.len;
 
-	md204l_req.cksum = (unsigned char)(temp%0x100);
+	md204l_req.cksum = (unsigned char)(temp % 0x100);
 
 	/*send data by usart*/
-	usart_putchar(md204l_req.station);
-	usart_putchar(md204l_req.cmd);
-	usart_putchar(md204l_req.addr);
-	usart_putchar(md204l_req.len);
-	usart_putchar(md204l_req.cksum);
-
+	chip->bus->putchar(md204l_req.station);
+	chip->bus->putchar(md204l_req.cmd);
+	chip->bus->putchar(md204l_req.addr);
+	chip->bus->putchar(md204l_req.len);
+	chip->bus->putchar(md204l_req.cksum);
 	/*receive ack data*/
-	md204l_ack.station = (unsigned char)md204l_GetByte();
-	md204l_ack.status = (unsigned char)md204l_GetByte();
-	if(md204l_ack.status != MD204L_OK)
-		return 1;
+	md204l_ack.station = (unsigned char)md204l_GetByte(chip);
+	md204l_ack.status = (unsigned char)md204l_GetByte(chip);
+	md204l_ack.addr  = (unsigned char)md204l_GetByte(chip);
+	md204l_ack.len = (unsigned char)md204l_GetByte(chip);
 	for (i = 0; i < count; i++) {
-		pbuf[i] = (short)((usart_getch() << 8) & 0xff00);
-		pbuf[i] |= (unsigned char)usart_getch();
+		pbuf[i] = (short)(md204l_GetByte(chip) << 8);
+		pbuf[i] |= (short)(md204l_GetByte(chip) & 0x00ff);
 	}
+	md204l_GetByte(chip);
 
+	if ((md204l_ack.status != MD204L_OK) || (md204l_ack.station != chip->station))
+		return 1;
 	return 0;
 }
 
 void md204l_Update(void)
 {
-	md204l_Read(MD204L_READ_ADDR, md204l_read, MD204L_READ_LEN);
+
 }
 
-static int md204l_GetByte(void)
+static int md204l_GetByte(md204l_t *chip)
 {
-	md204l_deadtime = time_get(25);
-	while(time_left(md204l_deadtime)) {
-		if(usart_IsNotEmpty())
-			return usart_getchar();
+	chip->deadtime = time_get(25);
+	while(time_left(chip->deadtime) > 0) {
+		if(chip->bus->poll())
+			return chip->bus->getchar();
 	}
-	return 1;
+	return -1;
 }
 
-#if 1
+#if 0
 #include "shell/cmd.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+static md204l_t dbg_hid_md204l = {
+	.bus = &uart2,
+	.station = 0x01,
+};
+
+
 static int cmd_md204l_func(int argc, char *argv[])
 {
-	short temp;
+	short temp = 0;
 	int addr;
 
-	const char usage[] = { \
+	const char *usage = { \
 		" usage:\n" \
-		" md204l init,md204l debug " \
-		" md204l write regaddr value, regaddr:0-127,value:16bits " \
-		" md204l read regaddr, regaddr:0-127,value:16bits " \
+		" md204l init,md204l debug \n" \
+		" md204l write regaddr value, regaddr:0-127,value:16bits \n" \
+		" md204l read regaddr, regaddr:0-127,value:16bits \n" \
 	};
 
 	if(argc < 2) {
@@ -148,23 +148,26 @@ static int cmd_md204l_func(int argc, char *argv[])
 		return 0;
 	}
 
-	if (strcmp("init",argv[1]) == 0)
-		md204l_Init();
+	if (argv[1][0] == 'i') {
+		md204l_Init(&dbg_hid_md204l);
+		printf("init ok \n");
+	}
 
-	if (strcmp("write",argv[1]) == 0) {
+	if (argv[1][0] == 'w') {
 		addr = atoi(argv[2]);
 		temp = atoi(argv[3]);
-		//sscanf(argv[2], "%x", &addr);
-		//sscanf(argv[3], "%x", &temp);
-		md204l_Write(addr, &temp, 1);
-	}
-	
-	if (strcmp("read",argv[1]) == 0) {
+		if (md204l_Write(&dbg_hid_md204l, addr, &temp, 1))
+			printf("Write error\n");
+		else
+			printf("Write sucessfully\n");
+	}
+	if (argv[1][0] == 'r') {
+		temp = 0;
 		addr = atoi(argv[2]);
-		//sscanf(argv[2], "%x", &addr);
-		//sscanf(argv[3], "%x", &temp);
-		md204l_Read(addr, &temp, 1);
-		printf("%d\n\r",temp);
+		if (md204l_Read(&dbg_hid_md204l, addr, &temp, 1))
+			printf("Read error!\n");
+		else
+			printf("data is: %d\n\r",temp);
 	}
 
 	return 0;
