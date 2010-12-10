61db0d622b1078d6572212f6bae00fa2b89d15ec
miaofng <feng.ni@linktronsys.com>
2010-12-10 12:12:38 +0800

    [nvm] fixed bugs
    - align bug when sz_ram like 2049
    - rom1 should be erased always, or nvm_save fail
    
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/drivers/flash/nvm.c
+++ src/drivers/flash/nvm.c
@@ -8,6 +8,7 @@
 #include <stddef.h>
 #include "nvm.h"
 #include "flash.h"
+#include "debug.h"
 
 #define NVM_MAGIC ((int)(0x13572468))
 
@@ -22,18 +23,19 @@ int nvm_init(void)
 	dst = __section_begin(".nvm.ram");
 	sz_ram = (int)__section_end(".nvm.ram") - (int)__section_begin(".nvm.ram");
 	sz_rom = (int)__section_end(".nvm.rom") - (int)__section_begin(".nvm.rom");
-
+	
 	sz_rom >>= 1;
+	sz_ram = (sz_ram + 3) & ( ~ 0x03); //align to 4 bytes boundary
+	assert(sz_ram <= sz_rom);
 	bak = src + sz_rom;
 
-	//rom 1 -> ram
+	//rom 1 -> ram, always read & erase rom 1
 	flash_Read(&magic, src, 4);
 	flash_Read(&sz_nvm, src + 4, 4);
-	if(magic == NVM_MAGIC && sz_nvm == sz_ram) {
-		flash_Read(dst, src + 8, sz_ram);
-		flash_Erase(src, (sz_rom + FLASH_PAGE_SZ - 1) / FLASH_PAGE_SZ);
-		
-		//erase & rom1 -> rom 2
+	flash_Read(dst, src + 8, sz_ram);
+	flash_Erase(src, (sz_rom + FLASH_PAGE_SZ - 1) / FLASH_PAGE_SZ);
+	if(magic == NVM_MAGIC && sz_nvm == sz_ram) {		
+		//rom1 -> rom 2
 		flash_Erase(bak, (sz_rom + FLASH_PAGE_SZ - 1) / FLASH_PAGE_SZ);
 		flash_Write(bak + 8, dst, sz_ram);
 		flash_Write(bak + 4, &sz_nvm, 4); 
@@ -64,7 +66,8 @@ int nvm_save(void)
 	dest = __section_begin(".nvm.rom");
 	src = __section_begin(".nvm.ram");
 	sz_ram = (int)__section_end(".nvm.ram") - (int)__section_begin(".nvm.ram");
-
+	sz_ram = (sz_ram + 3) & ( ~ 0x03); //align to 4 bytes boundary
+	
 	flash_Write(dest + 8, src, sz_ram);
 	flash_Write(dest + 4, &sz_ram, 4);
 	flash_Write(dest + 0, &magic, 4);
