cfeaf991ea36e687c53b6bf2bfc9735f91b4c3df
miaofng <feng.ni@linktronsys.com>
2011-02-24 16:47:55 +0800

    [sys] add sys/malloc.h
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

---
+++ src/include/sys/malloc.h
@@ -0,0 +1,25 @@
+/**
+* this file is provided in purpose to provide arch/os/compiler independent api
+* for the whole software platform
+* 
+* miaofng@2010 initial version
+*
+*/
+
+#ifndef __SYS_MALLOC_H_
+#define __SYS_MALLOC_H_
+
+#include "config.h"
+#include <stdlib.h>
+#include "FreeRTOS.h"
+
+//memory management functions
+#ifdef CONFIG_LIB_FREERTOS
+#define sys_malloc pvPortMalloc
+#define sys_free vPortFree
+#else
+#define sys_malloc malloc
+#define sys_free free
+#endif
+
+#endif /*__SYS_MALLOC_H_*/
--- src/include/sys/sys.h
+++ src/include/sys/sys.h
@@ -9,17 +9,6 @@
 #ifndef __SYS_H_
 #define __SYS_H_
 
-#include "config.h"
-#include <stdlib.h>
-#include "FreeRTOS.h"
-
-//memory management functions
-#ifdef CONFIG_LIB_FREERTOS
-#define sys_malloc pvPortMalloc
-#define sys_free vPortFree
-#else
-#define sys_malloc malloc
-#define sys_free free
-#endif
+#include "sys/malloc.h"
 
 #endif /*__SYS_H_*/
