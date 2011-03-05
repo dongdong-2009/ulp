a2bf466984b2f291a149465bdcd0182c67428249
miaofng <feng.ni@linktronsys.com>
2011-03-05 19:35:24 +0800

    [nest] old mt60 nest use different power relay configuration
    Signed-off-by: miaofng <feng.ni@linktronsys.com>

--- src/tasks/nest/lib/nest_power.h
+++ src/tasks/nest/lib/nest_power.h
@@ -5,25 +5,43 @@
 #ifndef __NEST_POWER_H_
 #define __NEST_POWER_H_
 
+#ifndef CONFIG_NEST_MT60_OLD
 #define RELAY_BAT_SET(ON) do { \
 	if(ON) cncb_signal(BAT, LSD_OFF);  /*inverted on circuit*/ \
 	else cncb_signal(BAT, LSD_ON); \
 } while(0)
-
 #define RELAY_IGN_SET(ON)  do { \
 	if(ON) cncb_signal(IGN, LSD_OFF); /*inverted on circuit*/ \
 	else cncb_signal(IGN, LSD_ON); \
 } while(0)
-
 #define RELAY_ETCBAT_SET(ON) do { \
 	if(ON) cncb_signal(LSD, LSD_OFF); /*inverted on circuit*/ \
 	else cncb_signal(LSD, LSD_ON); \
 } while(0)
+#define RELAY_LOCK_SET(ON)	do { \
+	if(ON) cncb_signal(SIG3, SIG_HI); \
+	else cncb_signal(SIG3, SIG_LO);  \
+} while(0)
 
+#else /*CONFIG_NEST_MT60_OLD*/
+#define RELAY_BAT_SET(ON) do { \
+	if(ON) cncb_signal(BAT, LSD_ON); \
+	else cncb_signal(BAT, LSD_OFF); \
+} while(0)
+#define RELAY_IGN_SET(ON)  do { \
+	if(ON) cncb_signal(IGN, LSD_ON); \
+	else cncb_signal(IGN, LSD_OFF); \
+} while(0)
+#define RELAY_ETCBAT_SET(ON) do { \
+	if(ON) cncb_signal(LSD, LSD_ON); \
+	else cncb_signal(LSD, LSD_OFF); \
+} while(0)
 #define RELAY_LOCK_SET(ON)	do { \
 	if(ON) cncb_signal(SIG4, SIG_HI); \
 	else cncb_signal(SIG4, SIG_LO);  \
 } while(0)
+
+#endif /*CONFIG_NEST_MT60_OLD*/
 
 int nest_power_on(void);
 int nest_power_off(void);
