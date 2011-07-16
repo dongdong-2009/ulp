79ce1188933a695a0978b62c57a0d0481f245d36
feng.ni <feng.ni@linktronsys.com>
2011-07-16 16:44:32 +0800

    [mcamos] server support
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/lib/priv/mcamos.c
+++ src/lib/priv/mcamos.c
@@ -12,20 +12,51 @@
 #include <string.h>
 #include "debug.h"
 
-int mcamos_init(const can_bus_t *can, int baud)
+struct mcamos_s mcamos, mcamos_def;
+
+#define MCAMOS_MSG_1_STD_ID mcamos.id_cmd
+#define MCAMOS_MSG_2_STD_ID mcamos.id_dat
+
+int mcamos_init_ex(const struct mcamos_s *mcamos_new)
 {
-	int ret;
+	int ret = 0;
 	can_cfg_t cfg = CAN_CFG_DEF;
+	struct mcamos_s mcamos_old;
+
+	mcamos_new = (mcamos_new == NULL) ? &mcamos_def : mcamos_new;
+	if(!memcmp(&mcamos_new, &mcamos, sizeof(struct mcamos_s))) {
+		//the same as current configuration, nothing needs to do
+		return 0;
+	}
+
+	memcpy(&mcamos_old, &mcamos, sizeof(struct mcamos_s));
+	memcpy(&mcamos, mcamos_new, sizeof(struct mcamos_s));
+
+	//can port init?
+	if((mcamos_new ->can != mcamos_old.can) || (mcamos_new ->baud != mcamos_old.baud)) {
+		cfg.baud = mcamos_new ->baud;
+		ret += mcamos_new -> can ->init(&cfg);
+	}
+
+#if 0
+	//can filter init
 	can_filter_t filter = {
-		.id = MCAMOS_MSG_2_STD_ID,
+		.id = mcamos_new.id_dat,
 		.mask = 0xffff,
 		.flag = 0,
 	};
-
-	cfg.baud = baud;
-	ret = can -> init(&cfg);
 	ret += can -> filt(&filter, 1);
+#endif
 	return ret;
+}
+
+int mcamos_init(const can_bus_t *can, int baud)
+{
+	mcamos_def.can = can;
+	mcamos_def.baud = baud;
+	mcamos_def.id_cmd = MCAMOS_MSG_CMD_ID;
+	mcamos_def.id_dat = MCAMOS_MSG_DAT_ID;
+	return mcamos_init_ex(NULL);
 }
 
 int mcamos_dnload(const can_bus_t *can, int addr, const char *buf, int n, int timeout)
@@ -151,3 +182,106 @@ int mcamos_execute(const can_bus_t *can, int addr, int timeout)
 
 	return ret;
 }
+
+int mcamos_srv_init(mcamos_srv_t *psrv)
+{
+	int ret;
+	can_cfg_t cfg = CAN_CFG_DEF;
+	can_filter_t filter[] = {
+		{
+			.id = MCAMOS_MSG_1_STD_ID,
+			.mask = 0xffff,
+			.flag = 0,
+		},
+
+		{
+			.id = MCAMOS_MSG_2_STD_ID,
+			.mask = 0xffff,
+			.flag = 0,
+		},
+	};
+
+	cfg.baud = psrv->baud;
+	ret = psrv->can ->init(&cfg);
+	ret += psrv->can ->filt(filter, 2);
+	memset(psrv->inbox, 0, 64);
+	memset(psrv->outbox, 0, 64);
+	return ret;
+}
+
+int mcamos_srv_update(mcamos_srv_t * psrv)
+{
+	int ret, bytes, i, n;
+	unsigned addr;
+	char dir;
+	can_msg_t msg;
+	time_t deadline;
+	struct mcamos_cmd_s *cmd = (struct mcamos_cmd_s *) msg.data;
+
+	ret = psrv->can->recv(&msg);
+	if(ret)
+		return 0;
+
+	//can frame received
+	if(msg.id != MCAMOS_MSG_1_STD_ID) {
+		//strange can frame, ignore
+		return -1;
+	}
+
+	//process mcamos cmd
+	addr = cmd ->byAddress[0];
+	addr <<= 8;
+	addr |= cmd->byAddress[1];
+	addr <<= 8;
+	addr |= cmd->byAddress[2];
+	addr <<= 8;
+	addr |= cmd->byAddress[3];
+	addr = (addr & 0xffffff00 == psrv->inbox_addr) ? ((unsigned) (psrv->inbox) | (addr & 0xff)) : addr;
+	addr = (addr & 0xffffff00 == psrv->outbox_addr) ? ((unsigned) (psrv ->outbox) | (addr & 0xff)) : addr;
+
+	bytes = cmd ->byByteCnt[0];
+	bytes <<= 8;
+	bytes |= cmd ->byByteCnt[1];
+
+	dir = cmd ->byDirection;
+
+	//try to tx/rx data to/from tester
+	deadline = time_get(psrv ->timeout);
+	for( i = 0; i < bytes; ) {
+		//tester write?
+		if(dir == MCAMOS_DOWNLOAD) {
+			do {
+				ret = psrv ->can ->recv(&msg);
+				if(time_left(deadline) < 0)
+					return -ERR_TIMEOUT;
+			} while(ret);
+
+			//store data
+			if(msg.id != MCAMOS_MSG_1_STD_ID) {
+				return -1;
+			}
+
+			memcpy((void *)addr, msg.data, msg.dlc);
+			i += msg.dlc;
+			addr += msg.dlc;
+		}
+
+		//tester read
+		else {
+			n = bytes - i;
+			n = (n > 8) ? 8 : n;
+			memcpy(msg.data, (void *)addr, n);
+			do {
+				ret = psrv ->can ->send(&msg);
+				if(time_left(deadline) < 0)
+					return -ERR_TIMEOUT;
+			} while(ret);
+
+			addr += n;
+			i += n;
+		}
+	}
+
+	return 0;
+}
+
