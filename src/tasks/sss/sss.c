924f5b1d2984d3d849660435a8787c06ca60e155
peng.guo <peng.guo@linktronsys.com>
2011-08-16 10:55:15 +0800

    add GPIO configration
    Signed-off-by: peng.guo <peng.guo@linktronsys.com>

--- src/tasks/sss/sss.c
+++ src/tasks/sss/sss.c
@@ -28,6 +28,24 @@ char can1_int(void)
 	return 0;
 }
 
+void ADDRESS_GPIO_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	//°å¿¨LED²¿·Ö
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_SetBits(GPIOC, GPIO_Pin_12);
+	//PCB Bug
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
 void RESET_Init(void)
 {
 	//PCB Bug
@@ -107,6 +125,7 @@ int main(void)
 {
 	task_Init();
 	can1_int();
+        ADDRESS_GPIO_Init();
 	while(1) {
 		task_Update();
 		can_updata();
