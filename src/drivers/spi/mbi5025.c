17740dc3e1415ba445fe58171f0c9d6ec0e2de0f
David <daoweifan@126.com>
2011-08-17 11:28:21 +0800

    [mbi5025] add mbi5025 debug cmd
    Signed-off-by: David <daoweifan@126.com>

--- src/drivers/spi/mbi5025.c
+++ src/drivers/spi/mbi5025.c
@@ -14,7 +14,7 @@ void mbi5025_Init(mbi5025_t *chip)
 		.cpha = 0,
 		.bits = 8,
 		.bseq = 1,
-		.freq = 9000000,
+		.freq = 1000000,
 	};
 	chip->bus->init(&cfg);
 }
@@ -24,34 +24,34 @@ void mbi5025_WriteByte(mbi5025_t *chip, unsigned char data)
 	chip->bus->wreg(chip->idx, data);
 }
 
-#if 0
+#if 1
 #include "shell/cmd.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static nxp_74hct595_t nxp_74hct595 = {
-	.bus = &spi1,
-	.idx = SPI_1_NSS,
+static mbi5025_t sr = {
+	.bus = &spi2,
+	.idx = SPI_CS_DUMMY,
 };
 
-static int cmd_74hct595_func(int argc, char *argv[])
+static int cmd_mbi5025_func(int argc, char *argv[])
 {
-	unsigned char temp = 0;
+	int temp = 0;
 
 	const char * usage = { \
 		" usage:\n" \
-		" 74hct595 init, chip init \n" \
-		" 74hct595 write value, write reg \n" \
+		" mbi5025 init, chip init \n" \
+		" mbi5025 write value, write reg \n" \
 	};
 
 	if (argc > 1) {
 		if(argv[1][0] == 'i')
-			nxp_74hct595_Init(&nxp_74hct595);
+			mbi5025_Init(&sr);
 
 		if (argv[1][0] == 'w') {
 			sscanf(argv[2], "%x", &temp);
-			nxp_74hct595_WriteByte(&nxp_74hct595, temp);
+			mbi5025_WriteByte(&sr, temp);
 		}
 	}
 
@@ -62,6 +62,6 @@ static int cmd_74hct595_func(int argc, char *argv[])
 
 	return 0;
 }
-const cmd_t cmd_74hct595 = {"74hct595", cmd_74hct595_func, "74hct595 cmd"};
-DECLARE_SHELL_CMD(cmd_74hct595)
+const cmd_t cmd_mbi5025 = {"mbi5025", cmd_mbi5025_func, "mbi5025 cmd"};
+DECLARE_SHELL_CMD(cmd_mbi5025)
 #endif
