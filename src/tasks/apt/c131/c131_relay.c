f587d6ba6ccce7b2d02d04ac381cc9a9c38c07d0
David <daoweifan@126.com>
2011-09-02 18:36:25 +0800

    [apt c131] add relay related functions
    Signed-off-by: David <daoweifan@126.com>

--- src/tasks/apt/c131/c131_relay.c
+++ src/tasks/apt/c131/c131_relay.c
@@ -10,12 +10,74 @@
 #include <stdio.h>
 #include <string.h>
 
+static unsigned char LoadRAM[8];static unsigned short pLoadRAM;
+static unsigned char SRImage[6];
 
-int loop_SetRelayStatus(int loop_status);
-int loop_GetRelayStatus(int * ploop_status);
-int ess_SetRelayStatus(int esss_tatus);
-int ess_GetRelayStatus(int * pess_status);
-int led_SetRelayStatus(int led_status);
-int led_GetRelayStatus(int * pled_status);
-int sw_SetRelayStatus(int sw_status);
-int sw_GetRelayStatus(int * psw_status);
+void c131_relay_Init(void)
+{
+	//load config from rom
+	pLoadRAM = (unsigned short)LoadRAM;
+}
+
+int loop_SetRelayStatus(unsigned short loop_relays, int act)
+{
+	if (act == RELAY_OFF) {
+		pLoadRAM[0] &= ~loop_relays;
+	} else {
+		pLoadRAM[0] |= loop_relays;
+	}
+	return 0;
+}
+
+int loop_GetRelayStatus(unsigned short * ploop_status)
+{
+	(* ploop_status) = pLoadRAM[0];
+	return 0;
+}
+
+int ess_SetRelayStatus(unsigned short ess_relays, int act)
+{
+	if (act == RELAY_OFF) {
+		pLoadRAM[1] &= ~ess_relays;
+	} else {
+		pLoadRAM[1] |= ess_relays;
+	}
+	return 0;
+}
+
+int ess_GetRelayStatus(unsigned short * pess_status)
+{
+	(* pess_status) = pLoadRAM[1];
+	return 0;
+}
+
+int led_SetRelayStatus(unsigned short led_relays, int act)
+{
+	if (act == RELAY_OFF) {
+		pLoadRAM[2] &= ~led_relays;
+	} else {
+		pLoadRAM[2] |= led_relays;
+	}
+	return 0;
+}
+
+int led_GetRelayStatus(unsigned short * pled_status)
+{
+	(* pled_status) = pLoadRAM[2];
+	return 0;
+}
+
+int sw_SetRelayStatus(unsigned short sw_relays, int act)
+{
+	if (act == RELAY_OFF) {
+		pLoadRAM[3] &= ~sw_relays;
+	} else {
+		pLoadRAM[3] |= sw_relays;
+	}
+	return 0;}
+
+int sw_GetRelayStatus(unsigned short * psw_status)
+{
+	(* psw_status) = pLoadRAM[3];
+	return 0;
+}
