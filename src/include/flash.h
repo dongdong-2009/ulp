e7ea41ed320729bf2b88dfefeb396ec858ab70fd
feng.ni <feng.ni@linktronsys.com>
2011-10-15 22:09:54 +0800

    [flash] get flash size info from chip
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/include/flash.h
+++ src/include/flash.h
@@ -8,21 +8,8 @@
 #include <stddef.h>
 #include <config.h>
 
-#if CONFIG_CPU_STM32 == 1
-#if CONFIG_STM32F10X_LD == 1
-#define FLASH_PAGE_SZ 1024
-#define FLASH_PAGE_NR 32
-#elif CONFIG_STM32F10X_MD == 1
-#define FLASH_PAGE_SZ 1024
-#define FLASH_PAGE_NR 128
-#elif CONFIG_STM32F10X_HD == 1
-#define FLASH_PAGE_SZ 2048
-#define FLASH_PAGE_NR 256
-#elif CONFIG_STM32F10X_CL == 1
-#define FLASH_PAGE_SZ 2048
-#define FLASH_PAGE_NR 128
-#endif
-#endif
+#define FLASH_PAGE_SZ	(2048)
+#define FLASH_PAGE_NR	((*(unsigned short *)0x1ffff7e0) >> 1)
 
 //erase n-pages of flash sectors, which is given by address of dest(must be PAGE_SIZE aligned)
 int flash_Erase(void *dest, size_t n);
