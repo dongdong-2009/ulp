6a007858a0f6667d5e4e39931ca7c5f98ad68469
peng.guo <peng.guo@linktronsys.com>
2011-10-18 18:14:18 +0800

    "updata ls1203 driver"
    Signed-off-by: peng.guo <peng.guo@linktronsys.com>

--- src/drivers/uart/ls1203.c
+++ src/drivers/uart/ls1203.c
@@ -1,12 +1,13 @@
 /*
  * 	2011 initial version
- * Author: david
+ * Author: peng.guo david
  */
 
 #include "config.h"
 #include "stm32f10x.h"
 #include "string.h"
 #include "ls1203.h"
+#include "ulp_time.h"
 
 void ls1203_Init(ls1203_t *chip)
 {
@@ -15,20 +16,26 @@ void ls1203_Init(ls1203_t *chip)
 	};
 
 	chip->bus->init(&cfg);
-
 }
 
-int ls1203_Read(ls1203_t *chip, char *pdata)
+int ls1203_Read(ls1203_t *chip, char *code_data)
 {
-	for (int i=0;i<chip->data_len;i++) {
-		if (chip->bus->poll() == 1) {
-			pdata[i] == chip->bus->getchar();
-			if (i == chip->data_len - 1 && chip->bus->poll() == 0)
-				return 1;
+	int i = 0;
+	time_t scanner_overtime;
+
+	if (chip->bus->poll()) {
+		for (i = 0; i < chip->data_len; i++) {
+			scanner_overtime = time_get(chip->dead_time);
+			while (chip->bus->poll() == 0) {
+				if (time_left(scanner_overtime) < 0)
+					return 1;
+			}
+			code_data[i] = chip->bus->getchar();
 		}
 		return 0;
 	}
-	return 0;
+
+	return 1;
 }
 
 #if 1
@@ -36,16 +43,16 @@ int ls1203_Read(ls1203_t *chip, char *pdata)
 #include <stdio.h>
 #include <stdlib.h>
 
-static ls1203_t pdi_ls1203 = {
+static ls1203_t sr = {
 	.bus = &uart2,
 	.data_len = 19,
 	.dead_time = 20,
 };
-
+static char pdi_barcode[19];
 
 static int cmd_ls1203_func(int argc, char *argv[])
 {
-	char *barcode;
+	
 
 	const char *usage = { \
 		" usage:\n" \
@@ -53,22 +60,29 @@ static int cmd_ls1203_func(int argc, char *argv[])
 		" ls1203 read,scan barcode \n" \
 	};
 
-	if(argc == 1) {
-		printf(usage);
-		return 0;
+	if(argc > 0) {
+		if(argc < 2){
+			printf(usage);
+			return 0;
+		}
+
+		if (argv[1][0] == 'i') {
+			ls1203_Init(&sr);
+			printf("init ok \n");
+			return 0;
+		}
+
+		if (argv[1][0] == 'r') {
+			if (ls1203_Read(&sr,pdi_barcode) == 0)
+				printf("data is:%s\n", pdi_barcode);
+			return 1;
+		}
 	}
 
-	if (argv[1][0] == 'i') {
-		ls1203_Init(&pdi_ls1203);
-		printf("init ok \n");
-	}
-
-	if (argv[1][0] == 'r') {
-		if (ls1203_Read(&pdi_ls1203, barcode))
-			printf("%s\r", barcode);
-		//else
-			//printf("the length of the barcode is not right");
-                return 1;
+	if(argc == 0) {
+		if (ls1203_Read(&sr, pdi_barcode) == 0)
+			printf("data is:%s\n", pdi_barcode);
+		return 1;
 	}
 
 	return 0;
