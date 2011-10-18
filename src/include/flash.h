ea9d68937bd4dff20e66f236f986bb9c076855aa
feng.ni <feng.ni@linktronsys.com>
2011-10-18 20:14:50 +0800

    [flash] pointer type change
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/include/flash.h
+++ src/include/flash.h
@@ -17,14 +17,14 @@
 #endif
 
 //erase n-pages of flash sectors, which is given by address of dest(must be PAGE_SIZE aligned)
-int flash_Erase(void *dest, size_t n);
+int flash_Erase(const void *dest, size_t n);
 
 /*write/read flash,  return bytes have been wrote/read
 note: 
 	1, flash mem space must be erased before program
 	2, dest, n must be 4 bytes aligned
 */
-int flash_Write(void *dest, const void *src, size_t n);
+int flash_Write(const void *dest, const void *src, size_t n);
 int flash_Read(void *dest, const void *src, size_t n);
 
 #endif /*__FLASH_H_*/
