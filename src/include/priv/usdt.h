9a02fe2567c9974409b06319a70eebb2401afc25
David <daoweifan@126.com>
2011-10-19 19:56:57 +0800

    [usdt] add Encoding of protocol control information(PCI) bytes of USDT frames
    Signed-off-by: David <daoweifan@126.com>

---
+++ src/include/priv/usdt.h
@@ -0,0 +1,15 @@
+/*
+ * 	david@2011 initial version
+ *
+ */
+#ifndef __USDT_H_
+#define __USDT_H_
+
+#include "config.h"
+#include "can.h"
+#include <stddef.h>
+
+int usdc_GetDiagFirstFrame(can_msg_t *pReq, can_filter_t *pResFilter, can_msg_t *pRes);
+int usdc_GetDiagLeftFrame(can_msg_t *pRes, int msg_len);
+
+#endif /*__USDT_H_*/
