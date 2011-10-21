ce97f87b96fc82ddcb183d1571919ece19ab1b4b
David <daoweifan@126.com>
2011-10-21 14:15:06 +0800

    [usdt] improved, support the request with multi can frame. such as "read the EEPROM" request
    Signed-off-by: David <daoweifan@126.com>

--- src/include/priv/usdt.h
+++ src/include/priv/usdt.h
@@ -10,7 +10,7 @@
 #include <stddef.h>
 
 int usdt_Init(can_cfg_t * cfg);
-int usdc_GetDiagFirstFrame(can_msg_t *pReq, can_filter_t *pResFilter, can_msg_t *pRes);
+int usdc_GetDiagFirstFrame(can_msg_t *pReq, int req_len, can_filter_t *pResFilter, can_msg_t *pRes);
 int usdc_GetDiagLeftFrame(can_msg_t *pRes, int msg_len);
 
 #endif /*__USDT_H_*/
--- src/lib/priv/usdt.c
+++ src/lib/priv/usdt.c
@@ -37,10 +37,10 @@ int usdt_Init(can_cfg_t * cfg)
 }
 
 //return the length of can frame, -1 means wrong
-int usdt_GetDiagFirstFrame(can_msg_t *pReq, can_filter_t *pResFilter, can_msg_t *pRes)
+int usdt_GetDiagFirstFrame(can_msg_t *pReq,  int req_len, can_filter_t *pResFilter, can_msg_t *pRes)
 {
 	time_t over_time;
-	int num_frame, data_len, mf_flag = 0;
+	int num_frame, data_len, i, mf_flag = 0;
 
 #ifdef CONFIG_TASK_APTC131
 	can_filter_t filter = {
@@ -74,9 +74,26 @@ int usdt_GetDiagFirstFrame(can_msg_t *pReq, can_filter_t *pResFilter, can_msg_t 
 	can_bus -> filt(pResFilter, 2);
 #endif
 
-
 	can_bus -> flush();
-	can_bus -> send(pReq);						//send request
+	if(req_len == 1)
+		can_bus -> send(pReq);						//send request
+	else if (req_len > 1) {
+		can_bus -> send(pReq);
+		//get the flow control msg reponse
+		over_time = time_get(50);
+		do {
+			if (time_left(over_time) < 0)
+				return -1;
+			if (can_bus -> recv(pRes) == 0)
+				break;
+		} while(1);
+		for (i = 1; i < req_len; i++) {
+			can_bus -> send(pReq + i);
+		}
+	}
+	else
+		return -1;
+
 
 	//recv reponse
 	over_time = time_get(50);
@@ -138,8 +155,8 @@ int usdc_GetDiagLeftFrame(can_msg_t *pRes, int msg_len)
 
 int cmd_usdt_func(int argc, char *argv[])
 {
-	int i, msg_len;
-	can_msg_t msg_req, msg_res, *pRes;
+	int i, msg_len, req_len;
+	can_msg_t msg_req[2], msg_res, *pRes;
 	can_cfg_t cfg = CAN_CFG_DEF;
 
 
@@ -157,20 +174,31 @@ int cmd_usdt_func(int argc, char *argv[])
 
 	//for geting diagnosis data
 	if (strcmp(argv[1], "req") == 0) {
-
 		//get the input can msg
-		msg_req.dlc = argc - 3;
-		msg_req.flag = 0;
-		if (argc > 3)
-			sscanf(argv[2], "%x", &msg_req.id); //id
+		req_len = (argc - 3) >> 3;
+		msg_req[0].dlc = argc - 3;
+		if (msg_req[0].dlc > 8) {
+			msg_req[0].dlc = 8;
+			msg_req[1].dlc = msg_req[0].dlc - 8;
+			msg_req[1].flag = 0;
+		}
+		msg_req[0].flag = 0;
+		if (argc > 3) {
+			sscanf(argv[2], "%x", &msg_req[0].id); //id
+			msg_req[1].id = msg_req[0].id;
+		}
 		else 
 			return 0;
-		for(i = 0; i < msg_req.dlc; i ++) {
-				sscanf(argv[3 + i], "%x", (int *)&msg_req.data[i]);
+		for(i = 0; i < msg_req[0].dlc; i ++) {
+				sscanf(argv[11 + i], "%x", (int *)&msg_req[0].data[i]);
+		}
+
+		for(i = 0; i < msg_req[1].dlc; i ++) {
+			sscanf(argv[3 + i], "%x", (int *)&msg_req[1].data[i]);
 		}
 
 		//get the diagnostic info
-		msg_len = usdt_GetDiagFirstFrame(&msg_req, NULL, &msg_res);
+		msg_len = usdt_GetDiagFirstFrame(msg_req, req_len, NULL, &msg_res);
 		// can_msg_print((can_msg_t const *)&msg_res, "\n");
 		// printf("can frame length : %d\n", msg_len);
 		if (msg_len > 1) {
