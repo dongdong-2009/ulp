9e2bf55e12f0ef68951f90453bcf0cd5bf1b4135
David <daoweifan@126.com>
2011-10-23 19:08:33 +0800

    [python apt] advanced
    Signed-off-by: David <daoweifan@126.com>

--- src/tasks/apt/python_apt/apt_serial.py
+++ src/tasks/apt/python_apt/apt_serial.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import serial
+import time
 
 class ComThread:
     def __init__(self, Port ,Baudrate, value = None):
@@ -20,9 +21,8 @@ class ComThread:
         self.l_serial = serial.Serial()
         self.l_serial.port = self.port
         self.l_serial.baudrate = self.baudrate
-        self.l_serial.timeout = 2
+        self.l_serial.timeout = 0.2   #read time out 
         self.l_serial.open()
-        
         if self.l_serial.isOpen():
             self.alive = True
             return True
@@ -36,10 +36,25 @@ class ComThread:
         self.alive = False
         if self.l_serial.isOpen():
             self.l_serial.close()
+    def read(self):
+        if self.alive:
+            time.sleep(0.2)
+            try:
+                data = ''
+                n = self.l_serial.inWaiting()
+                print "Receive %d bytes" %n
+                if n:
+                    data = data + self.l_serial.read(n)
+                    #for i in xrange(len(data)):
+                     #   print '%s'  % data[i]
+            except Exception,ex:
+                print str(ex)
+        return data
+            
 
 if __name__ == '__main__':
-    rt = ComThread(2,115200)
-    rt.set_option(3,9600)
+    rt = ComThread(3,115200)
+    #rt.set_option(11,115200)
     try:
         if rt.start():
             while True:
---
+++ src/tasks/apt/python_apt/bin_setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from cx_Freeze import setup, Executable
+
+includeFiles = [
+   # (r"D:\lib\Python26\tcl\tcl8.5", "tcl"),
+    #(r"D:\lib\Python26\tcl\tk8.5", "tk")
+    ]
+
+setup(
+        name = "hello",
+        version = "0.1",
+        description = "Sample cx_Freeze script",
+        options = {"build_exe": {"include_files": includeFiles,}},
+        executables = [Executable("apt.py",base="Win32GUI",icon = "py.ico",)])
