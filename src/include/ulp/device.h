2c479652e285340fd5fda2045ed2f0c933bf2444
feng.ni <feng.ni@linktronsys.com>
2011-12-07 18:15:44 +0800

    update ulp driver structure
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

---
+++ src/include/ulp/device.h
@@ -0,0 +1,56 @@
+/*
+ * 	miaofng@2010 initial version
+ *	miaofng@2011 implement
+ *	to identify which driver belongs to wich device, the name of driver and device must be the same
+ */
+#ifndef __DEVICE_H_
+#define __DEVICE_H_
+
+#include "ulp/driver.h"
+#include "ulp/debug.h"
+#include "linux/list.h"
+#include <stdlib.h>
+
+struct device_s {
+	const char *name;
+	char suffix; //uart_stm32.1 uart_stm32.2 uart_sam3u.g
+	struct list_head list; //next dev in all devs list
+	struct list_head lcls; //next dev in the same class
+	struct driver *pdrv;
+	void *priv; //to store device specific data
+};
+
+static inline void* dev_priv_get(int fd) {
+	struct device_s * dev = (struct device_s *)fd;
+	assert(dev != NULL);
+	return dev->priv;
+}
+
+static inline int dev_priv_set(int fd, const void *priv) {
+	struct device_s * dev = (struct device_s *)fd;
+	assert(dev != NULL);
+	fd->priv = priv;
+	return 0;
+}
+
+/*path is something like 'uart0'(class name + index) or 'uart_stm32.0'(device name)*/
+int dev_open(const char *path, const char *mode);
+int dev_ioctl(int fd, int request, ...);
+int dev_poll(int fd, int event);
+int dev_read(int fd, void *buf, int count);
+int dev_write(int fd, const void *buf, int count);
+int dev_close(int fd);
+
+int dev_register(const char *path, void *pcfg);
+
+/*handling device class*/
+struct class_s {
+	const char *name;
+	struct list_head list; //next class in all class list
+	struct list_head devs; //head of devs of this class
+}
+
+int class_dev_add(const char *name, struct device_s *pdev);
+struct device_s* class_dev_get(const char *name);
+
+#endif /*__DEVICE_H_*/
---
+++ src/include/ulp/driver.h
@@ -0,0 +1,35 @@
+/*
+ * 	miaofng@2010 initial version
+ */
+#ifndef __DRIVER_H_
+#define __DRIVER_H_
+
+#include "linux/list.h"
+
+struct drv_ops_s {
+	int (*init)(int fd, void *pcfg);
+	int (*open)(int fd);
+	int (*ioctl)(int fd, int request, ...);
+	int (*poll)(int fd, int event);
+	int (*read)(int fd, void *buf, int count);
+	int (*write)(int fd, const void *buf, int count);
+	int (*close)(int fd);
+};
+
+struct driver_s {
+	const char *name;
+	struct list_head list; //next drv in all drv list
+	const struct drv_ops_s *ops; //init/open/close/read/write/ioctl/poll
+};
+
+struct drv_ops_s* drv_register(const char *name, const void *ops);
+struct drv_ops_s* drv_open(const char *name);
+int drv_close(driver_s *pdrv);
+
+#pragma section=".driver" 4
+#define driver_init(init) \
+	void (*##init##_entry)(void)@".driver" = &##init
+
+void drv_Init(void);
+
+#endif /*__DRIVER_H_*/
