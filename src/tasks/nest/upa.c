7c1d2502e8af200e9ebb075a9dbcd7f599990405
feng.ni <feng.ni@linktronsys.com>
2012-01-09 17:27:06 +0800

    [upa] upa prx mode works now
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/tasks/nest/upa.c
+++ src/tasks/nest/upa.c
@@ -7,13 +7,33 @@
 #include "shell/cmd.h"
 #include "shell/shell.h"
 #include "linux/list.h"
+#include "lib/nest_wl.h"
+#include <string.h>
+#include "sys/malloc.h"
+
+#define UPA_FLAG_MON (1<<0)
+#define UPA_FLAG_SEL (1<<1)
+#define UPA_FLAG_UPD (1<<2)
+
+struct upa_nest_s {
+	unsigned addr;
+	unsigned flag;
+	struct list_head list;
+};
 
 static int upa_wl_fd;
 static struct console_s *upa_wl_cnsl;
+static LIST_HEAD(upa_nest_queue);
+static enum {
+	UPA_STATUS_INIT,
+	UPA_STATUS_PRX,
+	UPA_STATUS_PTX_MON, //update monitor nest
+	UPA_STATUS_PTX_SEL, //update selected nest
+	UPA_STATUS_PTX_UPD, //update each nest
+} upa_status;
 
 static int upa_Init(void)
 {
-	struct console_s *cnsl;
 	static const struct nrf_cfg_s nrf_cfg = {
 		.spi = &spi1,
 		.gpio_cs = SPI_1_NSS,
@@ -22,21 +42,101 @@ static int upa_Init(void)
 
 	dev_register("nrf", &nrf_cfg);
 	upa_wl_fd = dev_open("wl0", 0);
-	dev_ioctl(upa_wl_fd, WL_SET_FREQ, UPA_FREQ);
-	dev_ioctl(upa_wl_fd, WL_ERR_FUNC, nest_wl_onfail);
+	dev_ioctl(upa_wl_fd, WL_SET_FREQ, NEST_WL_FREQ);
+	//dev_ioctl(upa_wl_fd, WL_ERR_FUNC, nest_wl_onfail);
+	upa_wl_cnsl = console_register(upa_wl_fd);
+	assert(upa_wl_cnsl != NULL);
+	shell_register(upa_wl_cnsl);
+	shell_mute(upa_wl_cnsl, 1);
+	dev_ioctl(upa_wl_fd, WL_FLUSH);
+	upa_status = UPA_STATUS_INIT;
 	return 0;
 }
 
 
 static int upa_Update(void)
 {
-	if(upa_wl_cnsl == NULL) {
-		//forward the received wireless data to host
-		char c = 0;
-		while(dev_poll(upa_wl_fd, POLLIN) > 0) {
-			dev_read(upa_wl_fd, &c, 1);
-			console_putch(c);
+	int ntx, nrx;
+	struct list_head *pos;
+	struct upa_nest_s *nest;
+	ntx = dev_poll(upa_wl_fd, POLLOUT);
+	nrx = dev_poll(upa_wl_fd, POLLIN);
+	if(ntx != 0 || nrx != 0) //to avoid tbuf&rbuf flush
+		return 0;
+
+	//state machine change?
+	if(upa_status == UPA_STATUS_PRX) {
+		if(!list_empty(&upa_nest_queue)) {
+			//switch to PTX mode
+			dev_ioctl(upa_wl_fd, WL_STOP);
+			dev_ioctl(upa_wl_fd, WL_SET_MODE, WL_MODE_PTX);
+			dev_ioctl(upa_wl_fd, WL_SET_ADDR, NEST_WL_ADDR);
+			dev_ioctl(upa_wl_fd, WL_START);
+			upa_status = UPA_STATUS_PTX_MON;
 		}
+	}
+	else {
+		if(list_empty(&upa_nest_queue)) {
+			//switch to PRX mode
+			dev_ioctl(upa_wl_fd, WL_STOP);
+			dev_ioctl(upa_wl_fd, WL_SET_MODE, WL_MODE_PRX);
+			dev_ioctl(upa_wl_fd, WL_SET_ADDR, NEST_WL_ADDR);
+			dev_ioctl(upa_wl_fd, WL_START);
+			upa_status = UPA_STATUS_PRX;
+		}
+	}
+
+	//At least one nest exist when upa in ptx mode!!!
+	switch (upa_status) {
+	case UPA_STATUS_PRX:
+		break;
+	case UPA_STATUS_PTX_MON:
+		list_for_each(pos, &upa_nest_queue) {
+			nest = list_entry(pos, upa_nest_s, list);
+			if(nest->flag & UPA_FLAG_MON)
+				break;
+		}
+		dev_ioctl(upa_wl_fd, WL_STOP);
+		dev_ioctl(upa_wl_fd, WL_SET_ADDR, nest->addr);
+		dev_ioctl(upa_wl_fd, WL_START);
+		upa_status = UPA_STATUS_PTX_SEL;
+		break;
+	case UPA_STATUS_PTX_SEL:
+		list_for_each(pos, &upa_nest_queue) {
+			nest = list_entry(pos, upa_nest_s, list);
+			if(nest->flag & UPA_FLAG_SEL)
+				break;
+		}
+		dev_ioctl(upa_wl_fd, WL_STOP);
+		dev_ioctl(upa_wl_fd, WL_SET_ADDR, nest->addr);
+		dev_ioctl(upa_wl_fd, WL_START);
+		upa_status = UPA_STATUS_PTX_UPD;
+		break;
+	case UPA_STATUS_PTX_UPD:
+		//seach for current
+		list_for_each(pos, &upa_nest_queue) {
+			nest = list_entry(pos, upa_nest_s, list);
+			if(nest->flag & UPA_FLAG_UPD) {
+				nest->flag &= ~UPA_FLAG_UPD;
+				break;
+			}
+		}
+
+		//search for next nest in list
+		pos = pos->next;
+		if(pos == &upa_nest_queue) {
+			pos = pos->next;
+		}
+		nest = list_entry(pos, upa_nest_s, list);
+		nest->flag |= UPA_FLAG_UPD;
+		dev_ioctl(upa_wl_fd, WL_STOP);
+		dev_ioctl(upa_wl_fd, WL_SET_ADDR, nest->addr);
+		dev_ioctl(upa_wl_fd, WL_START);
+		upa_status = UPA_STATUS_PTX_MON;
+		break;
+	default:
+		upa_status = UPA_STATUS_INIT;
+		break;
 	}
 	return 0;
 }
@@ -45,7 +145,7 @@ void main(void)
 {
 	task_Init();
 	upa_Init();
-	printf("\nPower Conditioning - UPA\n");
+	printf("Power Conditioning - UPA\n");
 	printf("IAR C Version v%x.%x, Compile Date: %s,%s\n", (__VER__ >> 24),((__VER__ >> 12) & 0xfff),  __TIME__, __DATE__);
 
 	while(1){
@@ -64,27 +164,9 @@ static int cmd_upa_func(int argc, char *argv[])
 	};
 
 	if(argc == 3) {
-		if(!strcmp(argv[1], "monitor")) {
-			dev_ioctl(upa_wl_fd, WL_SET_ADDR, NEST_WL_ADDR);
-			if(!strcmp(argv[2], 'start')) {
-				upa_wl_cnsl = console_register(upa_wl_fd);
-				assert(upa_wl_cnsl != NULL);
-				dev_ioctl(upa_wl_fd, WL_SET_MODE, WL_MODE_PRX);
-				shell_register(upa_wl_cnsl);
-			}
-			else {
-				dev_ioctl(upa_wl_fd, WL_SET_MODE, WL_MODE_PTX);
-				shell_unregister(upa_wl_cnsl);
-				console_unregister(upa_wl_cnsl);
-				upa_wl_cnsl = NULL;
-			}
-			return 0;
-		}
-
 		if(!strcmp(argv[1], "select")) {
 			int addr = 0;
 			sscanf(argv[2], "%x", &addr);
-			dev_ioctl(upa_wl_fd, WL_SET_ADDR, addr);
 			return 0;
 		}
 
