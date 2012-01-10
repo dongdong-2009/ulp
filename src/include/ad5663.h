c7be161dd128b8b73aa0e1c9047cdce83b5ca5ba
lijun.xu <lijun.xu@linktronsys.com>
2012-01-10 14:34:35 +0800

    [YBS] add DAC IC of ad5663 and  PA2&PA3 cs signal
    Signed-off-by: lijun.xu <lijun.xu@linktronsys.com>

---
+++ src/include/ad5663.h
@@ -0,0 +1,43 @@
+/*
+ * 	junjun@2011 initial version
+ */
+#ifndef __AD5663_H_
+#define __AD5663_H_
+
+#include "ulp/device.h"
+
+/*IOCTL CMDS - there is no specific call sequency requirements*/
+enum {
+	DAC_WRITEN,
+	DAC_UPDATEN,
+	DAC_WRIN_UPDALL,
+	DAC_WRIN_UPDN,
+	DAC_POWER_DW,
+	DAC_RESET,
+	DAC_REG_SET,
+	DAC_RESV,
+	DAC_CHOOSE_CHANNEL1, /*Channel 1 choose*/
+	DAC_CHOOSE_CHANNEL2,
+};
+
+struct ad5663_chip_s {
+	const spi_bus_t *spi;
+	char gpio_cs;
+	char gpio_ldac;
+	char gpio_clr;
+	struct list_head pipes;
+};
+
+struct ad5663_buffer_s {
+	unsigned short dv;
+	unsigned addr : 3;
+	unsigned cmd : 3;
+	unsigned resv : 2;
+};
+
+struct ad5663_priv_s {
+	struct ad5663_chip_s *chip;
+	struct ad5663_buffer_s *buffer;
+};
+
+#endif /*__AD5663_H_*/
