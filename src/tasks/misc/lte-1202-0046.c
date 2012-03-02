3389b19f8406e52e5224c1c3731e3eae289d3c95
unknown <Simon@.(none)>
2012-03-02 10:16:36 +0800

    Simon update for ict
    Signed-off-by: peng.guo <peng.guo@linktronsys.com>

--- src/tasks/misc/lte-1202-0046.c
+++ src/tasks/misc/lte-1202-0046.c
@@ -1,196 +1,179 @@
+/*
+ * 	peng.guo@2012 initial version
+ */
+
 #include "config.h"
 #include "debug.h"
 #include "ulp_time.h"
 #include "stm32f10x_gpio.h"
 #include "sys/task.h"
-#include "74hct595.h"
+#include "mbi5025.h"
+#include "dac.h"
+#include "shell/cmd.h"
 
-#define LED_GREEN				(1<<12)
-#define LED_RED					(1<<10)
-#define BO_0					(1<<6)
-#define BO_1					(1<<7)
-#define BO_2					(1<<8)
-#define BO_3					(1<<9)
-#define BO_CTRL					(1<<8)
-#define WLI_0					(1<<0)
-#define WLI_1					(1<<1)
-#define WLI_2					(1<<2)
-#define WLI_3					(1<<3)
-#define test1					(1<<6)
-#define test2					(1<<7)
-#define NSS					(1<<4)
-
-
-static nxp_74hct595_t chip = {
-	.bus = &spi1,
-	.idx = SPI_1_NSS,
+static const mbi5025_t ict_mbi5025 = {
+		.bus = &spi2,
+		.idx = SPI_CS_DUMMY,
+		.load_pin = SPI_CS_PD8,
+		.oe_pin = SPI_CS_PD9,
 };
 
-static int a1=1,a2=1,a3=1,i1=1,i2,i3,i4,i5,b1=1,b2=1,b3=1,b4=1,b5=1,b6=1;
-static time_t d1=0,d2=0,d3=0,d4=0,d5=0,d6=0,d7=0,d8=0;
-/*	a1，a2，a3让时间捕获不会重复赋值
-	i1，i2,可以使能或者失能按键
-	i3，i4，i5失能或者使能else中的语句
-	b1~b6，不重复执行时间到达函数
-	d1~d7是时间赋值的目标变量*/
-
-void spc_Init(void)
+void ict_Init(void)
 {
-
 	GPIO_InitTypeDef GPIO_InitStructure;
+	ADC_InitTypeDef ADC_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	DMA_InitTypeDef DMA_InitStructure;
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD|RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_ADCCLKConfig(RCC_PCLK2_Div8); /*72Mhz/8 = 9Mhz*/
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_12|GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_7|GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_10|GPIO_Pin_11;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	GPIOC->ODR=0x00000000;
-	GPIOA->ODR&=~BO_CTRL;					//扬声器断电
-	nxp_74hct595_Init(&chip);				//初始化移位寄存器
-	nxp_74hct595_WriteByte(&chip,0x00);			//七段数码管灭
-	GPIOA->ODR&=~NSS;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_6|GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	ADC_DeInit(ADC1);
+	ADC_StructInit(&ADC_InitStructure);
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_Init(ADC1, &ADC_InitStructure);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_71Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 2, ADC_SampleTime_71Cycles5); //9Mhz/(71.5 + 12.5) = 107.1Khz
+	ADC_Cmd(ADC1, ENABLE);
+
+	ADC_DeInit(ADC2);
+	ADC_StructInit(&ADC_InitStructure);
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_Init(ADC2, &ADC_InitStructure);
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_6, 1, ADC_SampleTime_71Cycles5);
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_7, 2, ADC_SampleTime_71Cycles5); //9Mhz/(71.5 + 12.5) = 107.1Khz
+	ADC_Cmd(ADC2, ENABLE);
+
+	ADC_AnalogWatchdogThresholdsConfig(ADC1, 0xCCC,0x000);
+	ADC_AnalogWatchdogSingleChannelConfig(ADC1, ADC_Channel_2);
+	ADC_AnalogWatchdogCmd(ADC1, ADC_AnalogWatchdog_SingleRegEnable);
+	ADC_ITConfig(ADC1, ADC_IT_AWD, DISABLE);
+
+	ADC_AnalogWatchdogThresholdsConfig(ADC2, 0xCCC,0x000);
+	ADC_AnalogWatchdogSingleChannelConfig(ADC2, ADC_Channel_7);
+	ADC_AnalogWatchdogCmd(ADC2, ADC_AnalogWatchdog_SingleRegEnable);
+	ADC_ITConfig(ADC2, ADC_IT_AWD, DISABLE);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = ADC1_2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	ADC_ResetCalibration(ADC1);
+	while (ADC_GetResetCalibrationStatus(ADC1));
+	ADC_StartCalibration(ADC1);
+	while (ADC_GetCalibrationStatus(ADC1));
+
+	ADC_ResetCalibration(ADC2);
+	while (ADC_GetResetCalibrationStatus(ADC2));
+	ADC_StartCalibration(ADC2);
+	while (ADC_GetCalibrationStatus(ADC2));
+
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+	ADC_SoftwareStartConvCmd(ADC2, ENABLE);
+
+	mbi5025_Init(&ict_mbi5025);
+	mbi5025_EnableOE(&ict_mbi5025);
+	dac_ch1.init(NULL);
+	dac_ch2.init(NULL);
 }
 
-void spc_Update(void)
+void ADC1_2_IRQHandler(void)
 {
-	if(((GPIOC->IDR&WLI_0)==WLI_0)&&(i1==1)) {
-		i2=i3=1;					//可以发令
-		i1=0;						//复位前不再有反应
-		GPIOC->ODR|=LED_GREEN;				//绿灯亮
-		GPIOC->ODR&=~LED_RED;				//红灯灭
-		GPIOA->ODR|=BO_CTRL;				//扬声器供电
-		GPIOC->ODR|=BO_2;				//发出准备声
-	}
-	else if(i3==1) {
-		if(a1==1) {
-			nxp_74hct595_WriteByte(&chip,0xda);	//显示5
-			d1=time_get(1000);			//d1取1s以后的值
-			d2=time_get(8000);			//给d2~d6赋值5s以后的一个值
-			d3=time_get(8000);
-			d4=time_get(8000);
-			d5=time_get(8000);
-			d6=time_get(8000);
-			}
-			a1=0;					//循环时不会执行上句程序
-			if((time_left(d1)<0)&&b1==1) {
-				GPIOC->ODR&=~BO_2;		//准备声消
-				GPIOA->ODR&=~BO_CTRL;
-				nxp_74hct595_WriteByte(&chip,0xcc);//显示4
-				d2=time_get(1000);
-				b1=0;
-			}
-			if((time_left(d2)<0)&&b2==1) {
-				nxp_74hct595_WriteByte(&chip,0x9e);//显示3
-				d3=time_get(1000);
-				b2=0;				//不会重复执行
-			}
-			if((time_left(d3)<0)&&b3==1) {
-				nxp_74hct595_WriteByte(&chip,0xb6);//显示2
-				d4=time_get(1000);
-				b3=0;
-			}
-			if((time_left(d4)<0)&&b4==1) {
-				nxp_74hct595_WriteByte(&chip,0x0c);//显示1
-				d5=time_get(1000);
-				b4=0;
-			}
-			if((time_left(d5)<0)&&b5==1) {
-				GPIOA->ODR|=BO_CTRL;
-				GPIOC->ODR|=BO_1;		//第二种声音，即报警声
-				GPIOC->ODR&=~LED_GREEN;		//绿灯灭
-				GPIOC->ODR|=LED_RED;		//红灯亮
-				nxp_74hct595_WriteByte(&chip,0xe2);//显示F
-				d6=time_get(4000);		//持续4S
-				b5=i2=0;
-			}
-			if((time_left(d6)<0)&&b6==1) {
-				GPIOA->ODR&=~BO_CTRL;
-				GPIOC->ODR&=~BO_1;		//声音消
-				i3=b6=0;			//循环不会重复执行
-			}
-		}
-	if((GPIOC->IDR&WLI_1)==WLI_1) {				//接收到犯规信号
-		nxp_74hct595_WriteByte(&chip,0xe2);		//显示F
-		GPIOC->ODR&=~LED_GREEN;				//绿灯灭
-		GPIOC->ODR|=LED_RED;				//红灯亮
-		GPIOA->ODR|=BO_CTRL;
-		GPIOC->ODR|=BO_1;				//报警声响
-		i1=i2=i3=0;					//不能启动,准备期间犯规，则取消准备状态
-		i4=1;						//下面else的语句可以执行了
-		b1=b2=b3=b4=b5=b6=0;				//同上注释，双重保护
-	}
-	else if(i4==1) {
-			if(a2==1)
-				d7=time_get(4000);
-			a2=0;
-			if(time_left(d7)<0) {			//持续4S
-				GPIOC->ODR&=~BO_1;		//报警声消
-				GPIOA->ODR&=~BO_CTRL;
-				i4=0;
-				a2=1;
-			}
-		}
-	if(((GPIOC->IDR&WLI_2)==WLI_2)&&(i2==1)) {		//接收到启动信号
-		GPIOA->ODR|=BO_CTRL;
-		GPIOC->ODR|=BO_0;				//启动声音响
-		i2=i3=0;					//准备期间犯规，则取消准备状态
-		i5=1;
-		b1=b2=b3=b4=b5=b6=0;				//同上注释，双重保护
-		GPIOC->ODR|=LED_GREEN;				//绿灯亮
-		GPIOC->ODR&=~LED_RED;
-	}
-	else if(i5==1) {
-			if(a3==1)
-				d8=time_get(1000);
-			a3=0;
-			if(time_left(d8)<0) {
-				GPIOC->ODR&=~BO_0;		//启动声持续1S
-				GPIOA->ODR&=~BO_CTRL;
-				i5=0;
-			}
-		}
-	if((GPIOC->IDR&WLI_3)==WLI_3) {				//接收到复位信号
-		i1=1;						//复位后可以按准备键
-		i2=i3=0;
-		a1=a2=a3=1;
-		b1=b2=b3=b4=b5=b6=1;
-		nxp_74hct595_WriteByte(&chip,0x00);
-		GPIOA->ODR&=~BO_CTRL;				//扬声器断电
-		GPIOC->ODR&=~LED_RED;				//红灯灭
-		GPIOC->ODR&=~LED_GREEN;				//绿灯灭
-		GPIOC->ODR&=~BO_2;
-		GPIOC->ODR&=~BO_1;
-		GPIOC->ODR&=~BO_0;
-		}
-	}
+	if(ADC_GetFlagStatus(ADC1, ADC_FLAG_AWD) == SET)
+		//set relay
+	else //set relay
+	ADC_ITConfig(ADC1, ADC_IT_AWD, DISABLE);
+	ADC_ITConfig(ADC2, ADC_IT_AWD, DISABLE);
+	ADC_ClearITPendingBit(ADC1, ADC_IT_AWD);
+	ADC_ClearITPendingBit(ADC2, ADC_IT_AWD);
+	//set relay
+}
+
+void ict_Update(void)
+{
+
+}
 
 void main(void)
 {
 
 	task_Init();
-	spc_Init();
+	ict_Init();
 	while(1) {
 		task_Update();
-		spc_Update();
 	}
 }
+
+static int cmd_ict_func(int argc, char *argv[])
+{
+	int temp = 0;
+	int i;
+
+	const char * usage = { \
+		" usage:\n" \
+		" ict relay xx xx xx ..., write value to set relay \n" \
+		" ict power_1 5123, set power_1 value 0~10000mv \n" \
+		" ict power_2 5123, set power_2 value 0~10000mv \n" \
+	};
+
+	if (argc > 1) {
+		if (argv[1][0] == 'r') {
+			for (i = 0; i < (argc - 2); i++) {
+				sscanf(argv[2+i], "%x", &temp);
+				mbi5025_WriteByte(&ict_mbi5025, temp);
+			}
+			printf("%d Bytes Write Successful!\n", argc-2);
+		}
+		if (argv[1][0] == 'p') {
+			if(atoi(argv[3]) < 0 || atoi(argv[3]) > 10)
+				printf("input error ,please input a value between 0 and 10!\n");
+			else {
+				if(argv[1][6] == '1')
+					dac_ch1.write(atoi(argv[3]) *(0xfff) /10000);
+				if(argv[1][6] == '2')
+					dac_ch2.write(atoi(argv[3]) *(0xfff) /10000);
+			}
+		}
+	}
+
+	if(argc < 2) {
+		printf(usage);
+		return 0;
+	}
+
+	return 0;
+}
+const cmd_t cmd_ict = {"ict", cmd_ict_func, "ict cmd"};
+DECLARE_SHELL_CMD(cmd_ict)
