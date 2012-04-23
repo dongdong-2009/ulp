207bbfa98628b8188841c0251c29f67a20d13778
peng.guo <peng.guo@linktronsys.com>
2012-04-23 11:39:07 +0800

    update for pdi j04n
    Signed-off-by: peng.guo <peng.guo@linktronsys.com>

--- src/tasks/pdi/J04N.c
+++ src/tasks/pdi/J04N.c
@@ -18,22 +18,16 @@
 
 #define PDI_DEBUG	1
 
-//pdi_B515 can msg
-static const can_msg_t b515_clrdtc_msg = {
-	0x737, 8, {0x03, 0x3b, 0x9f, 0xff, 0, 0, 0, 0}, 0
-};
-static const can_msg_t b515_errcode_msg = {
-	0x737, 8, {0x03, 0x22, 0xfd, 0x39, 0, 0, 0, 0}, 0
-};
-static const can_msg_t b515_getsn_msg = {
-	0x737, 8, {0x03, 0x22, 0xfd, 0x74, 0, 0, 0, 0}, 0
-};
-static const can_msg_t b515_reqseed_msg = {
-	0x737, 8, {0x02, 0x27, 0x61, 0, 0, 0, 0, 0}, 0
-};
-static const can_msg_t b515_start_msg = {
-	0x737, 8, {0x02, 0x10, 0x03, 0, 0, 0, 0, 0}, 0
-};
+//pdi_jn can msg
+static const can_msg_t jn_rst_msg =		{0x737, 8, {0x02, 0x11, 0x81, 0, 0, 0, 0, 0}, 0};
+static const can_msg_t jn_clrdtc_msg =	{0x737, 8, {0x05, 0x31, 0x01, 0xf0, 0x05, 0, 0, 0}, 0};
+static const can_msg_t jn_errcode_msg =	{0x737, 8, {0x03, 0x22, 0xfd, 0x39, 0, 0, 0, 0}, 0};
+static const can_msg_t jn_getsn_msg =	{0x737, 8, {0x03, 0x22, 0xfd, 0x3f, 0, 0, 0, 0}, 0};
+static const can_msg_t jn_reqseed_msg =	{0x737, 8, {0x02, 0x27, 0x61, 0, 0, 0, 0, 0}, 0};
+static const can_msg_t jn_start_msg1 =	{0x737, 8, {0x02, 0x10, 0x03, 0, 0, 0, 0, 0}, 0};
+static const can_msg_t jn_start_msg2 =	{0x737, 8, {0x02, 0x3E, 0x80, 0, 0, 0, 0, 0}, 0};
+static const can_msg_t jn_start_msg3 =	{0x737, 8, {0x02, 0x3E, 0x00, 0, 0, 0, 0, 0}, 0};
+
 
 static const mbi5025_t pdi_mbi5025 = {
 		.bus = &spi1,
@@ -44,38 +38,39 @@ static const mbi5025_t pdi_mbi5025 = {
 
 static const ls1203_t pdi_ls1203 = {
 		.bus = &uart2,
-		.data_len = 14,//长度没有定下来
+		.data_len = 12,//长度没有定下来
 		.dead_time = 20,
 };
 
 static const can_bus_t* pdi_can_bus = &can1;
 static can_msg_t pdi_msg_buf[32];		//for multi frame buffer
-static char b515_data_buf[256];			//data buffer
-static char b515_fault_buf[64];			//fault buffer
-static char bcode_1[14];
+static char jn_data_buf[256];			//data buffer
+static char jn_fault_buf[64];			//fault buffer
+static char bcode_1[16];
 
-static int b515_check();
-static int b515_init_OK();
-static int b515_clear_dtc();
-static int b515_mdelay(int );
-static int b515_StartSession();
-static int b515_check_bab515ode();
-static int b515_GetCID(short cid, char *data);
-static int b515_check_init(const struct pdi_cfg_s *);
-static int b515_GetFault(char *data, int * pnum_fault);
+static int jn_check();
+static int jn_init_OK();
+static int jn_clear_dtc();
+static int jn_mdelay(int );
+static int jn_StartSession();
+static int jn_check_barcode();
+static int jn_GetCID(short cid, char *data);
+static int jn_check_init(const struct pdi_cfg_s *);
+static int jn_GetFault(char *data, int * pnum_fault);
 static int pdi_pass_action();
 static int pdi_fail_action();
 static int pdi_led_start();
 static int target_noton_action();
 static int counter_pass_add();
 static int counter_fail_add();
-static void b515_process();
+static void jn_process();
+static void jn_CalculateKey(unsigned char , unsigned char parameter_1[8], unsigned char parameter_2[3]);
 
 /**************************************************************************/
 /************         Local funcitons                         *************/
 /**************************************************************************/
 
-static int b515_mdelay(int ms)
+static int jn_mdelay(int ms)
 {
 	int left;
 	time_t deadline = time_get(ms);
@@ -89,25 +84,99 @@ static int b515_mdelay(int ms)
 	return 0;
 }
 
+static void jn_CalculateKey(unsigned char accessLevel, unsigned char challenge[8], unsigned char secKeyOut[3])
+{
+	unsigned char idx, i;
+	unsigned char mask = 0x01, x = 0;
+	unsigned char kSecB2[2] = {0x00, 0x80};
+	unsigned char kSecC2[2] = {0x00, 0x10};
+	unsigned char kSecC1[2] = {0x00, 0x90};
+	unsigned char kSecC0[2] = {0x00, 0x28};
+	unsigned char secKey[3] = {0xA9, 0x41, 0xC5};
+
+	// Select the five fixed bytes
+	switch(accessLevel) {
+		case 0x01:
+			challenge[3] = 0x52;
+			challenge[4] = 0x6F;
+			challenge[5] = 0x77;
+			challenge[6] = 0x61;
+			challenge[7] = 0x6E;
+			break;
+
+		case 0x03:
+			challenge[3] = 0x5A;
+			challenge[4] = 0x89;
+			challenge[5] = 0xE4;
+			challenge[6] = 0x41;
+			challenge[7] = 0x72;
+			break;
+
+		case 0x61:
+			challenge[3] = 0x4D;
+			challenge[4] = 0x5A;
+			challenge[5] = 0x04;
+			challenge[6] = 0x68;
+			challenge[7] = 0x38;
+			break;
+	}
+
+	// Key calculation
+	for(i = 0; i < 64; i ++) {
+		if((mask & challenge[x]) !=0)
+			idx = 0x01;
+		else idx = 0x00;
+		idx ^= (secKey[0] & 0x01);
+		if(mask == 0x80) {
+			mask = 0x01;
+			x ++;
+		} else mask <<= 1;
+
+		secKey[0] >>= 1;
+		if((secKey[1] & 0x01) != 0)
+			secKey[0] |= 0x80;
+
+		secKey[1] >>= 1;
+		if((secKey[2] & 0x01) != 0)
+			secKey[1] |= 0x80;
+
+		secKey[2] >>= 1;
+		secKey[2] = (secKey[2] | kSecB2[idx]);
+		secKey[0] = (secKey[0] ^ kSecC0[idx]);
+		secKey[1] = (secKey[1] ^ kSecC1[idx]);
+		secKey[2] = (secKey[2] ^ kSecC2[idx]);
+	}
+
+	secKeyOut[0] = (secKey[0] >> 4);
+	secKeyOut[0] |= (secKey[1] << 4);
+
+	secKeyOut[1] = (secKey[2] >> 4);
+	secKeyOut[1] |= (secKey[1] & 0xF0);
+
+	secKeyOut[2] = (secKey[2] & 0x0F);
+	secKeyOut[2] |= (secKey[0] << 4);
+}
+
 //for start the session
-static int b515_StartSession(void)
+static int jn_StartSession(void)
 {
 	int i, msg_len, num_fault;
-	unsigned char seed[2], result;
+	unsigned char seed[8], sendkey[3];
 	can_msg_t msg;
 	can_msg_t sendkey_msg = {
 		0x737,
 		8,
-		{0x04, 0x27, 0x62, 0xff, 0xff, 0, 0, 0},
+		{0x05, 0x27, 0x62, 0xff, 0xff, 0xff, 0, 0},
 		0
 	};
-
-	if (usdt_GetDiagFirstFrame(&b515_start_msg, 1, NULL, &msg, &msg_len))		//start session
+	//for start the required session
+	if (usdt_GetDiagFirstFrame(&jn_start_msg, 1, NULL, &msg, &msg_len))		//start session
 		return 1;
 #if PDI_DEBUG
 	can_msg_print(&msg, "\n");
 #endif
-	if (usdt_GetDiagFirstFrame(&b515_reqseed_msg, 1, NULL, &msg, &msg_len))	//req seed
+	//send seed require msg and seed saved in &msg
+	if (usdt_GetDiagFirstFrame(&jn_reqseed_msg, 1, NULL, &msg, &msg_len))		//req seed
 		return 1;
 #if PDI_DEBUG
 	can_msg_print(&msg, "\n");
@@ -116,13 +185,14 @@ static int b515_StartSession(void)
 	//calculate the key from seed
 	seed[0] = (unsigned char)msg.data[3];
 	seed[1] = (unsigned char)msg.data[4];
-	result = seed[0] ^ seed[1];
-	result ^= 0x34;
-	result ^= 0xab;
-	sendkey_msg.data[3] = (char)((0x34ab + result) >> 8);
-	sendkey_msg.data[4] = (char)((0x34ab + result) & 0x00ff);
+	seed[2] = (unsigned char)msg.data[5];
+	jn_CalculateKey(0x61, seed, sendkey);
+	sendkey_msg.data[3] = sendkey[0];
+	sendkey_msg.data[4] = sendkey[1];
+	sendkey_msg.data[5] = sendkey[2];
 
-	if (usdt_GetDiagFirstFrame(&sendkey_msg, 1, NULL, &msg, &msg_len))		//send key
+	//send keys
+	if (usdt_GetDiagFirstFrame(&sendkey_msg, 1, NULL, &msg, &msg_len))
 		return 1;
 	//judge the send key response
 	if ((msg.data[1] != 0x67) || (msg.data[2] != 0x62))
@@ -131,10 +201,10 @@ static int b515_StartSession(void)
 	can_msg_print(&msg, "\n");
 #endif
 
-#if 1
+#if PDI_DEBUG
 	//get serial number
 	printf("\nSN Code:\n");
-	usdt_GetDiagFirstFrame(&b515_getsn_msg, 1, NULL, pdi_msg_buf, &msg_len);
+	usdt_GetDiagFirstFrame(&jn_getsn_msg, 1, NULL, pdi_msg_buf, &msg_len);
 	if (msg_len > 1)
 		usdt_GetDiagLeftFrame(pdi_msg_buf, msg_len);
 	for (i = 0; i < msg_len; i++)
@@ -142,33 +212,24 @@ static int b515_StartSession(void)
 
 	// get error code
 	printf("\nError Code:\n");
-	usdt_GetDiagFirstFrame(&b515_errcode_msg, 1, NULL, pdi_msg_buf, &msg_len);
+	usdt_GetDiagFirstFrame(&jn_errcode_msg, 1, NULL, pdi_msg_buf, &msg_len);
 	if (msg_len > 1)
 		usdt_GetDiagLeftFrame(pdi_msg_buf, msg_len);
 	for (i = 0; i < msg_len; i++)
 		can_msg_print(pdi_msg_buf + i, "\n");
 
-	//tester point
-	printf("\nConnector Bar:\n");
-	usdt_GetDiagFirstFrame(&b515_connector_msg, 1, NULL, pdi_msg_buf, &msg_len);
-	if (msg_len > 1)
-		usdt_GetDiagLeftFrame(pdi_msg_buf, msg_len);
-	for (i = 0; i < msg_len; i++)
-		can_msg_print(pdi_msg_buf + i, "\n");
-
-	if (b515_GetFault(b515_fault_buf, &num_fault))
+	if (jn_GetFault(jn_fault_buf, &num_fault))
 		printf("##ERROR##\n");
 	else {
 		printf("##OK##\n");
 		printf("num of fault is: %d\n", num_fault);
 		for (i = 0; i < num_fault*3; i += 3)
-			printf("0x%2x, 0x%2x, 0x%2x\n", b515_fault_buf[i]&0xff, b515_fault_buf[i+1]&0xff, b515_fault_buf[i+2]&0xff);
+			printf("0x%2x, 0x%2x, 0x%2x\n", jn_fault_buf[i]&0xff, jn_fault_buf[i+1]&0xff, jn_fault_buf[i+2]&0xff);
 	}
 
-	//clear all
+	//clear all include error code
 	printf("\nClear all:\n");
-	if (usdt_GetDiagFirstFrame(&b515_clrdtc_msg, 1, NULL, &msg, &msg_len))	//req seed
-		return 1;
+	usdt_GetDiagFirstFrame(&jn_clrdtc_msg, 1, NULL, &msg, &msg_len);
 	can_msg_print(&msg, "\n");
 
 #endif
@@ -176,7 +237,7 @@ static int b515_StartSession(void)
 	return 0;
 }
 
-static int b515_GetCID(short cid, char *data)
+static int jn_GetCID(short cid, char *data)
 {
 	can_msg_t msg_res, pdi_send_msg = {0x737, 8, {0x03, 0x22, 0, 0, 0, 0, 0, 0}, 0};
 	int i = 0, msg_len;
@@ -191,12 +252,11 @@ static int b515_GetCID(short cid, char *data)
 			return 1;
 	}
 
-	//pick up the data
+	//pick up datas
 	if (msg_len == 1) {
 		if (msg_res.data[1] == 0x62)
 			memcpy(data, (msg_res.data + 4), msg_res.data[0] - 3);
-		else
-			return 1;
+		else return 1;
 	} else if (msg_len > 1) {
 		memcpy(data, (msg_res.data + 5), 3);
 		data += 3;
@@ -209,23 +269,23 @@ static int b515_GetCID(short cid, char *data)
 	return 0;
 }
 
-static int b515_init_OK()
+static int jn_init_OK()
 {
 	led_on(LED_RED);
 	led_on(LED_GREEN);
 	beep_on();
-	b515_mdelay(200);
+	jn_mdelay(200);
 	led_off(LED_RED);
 	led_off(LED_GREEN);
 	beep_off();
-	b515_mdelay(100);
+	jn_mdelay(100);
 	for(int i = 0; i < 5; i++) {
 		led_on(LED_RED);
 		led_on(LED_GREEN);
-		b515_mdelay(200);
+		jn_mdelay(200);
 		led_off(LED_RED);
 		led_off(LED_GREEN);
-		b515_mdelay(100);
+		jn_mdelay(100);
 	}
 	return 0;
 }
@@ -233,7 +293,7 @@ static int b515_init_OK()
 static int counter_pass_add()
 {
 	counter_pass_rise();
-	b515_mdelay(40);
+	jn_mdelay(40);
 	counter_pass_down();
 	return 0;
 }
@@ -241,7 +301,7 @@ static int counter_pass_add()
 static int counter_fail_add()
 {
 	counter_fail_rise();
-	b515_mdelay(40);
+	jn_mdelay(40);
 	counter_fail_down();
 	return 0;
 }
@@ -253,7 +313,7 @@ static int pdi_fail_action()
 	led_on(LED_RED);
 	counter_fail_add();
 	beep_on();
-	b515_mdelay(3000);
+	jn_mdelay(2000);
 	beep_off();
 	return 0;
 }
@@ -264,14 +324,14 @@ static int pdi_pass_action()
 	led_off(LED_RED);
 	led_on(LED_GREEN);
 	beep_on();
-	b515_mdelay(20);
+	jn_mdelay(20);
 	printf("##START##EC-Test Result : No Error ##END##\n");
 	counter_pass_add();
-	b515_mdelay(1000);
+	jn_mdelay(800);
 	beep_off();
-	b515_mdelay(200);
+	jn_mdelay(150);
 	beep_on();
-	b515_mdelay(1000);
+	jn_mdelay(800);
 	beep_off();
 	return 0;
 }
@@ -280,23 +340,23 @@ static int target_noton_action()
 {
 	led_off(LED_GREEN);
 	led_off(LED_RED);
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; i ++) {
 		beep_on();
 		led_on(LED_GREEN);
 		led_on(LED_RED);
-		b515_mdelay(200);
+		jn_mdelay(200);
 		beep_off();
 		led_off(LED_GREEN);
 		led_off(LED_RED);
-		b515_mdelay(100);
+		jn_mdelay(100);
 	}
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; i ++) {
 		led_on(LED_GREEN);
 		led_on(LED_RED);
-		b515_mdelay(200);
+		jn_mdelay(200);
 		led_off(LED_GREEN);
 		led_off(LED_RED);
-		b515_mdelay(100);
+		jn_mdelay(100);
 	}
 	return 0;
 }
@@ -311,7 +371,7 @@ static int pdi_led_start()
 	return 0;
 }
 
-static int b515_check_init(const struct pdi_cfg_s *sr)
+static int jn_check_init(const struct pdi_cfg_s *sr)
 {
 	char *o = (char *)&(sr -> relay_ex);
 	mbi5025_WriteByte(&pdi_mbi5025, *(o+3));
@@ -328,39 +388,41 @@ static int b515_check_init(const struct pdi_cfg_s *sr)
 	return 0;
 }
 
-static int b515_check_bab515ode()
+static int jn_check_barcode()
 {
-	b515_GetCID(0xfd47, b515_data_buf);
+	jn_GetCID(0xfd3f, jn_data_buf);
 
-	if (memcmp(b515_data_buf, bcode_1, 19))
+	if (memcmp(jn_data_buf, bcode_1, 16))
 		return 1;
 
 	return 0;
 }
 
-static int b515_clear_dtc(void)
+static int jn_clear_dtc(void)
 {
 	int msg_len;
 	can_msg_t msg;
 
-	if (usdt_GetDiagFirstFrame(&b515_clrdtc_msg, 1, NULL, &msg, &msg_len))
+	if (usdt_GetDiagFirstFrame(&jn_start_msg3, 1, NULL, &msg, &msg_len))
 		return 1;
-	if (msg.data[1] != 0x7b)	//positive response is 0x54
-		return 0;
+	if (usdt_GetDiagFirstFrame(&jn_clrdtc_msg, 1, NULL, &msg, &msg_len))
+		return 1;
+	if (msg.data[1] != 0x71)	//positive response is 0x54
+		return 1;
 
-	return 1;
+	return 0;
 }
 
-static int b515_GetFault(char *data, int * pnum_fault)
+static int jn_GetFault(char *data, int * pnum_fault)
 {
 	int i, result = 0;
 
-	if(b515_GetCID(0xfd39, data))
+	if(jn_GetCID(0xfd39, data))
 		return 1;
 
-	memset(data + 117, 0x00, 10);
+	memset(data + 66, 0x00, 10);
 
-	for (i = 0; i < 117; i += 3) {
+	for (i = 0; i < 66; i += 3) {
 		if (data[i] | data[i+1] | data[i+2])
 			result ++;
 	}
@@ -370,25 +432,20 @@ static int b515_GetFault(char *data, int * pnum_fault)
 	return 0;
 }
 
-static int b515_check()
+static int jn_check()
 {
 	int i, num_fault, try_times = 5;
 
 	pdi_IGN_on();
 
-	b515_mdelay(10000);
+	jn_mdelay(10000);
 
-	if(b515_check_bab515ode()) {
+	if(jn_check_barcode()) {
 		printf("##START##EC-Barcode Wrong##END##\n");
 		return 1;
 	}
 
-	if(b515_connector_check()) {
-		printf("##START##EC-Connecter Bar Wrong##END##\n");
-		return 1;
-	}
-
-	while (b515_GetFault(b515_fault_buf, &num_fault)) {
+	while (jn_GetFault(jn_fault_buf, &num_fault)) {
 		try_times --;
 		if (try_times < 0) {
 			printf("##START##EC-read DTC error##END##\n");
@@ -397,11 +454,11 @@ static int b515_check()
 	}
 
 	if (num_fault) {
-		//b515_clear_dtc();
+		//jn_clear_dtc();
 		printf("##START##EC-");
 		printf("num of fault is: %d*", num_fault);
 		for (i = 0; i < num_fault*3; i += 3)
-			printf("0x%2x, 0x%2x, 0x%2x*", b515_fault_buf[i]&0xff, b515_fault_buf[i+1]&0xff, b515_fault_buf[i+2]&0xff);
+			printf("0x%2x, 0x%2x, 0x%2x\n", jn_fault_buf[i]&0xff, jn_fault_buf[i+1]&0xff, jn_fault_buf[i+2]&0xff);
 		printf("##END##\n");
 		return 1;
 	}
@@ -415,18 +472,18 @@ void pdi_init(void)
 		.baud = 500000,
 	};
 
-	pdi_drv_Init();//led,beep
-	mbi5025_Init(&pdi_mbi5025);//SPI总线	移位寄存器
+	pdi_drv_Init();					//led,beep
+	mbi5025_Init(&pdi_mbi5025);		//SPI总线	移位寄存器
 	mbi5025_EnableOE(&pdi_mbi5025);
-	ls1203_Init(&pdi_ls1203);//scanner
+	ls1203_Init(&pdi_ls1203);		//scanner
 	pdi_can_bus->init(&cfg_pdi_can);
 	usdt_Init(pdi_can_bus);
 }
 
-static void b515_process(void)
+static void jn_process(void)
 {
 	const struct pdi_cfg_s* pdi_cfg_file;
-	char bcode[20];				//TBD
+	char bcode[20];
 	if(target_on())
 		start_botton_on();
 	else start_botton_off();
@@ -434,13 +491,13 @@ static void b515_process(void)
 
 		start_botton_off();
 		pdi_led_start();
-		bcode[14] = '\0';
+		bcode[16] = '\0';
 
-		memcpy(bcode_1, bcode, 14);
+		memcpy(bcode_1, bcode, 16);
 		printf("##START##SB-");
 		printf(bcode,"\0");
 		printf("##END##\n");
-		bcode[9] = '\0';		//TBD
+		bcode[5] = '\0';
 
 		pdi_cfg_file = pdi_cfg_get(bcode);
 
@@ -449,14 +506,12 @@ static void b515_process(void)
 				pdi_fail_action();
 				printf("##START##EC-No This Config File##END##\n");
 			}
-			b515_check_init(pdi_cfg_file);		//relay config
-			if(b515_check() == 0)
-				pdi_pass_action();
-			else
-				pdi_fail_action();
+			jn_check_init(pdi_cfg_file);		//relay config
+			if(jn_check() == 0) pdi_pass_action();
+			else pdi_fail_action();
 		} else {
 			target_noton_action();
-			printf("##START##EC-target is not on the right position##END##\n");
+			printf("##START##EC-target is not on the right position or the pin protector is not placed##END##\n");
 		}
 	}
 }
@@ -465,24 +520,23 @@ int main(void)
 {
 	ulp_init();
 	pdi_init();
-	b515_init_OK();
+	jn_init_OK();
 	while(1) {
-		b515_process();
+		jn_process();
 		ulp_update();
 	}
 }
 
-#if 1
-static int cmd_b515_func(int argc, char *argv[])
+static int cmd_jn_func(int argc, char *argv[])
 {
 	int num_fault, i;
 
 	const char *usage = {
-		"b515 , usage:\n"
-		"b515 fault\n"
-		"b515 clear\n"
-		"b515 batt on/off\n"
-		"b515 start, start the diagnostic seesion\n"
+		"jn , usage:\n"
+		"jn fault\n"
+		"jn clear\n"
+		"jn batt on/off\n"
+		"jn start, start the diagnostic seesion\n"
 	};
 
 	if (argc < 2) {
@@ -492,25 +546,19 @@ static int cmd_b515_func(int argc, char *argv[])
 
 	if(argc == 2) {
 		if(argv[1][0] == 'f') {
-			if (b515_GetFault(b515_fault_buf, &num_fault))
+			if (jn_GetFault(jn_fault_buf, &num_fault))
 				printf("##ERROR##\n");
 			else {
 				printf("##OK##\n");
 				printf("num of fault is: %d\n", num_fault);
-				for (i = 0; i < num_fault*2; i += 2)
-					printf("0x%2x, 0x%2x\n", b515_fault_buf[i]&0xff, b515_fault_buf[i+1]&0xff);
+				for (i = 0; i < num_fault*3; i += 3)
+					printf("0x%2x, 0x%2x, 0x%2x\n", jn_fault_buf[i]&0xff, jn_fault_buf[i+1]&0xff, jn_fault_buf[i+2]&0xff);
 			}
-		}
-		if(argv[1][0] == 'c') {
-			if (b515_clear_dtc())
-				printf("##ERROR##\n");
-			else
-				printf("##OK##\n");
 		}
 
 		// start the diagnostic session
 		if(argv[1][0] == 's') {
-			b515_StartSession();
+			jn_StartSession();
 		}
 	}
 
@@ -526,6 +574,34 @@ static int cmd_b515_func(int argc, char *argv[])
 	return 0;
 }
 
-const cmd_t cmd_b515 = {"b515", cmd_b515_func, "b515 cmd i/f"};
-DECLARE_SHELL_CMD(cmd_b515)
-#endif
+const cmd_t cmd_jn = {"jn", cmd_jn_func, "jn cmd i/f"};
+DECLARE_SHELL_CMD(cmd_jn)
+
+static int cmd_pdi_func(int argc, char *argv[])
+{
+	const char *usage = {
+		"pdi , usage:\n"
+		"pdi clear		clear the error code\n"
+	};
+
+	if (argc < 2) {
+		printf("%s", usage);
+		return 0;
+	}
+	//clear error code
+	if(argc == 2) {
+		if(argv[1][0] == 'c') {
+			pdi_IGN_on();
+			jn_mdelay(4000);
+			jn_StartSession();
+			if(jn_clear_dtc())
+				printf("##ERROR##\n");
+			else printf("##OK##\n");
+			pdi_IGN_off();
+		}
+	}
+
+	return 0;
+}
+const cmd_t cmd_pdi = {"pdi", cmd_pdi_func, "pdi cmd i/f"};
+DECLARE_SHELL_CMD(cmd_pdi)
