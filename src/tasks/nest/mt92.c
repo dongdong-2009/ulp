8223529f278479156a0bec9bab4bd7cf0bfe512a
feng.ni <feng.ni@linktronsys.com>
2012-05-28 13:49:40 +0800

    [mt92] update
    - add mt92's delay, delay is very important in mt92 :(
    - do not write in case of CAN_FAIL
    
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/tasks/nest/mt92.c
+++ src/tasks/nest/mt92.c
@@ -280,12 +280,12 @@ static int Read_Memory(int addr, char *data, int n)
 	if(fail)
 		return -1;
 
-	nest_mdelay(100);
+	nest_mdelay(300);
 	fail = mcamos_execute_ex(VECTOR_FLASH_READ);
 	if(fail)
 		return -2;
 
-	nest_mdelay(100);
+	nest_mdelay(300);
 
 	//dummy read
 	memset(mailbox.bytes, 0, sizeof(mailbox));
@@ -293,7 +293,7 @@ static int Read_Memory(int addr, char *data, int n)
 	if(fail)
 		return -3;
 
-	nest_mdelay(100);
+	nest_mdelay(300);
 	fail = mcamos_upload_ex(OUTBOX_ADDR + sizeof(mailbox.outbox), data, n);
 	dump(addr, data, n);
 	return (fail) ? -4 : 0;
@@ -469,6 +469,7 @@ void TestStart(void)
 	nest_mdelay(100);
 	mcamos_upload_ex(OUTBOX_ADDR + sizeof(mailbox.outbox), mailbox.bytes, 32);
 	nest_message("DUT SW: %s\n", mailbox.bytes);
+	nest_mdelay(300);
 
 	if(0) {
 		static const char mfg_data_dk277130[] = { //4 cyl
@@ -507,7 +508,7 @@ void TestStart(void)
 	if(!nest_ignore(BMR)) {
 		bmr = nest_map(bmr_map, mfg_data.bmr);
 		if(bmr < 0) {
-			nest_error_set(MTYPE_FAIL, "Model Type");
+			nest_error_set(CAN_FAIL, "Model Type");
 			return;
 		}
 	}
@@ -545,7 +546,8 @@ void TestStart(void)
 
 void TestStop(void)
 {
-	int fail = 0;
+	int fail;
+	struct nest_error_s *err = nest_error_get();
 	nest_message("#Test Stop\n");
 
 	//store nest_psv & nest_nec
@@ -553,11 +555,14 @@ void TestStop(void)
 		mfg_data.nest_psv |= 0x80;
 	else
 		mfg_data.nest_psv &= 0x7F;
-	mfg_data.nest_nec = nest_error_get() -> nec;
+	mfg_data.nest_nec = err->nec;
 	//for(int i = 0; i < sizeof(mfg_data); i ++) *((char *)&mfg_data + i) = i;
-	fail += Write_Memory(MFGDAT_ADDR, (void *)&mfg_data, sizeof(mfg_data));
-	if(fail)
-		nest_error_set(CAN_FAIL, "Eeprom Write");
+
+	if(err->nec != CAN_FAIL) {
+		fail += Write_Memory(MFGDAT_ADDR, (void *)&mfg_data, sizeof(mfg_data));
+		if(fail)
+			nest_error_set(CAN_FAIL, "Eeprom Write");
+	}
 
 	//send FIS BCMP(build complete) message
 	//fisBCMP();
