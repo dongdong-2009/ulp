6598ceca7175dfd684df2a4e339c1b636b472472
feng.ni <feng.ni@linktronsys.com>
2012-06-13 14:38:47 +0800

    [ybs_repeater] update dc module
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/tasks/ybs_repeater/ybs_dc.c
+++ src/tasks/ybs_repeater/ybs_dc.c
@@ -11,7 +11,7 @@ static struct dc_fifo_s
 	/*for dc_save/dc_read*/
 	int wn;
 	int rn;
-	short *data[DC_FIFO_SZ];
+	short *data[DC_FIFO_NP];
 	time_t time;
 
 	/*for dc_keep usage*/
@@ -20,6 +20,7 @@ static struct dc_fifo_s
 	int keep_start;
 	int keep_end;
 } dc_fifos[DC_CH], *dc_fifo;
+#define DC_FIFO_SZ (sizeof(struct dc_fifo_s))
 
 static int sect_empty_nr;
 static struct list_head queue_empty;
@@ -30,7 +31,7 @@ static struct list_head queue_ybs[DC_CH];
 #define sect_get(idx) (struct dc_sect_s *)(DC_BASE + DC_SECT_SZ * idx)
 static inline void sect_init(struct dc_sect_s *sect)
 {
-	sect->offset = sizeof(struct dc_sect_s);
+	sect->offset = 0;
 	INIT_LIST_HEAD(&sect->list_empty);
 	INIT_LIST_HEAD(&sect->list_keep);
 	INIT_LIST_HEAD(&sect->list_temp);
@@ -39,9 +40,8 @@ static inline void sect_init(struct dc_sect_s *sect)
 	INIT_LIST_HEAD(&sect->list_hash);
 }
 
-static int __hash(struct dc_sect_s *sect)
+static int __hash(int key)
 {
-	int key = sect->time + sect->ybs;
 	key = key % DC_SECT_NR;
 	return key;
 }
@@ -82,42 +82,42 @@ static int __save(int ch)
 	short *dst;
 
 	//to ensure atom ops, to avoid irq occurs when we are reading ..
-	memcpy(&fifo, &dc_fifos[ch], sizeof(struct dc_fifo_s));
-	if(memcmp(&fifo, &dc_fifos[ch], sizeof(struct dc_fifo_s))) {
-		memcpy(&fifo, &dc_fifos[ch], sizeof(struct dc_fifo_s));
+	memcpy(&fifo, &dc_fifos[ch], DC_FIFO_SZ);
+	if(memcmp(&fifo, &dc_fifos[ch], DC_FIFO_SZ)) {
+		memcpy(&fifo, &dc_fifos[ch], DC_FIFO_SZ);
 	}
 
 	time_t time = fifo.time;
 	words = fifo.wn - fifo.rn;
-	words += (words < 0) ? DC_FIFO_SZ : 0;
+	words += (words < 0) ? DC_FIFO_NP : 0;
 	for(i = 0; i < words; i ++) {
 		//always mv tension data from fifo to sectors
 		sect = list_first_entry(&queue_ybs[dc_ch]);
-		n = (DC_SECT_SZ - sect->offset) >> 1;
+		n = DC_SECT_NP - sect->offset;
 		n = (n < words) ? n : words;
-		dst = (short *) ((int)sect + sect->offset);
+		dst = &sect->data[sect->offset];
 		for(j = 0; j < n; j ++) {
 			ofs = fifo.rn + i + j;
-			ofs -= (ofs >= DC_FIFO_SZ) ? DC_FIFO_SZ : 0;
+			ofs -= (ofs >= DC_FIFO_NP) ? DC_FIFO_NP : 0;
 			*dst = fifo.data[ofs];
 		}
 
-		sect->offset += (n << 1);
+		sect->offset += n;
 		words -= n;
 		i += n;
 
-		if(sect->offset + 2 >= DC_SECT_SZ) {
+		if(sect->offset >= DC_SECT_NP) {
 			sect = list_first_entry(&queue_empty);
 			list_del(&sect->list_empty);
 			sect_empty_nr --;
 			sect->ch = ch;
-			sect->time = time - DC_SAVE_MS * (words - i - 1);
+			sect->time = time - DC_SAVE_MS * (words - i);
 			list_add_tail(&queue_temp, &sect->list_temp);
 			list_add_tail(&queue_ybs[dc_ch], &sect->list_ybs);
 		}
 	}
 	n = fifo.rn + words;
-	dc_fifos[ch].rn = (n >= DC_FIFO_SZ) ? (n - DC_FIFO_SZ) : n;
+	dc_fifos[ch].rn = (n >= DC_FIFO_NP) ? (n - DC_FIFO_NP) : n;
 	return words;
 }
 
@@ -130,19 +130,19 @@ static int __keep(int ch)
 	int p, words, ps, pe, nr;
 
 	//to ensure atom ops, to avoid irq occurs when we are reading ..
-	memcpy(&fifo, &dc_fifos[ch], sizeof(struct dc_fifo_s));
-	if(memcmp(&fifo, &dc_fifos[ch], sizeof(struct dc_fifo_s))) {
-		memcpy(&fifo, &dc_fifos[ch], sizeof(struct dc_fifo_s));
+	memcpy(&fifo, &dc_fifos[ch], DC_FIFO_SZ);
+	if(memcmp(&fifo, &dc_fifos[ch], DC_FIFO_SZ)) {
+		memcpy(&fifo, &dc_fifos[ch], DC_FIFO_SZ);
 	}
 
 	if(fifo.keep_ms_i <= fifo.keep_ms_e) //no keep request
 		return 0;
 
-	//warnning: the time diff is ignored here, it should be a big problem ..
+	//warnning: the time diff is ignored here, it should not be a big problem ..
 	p = nr = 0;
 	list_for_each_prev(pos, &queue_ybs[ch]) {
 		sect = list_entry(pos, dc_sect_s, list_ybs);
-		words = (sect->offset - sizeof(struct dc_sect_s)) >> 1;
+		words = sect->offset;
 		ps = p - words;
 		pe = p;
 		if(((ps > fifo.keep_start) && (ps < fifo.keep_end)) || \
@@ -153,7 +153,7 @@ static int __keep(int ch)
 			nr ++;
 
 			//hash search support
-			int idx = __hash(sect);
+			int idx = __hash(sect->time / DC_HASH_MS);
 			dc_sect_s *hash = sect_get(idx);
 			list_add_tail(&hash->queue_hash, &sect->list_hash);
 		}
@@ -227,7 +227,7 @@ int dc_save(short tension)
 	int n = dc_fifo->wn;
 	dc_fifo->data[n] = tension;
 	n ++;
-	n = (n >= DC_FIFO_SZ) ? 0 : n;
+	n = (n >= DC_FIFO_NP) ? 0 : n;
 	dc_fifo->wn = n;
 	dc_fifo->time = time_get(0);
 	sys_assert(n != dc_fifo->rn); //fifo overflow???
@@ -238,7 +238,7 @@ int dc_read(int offset)
 {
 	int n = dc_fifo->wn;
 	n += offset;
-	n += (n < 0) ? DC_FIFO_SZ : 0;
+	n += (n < 0) ? DC_FIFO_NP : 0;
 	return dc_fifo->data[n];
 }
 
@@ -254,38 +254,119 @@ int dc_keep(int start, int end)
 	return 0;
 }
 
-struct dc_sect_s *__search(int ybs, time_t time)
+struct dc_sect_s *__search(int ch, time_t time)
 {
+	dc_sect_s *hash, *sect;
+	int idx = __hash(sect->time / DC_HASH_MS);
+	hash = sect_get(idx);
+	list_for_each_entry(sect, &hash->queue_hash, list_hash) {
+		if(sect->ybs == ch) {
+			return sect;
+		}
+	}
+	return NULL;
+}
+
+static const short dummy[] = {
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+	DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA, DC_DATA_NA,
+};
+
+int dc_send_tcp_dummy(struct tcp_pcb *pcb, int nwords)
+{
+	int n;
+	#define DUMMY_NP (sizeof(dummy) / 2)
+	for(; nwords > 0; nwords -= DUMMY_NP) {
+		n = (nwords > DUMMY_NP) ? DUMMY_NP : nwords;
+		tcp_write(pcb, dummy, n << 1, TCP_WRITE_FLAG_MORE);
+	}
+	return 0;
 }
 
 int dc_send_tcp(struct tcp_pcb *pcb, int ch, time_t start, time_t end);
 {
-	struct dc_sect_s *sect;
-	int ms, n = 0, bytes;
+	struct dc_sect_s *sect, *sect_prev;
+	int ms, n = 0, words_sent, words;
 	time_t time;
 
-	bytes = time_diff(end, start) / DC_SAVE_MS;
-	if((bytes <= 0) || (bytes >= DC_SEND_TCP_LIMIT)) {
+	words = time_diff(end, start) / DC_SAVE_MS;
+	if((words <= 0) || (words >= DC_SEND_TCP_LIMIT)) {
 		assert(0);
 		return -1;
 	}
 
-	time = time_shift(time, -DC_SECT_MS);
+	/*note: time is not the para start!!!*/
+	time = time_shift(start, -DC_SECT_MS);
 	while(time_diff(end, time) > 0) {
 		sect = __search(ch, time);
 		if(sect == NULL) { //not found
 			time = time_shift(time, DC_HASH_MS);
 			continue;
 		}
-
-		//found the sect :) walk through the ybs chain
-		struct list_head *pos = &sect->list_ybs;
-		for (; pos != &queue_ybs[ch]; pos = pos->next) {
-			sect = list_entry(pos, dc_sect_s, list_ybs);
-			ms = time_diff(start, sect->time);
-			if(ms < 0) { //start is earlier than sect->time, insert empty data
-			}
-
-		}
 	}
+
+	if(sect == NULL) { //not found, send dummy data
+		dc_send_tcp_dummy(pcb, words);
+		return words;
+	}
+
+	//found the sect :) walk through the ybs chain
+	words_sent = 0;
+	sect_prev = NULL;
+	struct list_head *pos = &sect->list_ybs;
+	for (; pos != &queue_ybs[ch]; pos = pos->next) {
+		sect = list_entry(pos, dc_sect_s, list_ybs);
+		time = time_shift(sect->time, DC_SECT_MS); //shift time to sect tail
+		ms = time_diff(time, start);
+		if(ms > 0) {
+			//current sect is earlier than the start time, try next ..
+			continue;
+		}
+
+		if(sect_prev != NULL) {
+			ms = time_diff(sect->time, sect->prev_time);
+			ms -= DC_SECT_MS;
+			if(ms > 0) { //these two sector is discontinuous on time
+				n = ms / DC_SAVE_MS;
+				dc_send_tcp_dummy(pcb, n);
+				words_sent += n;
+			}
+		}
+
+		sect_prev = sect;
+
+		ms = time_diff(start, sect->time);
+		if(ms > 0) {
+			//current sect is later than the start time, insert dummy data first...
+			n = ms / DC_SAVE_MS;
+			dc_send_tcp_dummy(pcb, n);
+			words_sent += n;
+		}
+
+		n = (ms < 0) ? (- ms / DC_SAVE_MS) : 0;
+		short *data = &sect->data[n];
+		n = DC_SECT_NP - n;
+		n = (n > (words - words_sent)) ? (words - words_sent) : n;
+		tcp_write(pcb, data, n * sizeof(short), TCP_WRITE_FLAG_MORE);
+		words_sent += n;
+		if(words_sent >= words)
+			break;
+	}
+
+	return words_sent;
 }
