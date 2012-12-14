ac9d902541ddb198c4626e26fd1ef7fd43b020d9
feng.ni <feng.ni@linktronsys.com>
2012-12-14 18:37:31 +0800

    gsm demo program can send\recv short message now!!!
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/tasks/misc/gsm.c
+++ src/tasks/misc/gsm.c
@@ -7,6 +7,11 @@
 #include "led.h"
 #include "key.h"
 #include "stm32f10x.h"
+#include "shell/shell.h"
+#include "console.h"
+#include <string.h>
+
+#define gsm_cnsl ((struct console_s *) &uart1)
 
 /*led1-4 for key event display
 when key1 is pressed, led1 will be turned off.
@@ -32,6 +37,13 @@ void led_init(void)
 #define led_set_status(led, on) do { \
 	 GPIOB->BSRR = (1 << (led_pinmap[led] + ((on) ? 0 : 16))); \
 } while(0)
+
+void led_set_binary(int value)
+{
+	for(int i = 0; i < 8; i ++) {
+		led_set_status(i + 1, value & (1 << i));
+	}
+}
 
 /*gpio keyboard hardware driver*/
 int key_hwinit(void)
@@ -69,12 +81,115 @@ int key_hwgetvalue(void)
 	return value;
 }
 
+static char gsm_line[128];
+const char phone_nr[] = "+8615062360545";//"+8615250499838"; //; //;//
 
-void gsm_mdelay(int ms)
+static void gsm_flush(void)
 {
+	while(console_IsNotEmpty()) {
+		console_getchar();
+	}
+}
+
+static void gsm_send(const char *cmd)
+{
+	gsm_flush();
+	printf("%s\n", cmd);
+	mdelay(500);
+}
+
+static int gsm_read(char *echo, int ms)
+{
+	int result = -1;
 	time_t deadline = time_get(ms);
 	while(time_left(deadline) > 0) {
-		sys_update();
+		if(shell_ReadLine(NULL, echo)) {
+			if(echo[0] != 0) { //to avoid an empty line
+				result = 0;
+				break;
+			}
+		}
+	}
+	return result;
+}
+
+static int gsm_msg_send(const char *phone_nr, const char *text)
+{
+	int result = -1;
+	sprintf(gsm_line, "AT+CMGS=\"%s\"", phone_nr);
+	gsm_send(gsm_line);
+	gsm_flush();
+	sprintf(gsm_line, "%s\x1a\x1a\x1a", text);
+	gsm_send(gsm_line);
+	if(!gsm_read(gsm_line, 5000)) {
+		int matched, bytes = 0;
+		matched = sscanf(gsm_line, "+CMGS: %d", &bytes);
+		if(matched == 1) {
+			printf("GSM: %d bytes has been sent successfully!!!\n", bytes);
+			gsm_read(gsm_line, 500);
+			result ++;
+		}
+	}
+	return result;
+}
+
+void gsm_led_update(void)
+{
+	int led, result;
+	gsm_send("AT+CMGL=\"ALL\"");
+	while(!gsm_read(gsm_line, 500)) {
+		if(!strcmp(gsm_line, "ERROR"))
+			break;
+		if(!strcmp(gsm_line, "OK"))
+			break;
+
+		if(!strncmp(gsm_line, "led=", 4)) {
+			result = sscanf(gsm_line, "led=%d", &led);
+			if(result == 1) {
+				led_set_binary(led);
+			}
+		}
+	}
+}
+
+int gsm_init(void)
+{
+	int result = -2;
+	gsm_send("ATE 0");
+	for(int i = 0; i < 3; i ++) {
+		gsm_send("AT");
+		if(!gsm_read(gsm_line, 500)) {
+			if(!strcmp(gsm_line, "OK")) {
+				result ++;
+				printf("GSM: gsm module detected\n");
+				break;
+			}
+		}
+	}
+	if(result == -1) {
+		//check simcard status
+		gsm_send("AT+CSMINS?");
+		if(!gsm_read(gsm_line, 500)) {
+			if(!strcmp(gsm_line, "+CSMINS: 0,1")) {
+				printf("GSM: sim card detected\n");
+				result ++;
+			}
+		}
+	}
+
+	mdelay(300);
+	gsm_send("AT+GSV");
+	mdelay(300);
+	gsm_flush();
+	return result;
+}
+
+void gsm_update(void)
+{
+	static time_t gsm_timer = 0;
+	if(time_left(gsm_timer) < 0) {
+		gsm_timer = time_get(10000);
+		gsm_led_update();
 	}
 }
 
@@ -83,16 +198,20 @@ void main(void)
 	sys_init();
 	led_init();
 	key_Init();
+	gsm_init();
 	for(int i = 1; i < 8; i ++)
 		led_set_status(i, 1);
 
 	while(1) {
 		int key= key_GetKey();
-		if(key != 0) {
+		if(key != 0 && key != 255) {
+			char text[32];
 			led_set_status(key, 0);
-			gsm_mdelay(1000);
+			sprintf(text, "gsm demo: key S%d is pressed!!! reply 'led=0~255' to control the led.\n", key);
+			gsm_msg_send(phone_nr, text);
 			led_set_status(key, 1);
 		}
 		sys_update();
+		gsm_update();
 	}
 }
