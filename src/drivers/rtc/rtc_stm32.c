043c0536ea10998bf3855ad68c19541de541b4a8
feng.ni <feng.ni@linktronsys.com>
2013-01-20 15:27:24 +0800

    [rtc] add some explanation
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/drivers/rtc/rtc_stm32.c
+++ src/drivers/rtc/rtc_stm32.c
@@ -1,6 +1,13 @@
 /*
  *	miaofng@2009 initial version, be carefull of the overflow bug!!!
  * 	miaofng@2012-12-30 modified to rtc driver
+ *	miaofng@2013-1-19 note:
+ *	- both rtc and bkp are in backup (power) domain(powered by VBAT)
+ *	- rtc is in 32KHz clock domain, so you must RTC_WaitForLastTask() before
+ *	setting RTC_PRL/DIV/CNT/ALR
+ *	- bkp is in APB1 clock domain, so it's similar as other register
+ *	except its power is from VBAT.
+ *	- bkp registers has write protection function
  */
 
 #include "config.h"
@@ -10,7 +17,10 @@
 void rtc_init(unsigned now)
 {
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP,ENABLE);
-	PWR_BackupAccessCmd(ENABLE);
+	PWR_BackupAccessCmd(ENABLE); //disable bkp domain write protection
+
+	/*note: once the rtc clock source has been set, it cannot be changed
+	anymore unless the bkp domain is reset!!!*/
 #if 0
 	RCC_LSICmd(ENABLE);
 	while(RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET);
