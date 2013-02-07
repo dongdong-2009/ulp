16b4cb6c29aa16577d11517efc84720abe7ca93e
feng.ni <feng.ni@linktronsys.com>
2013-02-07 17:00:14 +0800

    [aduc] add SystemFrequency_UCLK glvar
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/cpu/aduc706x/cmsis/system_aduc706x.h
+++ src/cpu/aduc706x/cmsis/system_aduc706x.h
@@ -4,6 +4,7 @@
 #define __SYSTEM_ADUC706X_H_
 
 extern const unsigned SystemFrequency;
+extern const unsigned SystemFrequency_UCLK;
 
 void SetSysClock(unsigned hz);
 void SysTick_Config(unsigned cd);
