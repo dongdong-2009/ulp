78187ee4e5d83acd5d47207a463a79a174fd46a1
feng.ni <feng.ni@linktronsys.com>
2013-02-07 17:00:50 +0800

    [aduc] fixed uart baudrate setting bug
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/drivers/uart/uart0_aduc706x.c
+++ src/drivers/uart/uart0_aduc706x.c
@@ -1,30 +1,45 @@
 /*
  *	junjun@2013 initial version
+ *	miaofng@2013 fixed baudrate setting bug
  */
 #include "uart.h"
 #include "aduc706x.h"
 
-
-#define BIT15 0x8000
-#define BIT11 0x800
-
 static int uart_Init(const uart_cfg_t *cfg)
 {
-	//Initialize the UART for115200-8-N-1
-	unsigned baud,i=0;
-	GP1CON = 0x11; // Select UART for P1.0/P1.1
-	baud = cfg->baud/600;
-	if(baud<64)
-		while(baud>>++i);
-	else{
-		baud /= 96;
-		while(baud>>++i);
-		i += 7;
+	/*baudrate = (Fcore / 16 / 2) / DL / ( M + N / 2048)*/
+	int M,N,DL,DELTA = SystemFrequency;
+	int f = SystemFrequency / 16 / 2;
+	for(char m = 1; m < 5; m ++) {
+		int DL_min = f / cfg->baud / (m + 1) - 1;
+		int DL_max = f / cfg->baud / m + 1;
+		DL_min = (DL_min < 0) ? 0 : DL_min;
+		DL_max = (DL_max > 0xffff) ? 0xffff : DL_max;
+		for(int dl = DL_min; dl < DL_max; dl ++) {
+			int N_min = f * 2048 / cfg->baud / dl - m * 2048 - 1;
+			int N_max = f * 2048 / cfg->baud / dl - m * 2048 + 1;
+			N_min = (N_min < 0) ? 0 : N_min;
+			N_max = (N_max > 2047) ? 2047 : N_max;
+			for(int n = N_min; n < N_max; n ++) {
+				int bps = f * 2048 / dl / (m * 2048 + n);
+				bps = bps - cfg->baud;
+				bps = (bps < 0) ? (0 - bps) : bps;
+				if(bps < DELTA) {
+					M = m & 0x03; //4->0
+					N = n;
+					DL = dl;
+					DELTA = bps;
+				}
+			}
+		}
 	}
-	UrtCfg(0,i-1,URT_68|URT_78);
-	i = (SystemFrequency<<6)/cfg->baud;
-	i %= 2048;
-	COMDIV2 = i + 0x8800;
+
+	COMCON0 = 0x80; /*DLAB = 1*/
+	COMDIV0 = DL & 0xff;
+	COMDIV1 = DL >> 8;
+	COMDIV2 = 0x8000 | (M << 11) | N;
+	COMCON0 = 0x03; /*DLAB = 0, WLS = 11(8bits)*/
+	GP1CON |= 0x11; // Select UART for P1.0/P1.1
 	return 0;
 }
 
