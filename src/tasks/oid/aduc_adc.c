2335f8caa8c126188abc3ee7de7a733ccdba9b39
feng.ni <feng.ni@linktronsys.com>
2013-02-21 01:00:28 +0800

    [oid] dmm prog is about to release now :)
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

---
+++ src/tasks/oid/aduc_adc.c
@@ -0,0 +1,75 @@
+/*
+ * 	miaofng@2013-2-20 initial version
+ *
+ *	ADC1: 24bit, PGA=1..512
+ *	ADC2: 24bit, PGA=1..8
+ *	IREF: 200uA*N, N=1:5, 10uA opt
+ *	Rref: 100Ohm
+ *	VRref: 20mV*N(2mV@10uA)
+ *
+ */
+#include "ulp/sys.h"
+#include "aduc706x.h"
+#include "aduc_adc.h"
+
+void aduc_adc_init(const aduc_adc_cfg_t *cfg)
+{
+	ADCMDE = (1 << 7) | ADUC_ADC_MODE_IDLE; //force adc to idle mode
+	IEXCON = (1 << 6) | (cfg->iexc << 1) | cfg->ua10;
+	ADCFLT = (1 << 7) | 127; //Fadc = 50Hz, chop off
+	//ADCFLT = 0xc000 | (1 << 7) | 52;//Fadc = 50.3Hz, chop on
+	ADCCFG = 0;
+
+	/*self calibration*/
+	ADC0CON = (1 << 15);
+	ADC1CON = (1 << 15);
+	ADCMDE = (1 << 7) | ADUC_ADC_MODE_ZCAL_SELF;
+	while((ADCSTA & 0x0001) == 0);
+	ADCMDE = (1 << 7) | ADUC_ADC_MODE_GCAL_SELF;
+	while((ADCSTA & 0x0001) == 0);
+
+	//ADC0CON = 0x00;
+	//ADC1CON = 0x00;
+	//sys_mdelay(10);
+
+	if(cfg->adc0) {
+		unsigned v = (1 << 15) | (cfg->mux0 << 6) | cfg->pga0;
+		ADC0CON = v; /*enable ADC0*/
+	}
+	if(cfg->adc1) {
+		unsigned v = (1 << 15) | (cfg->mux1 << 7) | cfg->pga1; /*enable ADC1*/
+		ADC1CON = v;
+	}
+
+	/*cal*/
+	ADCMDE = (1 << 7) | 4; //self offset calibration
+	//ADCMDE = (1 << 7) | 6; //system offset calibration
+	while((ADCSTA & 0x8000) == 0);
+	//ADCMDE = (1 << 7) | 5; //self gain calibration
+	//while((ADCSTA & 0x8000) == 0);
+
+	ADCMDE = (1 << 7) | ADUC_ADC_MODE_AUTO;
+}
+
+int aduc_adc_get(int adc, int *result)
+{
+	int ecode = 1; //busy
+	aduc_adc_status_t status;
+	status.value = ADCSTA;
+
+	if(adc == 0) {
+		if(status.ADC0RDY) {
+			ecode = - status.ADC0CERR;
+			*result = ADC0DAT;
+		}
+	}
+	else {
+		if(status.ADC1RDY) {
+			ecode = - status.ADC1CERR;
+			ADC0DAT; /*dummy*/
+			*result = ADC1DAT;
+		}
+	}
+
+	return ecode;
+}
--- src/tasks/oid/oid_dmm.h
+++ src/tasks/oid/oid_dmm.h
@@ -12,11 +12,12 @@ then do r_auto for detail in case it's not open*/
 #define MODE_IS_R(mode) ((mode & 0xf0) == 0x20)
 
 enum {
-	DMM_V_AUTO = 0x10, /*precision&slow, range: 1uV ~ 1V*/
-	DMM_R_AUTO = 0x20, /*precision&slow, range: 1mOhm ~ 100Kohm*/
-	DMM_R_SHORT, /*rough&fast, range: 1~2000mohm (I=1mA, G=512)*/
-	DMM_R_OPEN, /*rough&fast, range: 50~120Kohm (I=10uA, G=1)*/
-	/*pls add more mode here to keep compatibility*/
+	DMM_V_AUTO = 0x10, /*precision&slow, range: 0~1V2(G=1..512)*/
+	DMM_R_AUTO = 0x20, /*precision&slow, range: 0~1K2(I=1mA, G=1..512)*/
+	DMM_R_SHORT, /*rough&fast, range: 0~2R (I=1mA, G=512)*/
+	DMM_R_OPEN, /*rough&fast, range: 0~120Kohm (I=10uA, G=1)*/
+	/*pls add more mode here to keep history compatibility*/
+	DMM_OFF = 0x30,
 };
 
 /* calibration procedure:
@@ -32,21 +33,14 @@ struct dmm_reg_s {
 			char ecode; /*R, dmm system or calibration error*/
 			char cal: 1; /*W, to start calibration procedure, auto zero*/
 			char ready : 1; /*R, new data ready to use*/
+			char clr : 1; /*W, clear ready flag*/
+			char over : 1; /*R, over-range*/
 		};
 		int value;
 	} mode;
 
 	#define DMM_REG_DATA	4
 	int value; /*R/W*/
-
-	#define DMM_REG_ADC	8
-	union {
-		struct dmm_adc_s {
-			int iexc_uA: 16; /*0-1000*/
-			int pga : 16; /*0-512*/
-		};
-		int value;
-	} adc;
 };
 
 #endif
