cf6f8a9d2aecffb19d6a818426a92a0465ffb728
feng.ni <feng.ni@linktronsys.com>
2013-05-30 12:06:34 +0800

    foc algo update
    Signed-off-by: kang.kong <kang.kong@linktronsys.com>
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/include/pmsm/foc.h
+++ src/include/pmsm/foc.h
@@ -4,17 +4,56 @@
 
 #ifndef __FOC_H
 #define __FOC_H
+#include "pmsm/pmsm.h"
+#include "ulp/pmsmd.h"
+#include "common/pid.h"
 
-enum {
-	FRQ,
-	PMSM,
-	BOARD,
-	ID_PID,
-	IQ_PID,
-	SPEED_PID,
-	THETA_PID,
-	ACCLR_PID,
-};
+typedef struct {
+	int status;
+	int frq;
+	int crp;
+	int pid_flag; //pid has been completed
+	int execute_update; //SVMWM module has executed the changes
+	pmsmd_ops_t pmsmd;
+	pmsm_t pmsm;
+
+	struct {
+		int theta_per_counter_q10;
+
+		int theta_before;
+		int theta_now;
+		int theta_expect;
+		int theta_next;
+		int theta_diff;
+
+		int speed_now;
+		int speed_expect;
+		int speed_calc_cnt;
+		int speed_calc_cnt_max;
+		int speed_theta_diff;
+
+		int acclr_now;
+		int acclr_expect;
+	} mech;
+
+	struct {
+		int theta;
+		int iu;
+		int iv;
+		int id;
+		int iq;
+		int ualpha;
+		int ubeta;
+		int tpwm;
+		int tpwm_multi_sqrt3;
+	} elec;
+
+	struct pid_q15_s id_pid;
+	struct pid_q15_s iq_pid;
+	struct pid_q15_s speed_pid;
+	struct pid_q15_s theta_pid;
+	struct pid_q15_s acclr_pid;
+} foc_t;
 
 int foc_new(void);
 void foc_cfg(int handle, ...);
