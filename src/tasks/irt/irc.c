75d9a014d01a616d557283a61a776cf0bb9c25db
feng.ni <feng.ni@linktronsys.com>
2014-05-14 19:08:47 +0800

    [irc] update
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/tasks/irt/irc.c
+++ src/tasks/irt/irc.c
@@ -14,19 +14,22 @@
 #include "shell/shell.h"
 #include "can.h"
 #include "err.h"
+#include "led.h"
 
 static const can_bus_t *irc_bus = &can1;
 static volatile int irc_vmcomp_pulsed;
 static int irc_ecode = 0;
-static int irc_mode = IRC_MODE_L2T;
-static int irc_mode_new;
 
 static struct {
 	unsigned opc : 1;
 	unsigned rst : 1;
 	unsigned abt : 1;
-	unsigned nmb : 1; /*new mode*/
 } irc_status;
+
+static inline void irc_error(int ecode)
+{
+	irc_ecode = -ecode;
+}
 
 #if 1
 #include "stm32f10x.h"
@@ -37,24 +40,35 @@ void EXTI3_IRQHandler(void)
 	EXTI->PR = EXTI_Line3;
 }
 
+/*TRIG PC2 OUT*/
 static inline void _trig_set(int high) {
 	irc_vmcomp_pulsed = 0;
-	BitAction ba = (high) ? Bit_SET : Bit_RESET;
-	GPIO_WriteBit(GPIOC, GPIO_Pin_2, ba);
+	if(high) {
+		GPIOC->BSRR = GPIO_Pin_2;
+	}
+	else {
+		GPIOC->BRR = GPIO_Pin_2;
+	}
 }
 
+/*VMCOMP PC3 IN*/
 static inline int _trig_get(void) {
 	return irc_vmcomp_pulsed;
 }
 
+/*LE_txd PC7*/
 static inline void _le_set(int high) {
-	BitAction ba = (high) ? Bit_SET : Bit_RESET;
-	GPIO_WriteBit(GPIOC, GPIO_Pin_6, ba);
+	if(high) {
+		GPIOC->BSRR = GPIO_Pin_7;
+	}
+	else {
+		GPIOC->BRR = GPIO_Pin_7;
+	}
 }
 
+/*LE_rxd	PC6*/
 static inline int _le_get(void) {
-	int level = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_7);
-	return level;
+	return (GPIOC->IDR & GPIO_Pin_6) ? 1 : 0;
 }
 
 /*
@@ -67,14 +81,15 @@ static inline void _irc_init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_7;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_6;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 	//vmcomp exti input
@@ -109,20 +124,12 @@ static inline void _rly_init(void)
 
 static inline void _rly_set(int x)
 {
-	int y = vGPIOE->ODR ;
-	y &= 0xf0;
+	int y = GPIOE->ODR ;
+	y &= 0xff00;
 	y |= x;
-	vGPIOE->ODR = y;
+	GPIOE->ODR = y;
 }
 #endif
-
-void irc_init(void)
-{
-	const can_cfg_t cfg = {.baud = CAN_BAUD, .silent = 0};
-	irc_bus->init(&cfg);
-	_irc_init();
-	_rly_init();
-}
 
 int relay_latch(void)
 {
@@ -180,13 +187,14 @@ int dmm_trig(void)
 
 static int irc_is_opc(void)
 {
-	vm_is_opc();
+	int opc = vm_is_opc();
 	opc = (irc_status.opc) ? opc : 0;
 	return opc;
 }
 
 static int irc_reset(void)
 {
+
 	return 0;
 }
 
@@ -195,32 +203,48 @@ static int irc_abort(void)
 	return 0;
 }
 
-static int irc_mode(int mode)
+static int irc_mode_change(int mode)
 {
 	int ecode = 0;
-	int cnt = 0, bytes, over;
 	can_msg_t msg;
 	irc_cfg_msg_t *cfg = (irc_cfg_msg_t *) msg.data;
+	memset(msg.data, 0x00, sizeof(msg.data));
 
-	//step 1, turn off all relays
-	cfg->cmd = IRC_CFG_ALLOFF_IBUS_ELNE;
+	//step 1, set slot relays
+	cfg->cmd = IRC_CFG_NORMAL;
+	cfg->slot = 0xFF;
+	cfg->bus = (mode & IRC_MASK_EBUS) ? 0xFF : 0;
+	cfg->line = (mode & IRC_MASK_ELNE) ? -1 : 0;
 	msg.id = CAN_ID_CFG;
+	msg.dlc = sizeof(irc_cfg_msg_t);
 	ecode = irc_bus->send(&msg);
 	if(ecode) {
-		irc_ecode = -IRT_E_CAN;
+		irc_error(IRT_E_CAN);
 		return ecode;
 	}
 
 	ecode = relay_latch();
 	if(ecode) { //find the bad guys hide inside the good slots
-		irc_ecode = -IRT_E_SLOT;
+		irc_error(-IRT_E_SLOT);
 		return ecode;
 	}
 
 	//step 2, set irc board itself relays
 	_rly_set(mode & 0xff);
-
 	return ecode;
+}
+
+void irc_init(void)
+{
+	_irc_init();
+	_rly_init();
+	_le_set(1); //tricky, to bring slot card from deadlock on waiting le signal
+	sys_mdelay(1);
+	_le_set(0);
+	_trig_set(0);
+
+	const can_cfg_t cfg = {.baud = CAN_BAUD, .silent = 0};
+	irc_bus->init(&cfg);
 }
 
 /*implement a group of switch operation*/
@@ -245,7 +269,7 @@ void irc_update(void)
 			msg.id += cnt & 0x0F;
 			ecode = irc_bus->send(&msg);
 			if(ecode) {
-				irc_ecode = -IRT_E_CAN;
+				irc_error(IRT_E_CAN);
 				return;
 			}
 
@@ -255,14 +279,14 @@ void irc_update(void)
 			if(over) {
 				ecode = relay_latch();
 				if(ecode) { //find the bad guys hide inside the good slots
-					irc_ecode = -IRT_E_SLOT;
+					irc_error(IRT_E_SLOT);
 					return;
 				}
 
 				if(vm_opgrp_is_scan()) {
 					ecode = dmm_trig();
 					if(ecode) {
-						irc_ecode = -IRT_E_DMM;
+						irc_error(IRT_E_DMM);
 						return;
 					}
 				}
@@ -285,8 +309,11 @@ void irc_update(void)
 int main(void)
 {
 	sys_init();
+	led_flash(LED_RED);
+	irc_init();
 	vm_init();
 	printf("irc v1.0, SW: %s %s\n\r", __DATE__, __TIME__);
+	irc_mode_change(IRC_MODE_L2T);
 	while(1) {
 		sys_update();
 		irc_status.opc = 0;
@@ -310,7 +337,7 @@ int cmd_xxx_func(int argc, char *argv[])
 
 	int ecode = 0;
 	if(!strcmp(argv[0], "*IDN?")) {
-		printf("<Linktron Technology,IRT16X3254,MY%s,%s\n\r", __DATE__, __TIME__);
+		printf("<Linktron Technology,IRT16X3254,%s,%s\n\r", __DATE__, __TIME__);
 		return 0;
 	}
 	else if(!strcmp(argv[0], "*OPC?")) {
@@ -319,11 +346,11 @@ int cmd_xxx_func(int argc, char *argv[])
 		return 0;
 	}
 	else if(!strcmp(argv[0], "*ERR?")) {
-		printf("<%+d\n\r", irc_ecode);
+		err_print(irc_ecode);
 		return 0;
 	}
 	else if(!strcmp(argv[0], "*RST")) {
-		irc_status.rst = 1;
+		NVIC_SystemReset();
 	}
 	else if(!strcmp(argv[0], "ABORT")) {
 		irc_status.abt = 1;
@@ -349,40 +376,35 @@ static int cmd_mode_func(int argc, char *argv[])
 	};
 
 	int ecode = 0;
-	if(!strcmp(argv[1], "MODE")) {
-		int lv = (argc > 2) ? atoi(argv[2]) : 0;
-		int is = (argc > 3) ? atoi(argv[3]) : 0;
-		const char *name[] = {
-			"HVR", "L4R", "W4R", "L2R", "L2T", "RMX", "VHV", "VLV", "IIS"
-		};
-		const int mode_list[] = {
-			IRC_MODE_HVR, IRC_MODE_L4R,
-			IRC_MODE_W4R, IRC_MODE_L2R,
-			IRC_MODE_L2T, IRC_MODE_RMX,
-			IRC_MODE_VHV, IRC_MODE_VLV,
-			IRC_MODE_IIS
-		};
-		ecode = -IRT_E_CMD_FORMAT;
-		for(int i = 0; i < sizeof(mode_list) / sizeof(int); i ++) {
-			if(!strcmp(argv[1], name[i])) {
-				int mode = mode_list[i];
-				mode |= (mode & IRC_MASK_ELV) ? lv : 0;
-				mode |= (mode & IRC_MASK_EIS) ? is : 0;
-				ecode = -IRT_E_OP_REFUSED;
-				if(irc_is_opc()) {
-					irc_mode_new = mode;
-					irc_status.nmd = 1;
-					ecode = 0;
-				}
-			}
-		}
-	}
-	else if(!strcmp(argv[1], "HELP")) {
+	int lv = (argc > 2) ? atoi(argv[2]) : 0;
+	int is = (argc > 3) ? atoi(argv[3]) : 0;
+	const char *name[] = {
+		"HVR", "L4R", "W4R", "L2R", "L2T", "RMX", "VHV", "VLV", "IIS"
+	};
+	const int mode_list[] = {
+		IRC_MODE_HVR, IRC_MODE_L4R,
+		IRC_MODE_W4R, IRC_MODE_L2R,
+		IRC_MODE_L2T, IRC_MODE_RMX,
+		IRC_MODE_VHV, IRC_MODE_VLV,
+		IRC_MODE_IIS
+	};
+
+	if(!strcmp(argv[1], "HELP")) {
 		printf("%s", usage);
 		return 0;
 	}
-	else {
-		ecode = -IRT_E_CMD_FORMAT;
+
+	ecode = -IRT_E_CMD_FORMAT;
+	for(int i = 0; i < sizeof(mode_list) / sizeof(int); i ++) {
+		if(!strcmp(argv[1], name[i])) {
+			int mode = mode_list[i];
+			mode |= (mode & IRC_MASK_ELV) ? lv : 0;
+			mode |= (mode & IRC_MASK_EIS) ? is : 0;
+			ecode = -IRT_E_OP_REFUSED;
+			if(irc_is_opc()) {
+				ecode = irc_mode_change(mode);
+			}
+		}
 	}
 	err_print(ecode);
 	return 0;
