2fc379e2e74b52378b1adadc7e3be4891df30557
feng.ni <feng.ni@linktronsys.com>
2014-09-15 19:26:52 +0800

    [irt] route V2.0 bsp support
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/tasks/irt/bsp.c
+++ src/tasks/irt/bsp.c
@@ -1,6 +1,7 @@
 /*
 *
 *  miaofng@2014-6-6   separate driver from irc main routine
+*  miaofng@2014-9-13 remove irt hw v1.x related codes
 *
 */
 #include "ulp/sys.h"
@@ -10,75 +11,80 @@
 #include "can.h"
 #include "bsp.h"
 #include "mbi5025.h"
+#include "shell/cmd.h"
 
 static volatile int irc_vmcomp_pulsed;
-const mbi5025_t irc_mbi = {.bus = &spi2, .load_pin = SPI_2_NSS, .oe_pin = SPI_CS_PB12};
+static const mbi5025_t irc_mbi = {.bus = &spi2, .load_pin = SPI_2_NSS, .oe_pin = SPI_CS_PB12};
 
-static void _rly_init(void)
+/*to conver relay name order(LSn) to mbi5024 control line nr(Km)*/
+static int rly_map(int relays)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
-
-	/*PE0~PE7*/
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | \
-		GPIO_Pin_3 |GPIO_Pin_4 |GPIO_Pin_5 |GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
-}
-
-void _rly_set(int mode)
-{
-	/*
-	enum {
-		IRC_MODE_HVR = 0x7f | IRC_MASK_IBUS | IRC_MASK_ELNE | IRC_MASK_ELV,
-		IRC_MODE_L4R = 0x48 | IRC_MASK_IBUS | IRC_MASK_ELNE | IRC_MASK_EIS,
-		IRC_MODE_W4R = 0x08 | IRC_MASK_IBUS | IRC_MASK_ELNE | IRC_MASK_ELV | IRC_MASK_EIS,
-		IRC_MODE_L2R = 0x00 | IRC_MASK_IBUS | IRC_MASK_ELNE | IRC_MASK_ELV,
-		IRC_MODE_L2T = IRC_MODE_L2R,
-		IRC_MODE_RMX = 0x1c | IRC_MASK_IBUS | IRC_MASK_ILNE,
-		IRC_MODE_VHV = 0x02 | IRC_MASK_EBUS | IRC_MASK_ILNE,
-		IRC_MODE_VLV = 0x80 | IRC_MASK_IBUS | IRC_MASK_ELNE | IRC_MASK_ADD, //!!!ISSUE!!!
-		IRC_MODE_IIS = 0x21 | IRC_MASK_IBUS | IRC_MASK_ELNE,
-	};
-	*/
-	const char image[] = {0x7f, 0x48, 0x08, 0x00, 0x1c/*probe mode*/, 0x1c, 0x02, 0x80, 0x21, 0x00};
-	sys_assert((mode >= IRC_MODE_HVR) && (mode <= IRC_MODE_OFF));
-
-	int y = GPIOE->ODR;
-	y &= 0xff00;
-	y |= image[mode];
-	GPIOE->ODR = y;
+	int image = 0;
+	const char map[] = {0,11,12,21,1,10,13,20,  2,9,14,19,5,6,24,3,  18,25,8,15,7,4,26,17,  16,23,22,27,};
+	for(int i = 0; i < sizeof(map); i ++) {
+		if(relays & (1 << i)) {
+			image |= 1 << map[i];
+		}
+	}
+	return image;
 }
 
 static void rly_init(void)
 {
-	_rly_init();
 	mbi5025_DisableOE(&irc_mbi);
 	mbi5025_Init(&irc_mbi);
+	mbi5025_EnableOE(&irc_mbi);
 }
 
+/* warnning:
+1, RPB: all slot's vline/iline must be shorten and PROBE line resistance will not be ignored!!
+2, RMX: the same as rpb, but slot's vline/iline is shorten only when required
+3, L4R = W4R, but IS is different
+4, L2T could be used for diode test
+5, maybe we should delete K4,K5,K6,K7,K20?
+*/
 void rly_set(int mode)
 {
-	_rly_set(mode);
-	const short image[] = {
-		(1 << 0) | (1 << 2), /*HVR*/
-		(1 << 7) | (1 << 4), /*L4R*/
-		(1 << 3) | (1 << 7), /*W4R*/
-		(1 <<3) | (1 << 10), /*L2T*/
-		(1 <<5) | (1 << 8), /*RPB*/
-		(1 <<5) | (1 << 8), /*RMX*/
-		(1 <<6) | (1 << 9), /*VHV*/
-		(1 <<6) | (1 << 11), /*VLV*/
-		(1 <<1) | (1 << 13), /*IIS*/
+	#define LS(n) (1 << n)
+
+	const int image[] = {
+		LS(12)|LS(14), /*HVR*/
+		LS(18)|LS(15)|LS(23)|LS(21), /*L4R, real 4 line resistor measurement*/
+		LS(17)|LS(15)|LS(24)|LS(21), /*W4R, use Is!*/
+		LS(18)|LS(15), /*L2T, dmm sense pin is open*/
+		LS(18)|LS(23)|LS(0)|LS(1)|LS(2)|LS(3)|LS(8)|LS(9)|LS(10)|LS(11), /*RPB*/
+		LS(18)|LS(23)|LS(0)|LS(1)|LS(2)|LS(3)|LS(8)|LS(9)|LS(10)|LS(11), /*RMX*/
+		LS(25)|LS(16)|LS(18), /*VHV*/
+		LS(26)|LS(16)|LS(18), /*VLV*/
+		LS(17)|LS(16)|LS(13), /*IIS*/
 		0x00,
 	};
 
 	sys_assert((mode >= IRC_MODE_HVR) && (mode <= IRC_MODE_OFF));
-	short relays = image[mode];
+	int relays = image[mode];
+	relays = rly_map(relays);
 	mbi5025_write_and_latch(&irc_mbi, &relays, sizeof(relays));
-	mbi5025_EnableOE(&irc_mbi);
 }
+
+static int cmd_relay_func(int argc, char *argv[])
+{
+	const char * usage = { \
+		"usage:\n" \
+		"relay 0-2,4	turn on relay k0,k1,k2,k4\n" \
+	};
+
+	if (argc >= 2) {
+		int relays = cmd_pattern_get(argv[1]);
+		relays = rly_map(relays);
+		mbi5025_write_and_latch(&irc_mbi, &relays, sizeof(relays));
+		return 0;
+	}
+
+	printf(usage);
+	return 0;
+}
+const cmd_t cmd_relay = {"relay", cmd_relay_func, "route board relay set cmds"};
+DECLARE_SHELL_CMD(cmd_relay)
 
 /*
 TRIG	PC2	OUT
