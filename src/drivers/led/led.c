db48c7f40abe1722878ae65ea02ea767a239ba33
feng.ni <feng.ni@linktronsys.com>
2014-09-25 18:56:28 +0800

    [led] update with dual color led support
    Signed-off-by: feng.ni <feng.ni@linktronsys.com>

--- src/drivers/led/led.c
+++ src/drivers/led/led.c
@@ -7,8 +7,6 @@
 #include "led.h"
 #include "ulp_time.h"
 
-#define CONFIG_LED_ECODE 1
-
 static time_t led_timer;
 static char flag_status; /*0->off/flash, 1->on*/
 static char flag_flash; /*1->flash*/
@@ -27,6 +25,7 @@ void led_Init(void)
 	flag_hwstatus = 0;
 #ifdef CONFIG_LED_ECODE
 	led_ecode = 0;
+	led_estep = 0;
 #endif
 
 	led_hwInit();
@@ -58,24 +57,7 @@ void led_Update(void)
 	}
 #endif
 
-	/*calcu the new status of leds*/
-	status = flag_hwstatus;
-	status ^= flag_flash;
-	status &= flag_flash;
-	status |= flag_status;
-
-	update = status ^ flag_hwstatus;
-	flag_hwstatus = status;
-
-	led = 0;
-	while(update > 0) {
-		if(update & 1)
-			led_hwSetStatus((led_t)led, (led_status_t)(status & 1));
-
-		led ++;
-		update >>= 1;
-		status >>= 1;
-	}
+	led_Update_Immediate();
 }
 
 void led_Update_Immediate(void)
@@ -84,8 +66,6 @@ void led_Update_Immediate(void)
 	int update;
 	int status;
 
-	led_timer = time_get(LED_FLASH_PERIOD);
-
 	/*calcu the new status of leds*/
 	status = flag_hwstatus;
 	status ^= flag_flash;
@@ -97,8 +77,17 @@ void led_Update_Immediate(void)
 
 	led = 0;
 	while(update > 0) {
-		if(update & 1)
+		if(update & 1) {
+#ifdef CONFIG_LED_DUAL
+			if((led_t) led == LED_YELLOW) {
+				led_hwSetStatus(LED_RED, (led_status_t)(status & 1));
+				led_hwSetStatus(LED_GREEN, (led_status_t)(status & 1));
+			}
+			else
+#else
 			led_hwSetStatus((led_t)led, (led_status_t)(status & 1));
+#endif
+		}
 
 		led ++;
 		update >>= 1;
@@ -109,18 +98,38 @@ void led_Update_Immediate(void)
 void led_on(led_t led)
 {
 	int i = (int)led;
+#ifdef CONFIG_LED_ECODE
+	led_error(0);
+#endif
+
+#if CONFIG_LED_DUAL
+	//clear red/green/yellow
+	flag_status &= ~0x07;
+	flag_flash &= ~0x07;
+#endif
 
 	flag_status |= 1 << i;
 	flag_flash &= ~(1 << i);
-	led_hwSetStatus(led, LED_ON);
+	led_Update_Immediate();
 }
 
 void led_off(led_t led)
 {
 	int i = (int)led;
 
+#ifdef CONFIG_LED_ECODE
+	led_error(0);
+#endif
+
+#if CONFIG_LED_DUAL
+	//clear red/green/yellow
+	flag_status &= ~0x07;
+	flag_flash &= ~0x07;
+#endif
+
 	flag_status &= ~(1 << i);
 	flag_flash &= ~(1 << i);
+
 	led_hwSetStatus(led, LED_OFF);
 }
 
@@ -128,24 +137,55 @@ void led_inv(led_t led)
 {
 	int i = (int)led;
 
+#ifdef CONFIG_LED_ECODE
+	led_error(0);
+#endif
+
+#if CONFIG_LED_DUAL
+	flag_status &= ~(0x07 & (~(1 << i)));
+	flag_flash &= ~0x07;
+#endif
+
 	flag_status ^= 1 << i;
 	flag_flash &= ~(1 << i);
-	led_hwSetStatus(led, (flag_status & (1 << i)) ? LED_ON : LED_OFF);
+
+	led_Update_Immediate();
 }
 
 void led_flash(led_t led)
 {
 	int i = (int)led;
 
+#ifdef CONFIG_LED_ECODE
+	led_error(0);
+#endif
+
+#if CONFIG_LED_DUAL
+	flag_status &= ~(0x07 & (~(1 << i)));
+	flag_flash &= ~0x07;
+#endif
+
 	flag_status &= ~(1 << i);
 	flag_flash |= 1 << i;
 }
 
+#ifdef CONFIG_LED_ECODE
 void led_error(int ecode)
 {
+	if(ecode) {
+#if CONFIG_LED_DUAL
+		flag_status &= ~0x07;
+		flag_flash &= ~0x07;
+#else
+		flag_status &= ~0x02;
+		flag_flash &= ~0x02;
+#endif
+	}
+
 	ecode = (ecode < 0) ? - ecode : ecode;
 	if((ecode == 0) || (led_ecode == 0)) {
 		led_ecode = (char) ecode;
 		led_estep = 0;
 	}
 }
+#endif
