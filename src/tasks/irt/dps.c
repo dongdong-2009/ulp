0f3b6395b234816e305d455c7e121708a37baf65
miaofng <miaofng@126.com>
2016-08-26 10:14:06 +0200

    [irc] dps lv v2.0 & hv v4.2 pre-release
    Signed-off-by: miaofng <miaofng@126.com>

--- src/tasks/irt/dps.c
+++ src/tasks/irt/dps.c
@@ -12,6 +12,13 @@
 *    pwm on = pwm off = 3mS(47K||10NF)
 *    bank0: IS_GS0 = 0&IS_GS1 = 1, 0.12mA-122.5mA (1.225v/10ohm)
 *    bank1: IS_GS0 = 1&IS_GS1 = 0, 2.04mA-2.085A (1.225V / (4.7ohm / 4) * 2)
+*  miaofng@2016-7-23  add dps hv board v3.1 support
+*  - static pwm hv configuration, >10mA@0-1KV drive capability
+*  - dynamic vs up/down control and over-current protection(about 8mA)
+*  - polar config & fast discharge (1kV->1A@1kohm)
+*  miaofng@2016-8-18  add dps hv board v4.2 support
+*  - add dps_hv(not vs) fast discharge support!
+*
 */
 #include "ulp/sys.h"
 #include "irc.h"
@@ -24,7 +31,8 @@
 #include "common/debounce.h"
 #include "vm.h"
 
-#define DPS_HV_V2 1
+#define DPS_HV_V4 1
+#define DPS_HV_AC 1 /*AUTO SWITCH HV POLAR*/
 #define DPS_LV_V2 1 /*XL4015*/
 
 static float dps_lv;
@@ -37,10 +45,15 @@ static int dps_flag_enable;
 static int dps_is_g;
 static int dps_hv_g;
 
+#if DPS_HV_V4 == 1
+static char dps_hv_polar = '+';
+#endif
+
 static struct debounce_s dps_mon_lv;
 static struct debounce_s dps_mon_hs;
 static struct debounce_s dps_mon_hv;
-static time_t dps_mon_timer;
+
+#define VREF_ADC 2.500
 
 #if DPS_LV_V2 == 1
 /*
@@ -49,13 +62,19 @@ vout = 1.25v + is * 10Kohm = 1.25v + vset / 0.5K * 10K
 */
 static int dps_lv_set(float v)
 {
-	dps_lv = v;
-	if(v < 1.25) v = 1.25; //at least 1.225v
+	dps_enable(DPS_LV, 0); //FAST DISCHARGE
+	vm_wait(3); //1.4mS in the scope
 
-	float vset = (v - 1.25) / 20;
-	int pwm = (int) (vset * 1024 / 1.225);
-	pwm = (pwm > 1023) ? 1023 : pwm;
-	lv_pwm_set(pwm);
+	dps_lv = v;
+	if(v > 1.5) {
+		float vset = (v - 1.25) / 20;
+		int pwm = (int) (vset * 1024 / 1.225);
+		pwm = (pwm > 1023) ? 1023 : pwm;
+		lv_pwm_set(pwm);
+		vm_wait(3); //2mS in the scope
+
+		dps_enable(DPS_LV, 1);
+	}
 	return 0;
 }
 
@@ -88,7 +107,8 @@ int dps_lv_stop(void)
 	dps_enable(DPS_LV, 0); //400uS@2A
 	return 0;
 }
-#else
+#endif
+#if DPS_LV_V1 == 1
 //VREF=2V5 R89 = 100K R88=47K R50=47K R81=1K
 //VOUT = VREF * RATIO * DF = VMAX * DF
 //=> DF = VOUT / VMAX
@@ -117,11 +137,13 @@ static int dps_is_pwm = 0;
 int dps_is_start(void)
 {
 	is_pwm_set(dps_is_pwm);
+	return 0;
 }
 
 int dps_is_stop(void)
 {
 	is_pwm_set(0);
+	return 0;
 }
 
 #if DPS_LV_V2 == 1
@@ -158,7 +180,8 @@ static int dps_is_set(float amp)
 	dps_is_pwm = pwm;
 	return 0;
 }
-#else
+#endif
+#if DPS_LV_V1 == 1
 /*GS0 INA225 GAIN, GS1 RELAY */
 #define IS_GSR50 (1 << 1)
 #define IS_GSR1R 0
@@ -209,12 +232,22 @@ static int dps_is_set(float amp)
 }
 #endif
 
+#if DPS_LV_V2 == 1
+static int dps_hs_set(float v)
+{
+	//hs always 12v
+	dps_hs = 12.0;
+	return 0;
+}
+#endif
+#if DPS_LV_V1 == 1
 static int dps_hs_set(float v)
 {
 	dps_hs = (v > 10.0) ? 13.0 : 7.0;
 	bsp_gpio_set(HS_VS, v > 10.0);
 	return 0;
 }
+#endif
 
 //6.8K + 1K
 static float dps_hs_get(void)
@@ -225,10 +258,8 @@ static float dps_hs_get(void)
 	return vout;
 }
 
-#if DPS_HV_V2
+#if DPS_HV_V2 == 1
 #define HV_VREF_PWM 1.250
-#define VREF_ADC 2.500
-
 static int dps_hv_set(float v)
 {
 	dps_hv = v;
@@ -273,6 +304,63 @@ static float dps_vs_get(void)
 	return vout;
 }
 #endif
+#if DPS_HV_V4 == 1
+
+#define HV_VREF_PWM 1.225
+static int dps_hv_set(float v)
+{
+	//too low, turn off
+	if(v <= 10.0) {
+		v = 0.0;
+	}
+
+	if(v < dps_hv) {
+		//fast discharge
+		dps_enable(DPS_HV, 0);
+
+		bsp_gpio_set(VS_UP, 1);
+		bsp_gpio_set(VS_DN, 1);
+		vm_wait(300); //123mS@(1kv,6mA)
+
+		//resume vs settings
+		dps_enable(DPS_VS, dps_flag_enable & (1 << DPS_VS));
+	}
+
+	dps_hv = v;
+	if(v > 0) {
+		dps_enable(DPS_HV, 1);
+		v /= 1000; //1000:1
+
+		int pwm = (int) (v * (1024/ HV_VREF_PWM));
+		pwm = (pwm > 1023) ? 1023 : pwm;
+		pwm = (pwm < 0) ? 0 : pwm;
+		hv_pwm_set(pwm);
+	}
+	return 0;
+}
+
+static float dps_hv_get(void)
+{
+	float vadc = hv_adc_get();
+	vadc *= VREF_ADC / 65536;
+	float vout = vadc * 1000;
+	return vout;
+}
+
+static int dps_vs_set(float v)
+{
+	//vs only has switch on/off function
+	return -IRT_E_OP_REFUSED;
+}
+
+static float dps_vs_get(void)
+{
+	float vadc = vs_adc_get();
+	vadc *= VREF_ADC / 65536;
+	float vout = vadc * 1000;
+	return vout;
+}
+#endif
 
 void dps_init(void)
 {
@@ -287,31 +375,40 @@ void dps_init(void)
 	dps_set(DPS_HV, 5.0);
 	dps_set(DPS_IS, 0.010);
 
-	dps_mon_timer = time_get(0);
+	dps_hv_stop();
 }
 
 void dps_update(void)
 {
 	float lv, hs, hv, delta, ref;
-	if(time_left(dps_mon_timer) > 0) {
-		return;
+
+#if 1
+	//power hv decrease too slow :(
+	//monitor hv output
+	if(dps_flag_enable & (1 << DPS_HV)) {
+		ref = dps_hv;
+		hv = dps_hv_get();
+		delta = hv - ref;
+		delta = (delta > 0) ? delta : -delta;
+		debounce(&dps_mon_hv, delta > 30.0);
+		if(dps_mon_hv.on) {
+			irc_error(-IRT_E_HV);
+		}
 	}
+#endif
 
-	dps_mon_timer = time_get(100);
-
-/*
 	//monitor lv output
 	if(dps_flag_enable & (1 << DPS_LV)) {
 		ref = dps_lv;
 		lv = dps_lv_get();
 		delta = lv - ref;
 		delta = (delta > 0) ? delta : -delta;
-		debounce(&dps_mon_lv, delta > 2.0);
+		debounce(&dps_mon_lv, delta > 3.0);
 		if(dps_mon_lv.on) {
 			irc_error(-IRT_E_LV);
 		}
 	}
-*/
+
 	//monitor hs output
 	if(dps_flag_enable & (1 << DPS_HS)) {
 		ref = dps_hs;
@@ -335,25 +432,24 @@ static void dps_mdelay(int ms)
 	}
 }
 
-#if DPS_HV_V2
 int dps_hv_start(void)
 {
 	//no needs to monitor vs now
 	//instead, over-current is monitored from dmm
-	bsp_gpio_set(VS_EN, 1);
+	dps_enable(DPS_VS, 1);
+	return 0;
 }
-#endif
 
 int dps_hv_stop(void)
 {
 	int ecode = - IRT_E_HV_DN;
 	float vs;
 
-	struct debounce_s vs_good;
-	debounce_init(&vs_good, 2, 0);
-	bsp_gpio_set(VS_EN, 0);
-	vm_wait(5);
+	dps_enable(DPS_VS, 0);
+	vm_wait(1);
 
+	struct debounce_s vs_good;
+	debounce_t_init(&vs_good, 5, 0); //ok more than 5mS
 	time_t deadline = time_get(DPS_HVDN_MS);
 	while(time_left(deadline) > 0) {
 		irc_update();
@@ -369,9 +465,20 @@ int dps_hv_stop(void)
 		dps_enable(DPS_HV, 0);
 		irc_error(ecode);
 	}
+
+	/*switch polar*/
+#if DPS_HV_V4 == 1
+	#if DPS_HV_AC == 1
+	if(dps_hv_polar == '+') dps_hv_polar = '-';
+	else dps_hv_polar = '+';
+	#endif
+
+	bsp_gpio_set(VS_POL, dps_hv_polar == '+');
+	vm_wait(5); //relay response 5mS
+#endif
+
 	return ecode;
 }
-
 
 int dps_enable(int dps, int enable)
 {
@@ -385,18 +492,23 @@ int dps_enable(int dps, int enable)
 
 	switch(dps) {
 	case DPS_LV:
-		debounce_init(&dps_mon_lv, 15, 0);
+		debounce_t_init(&dps_mon_lv, 100, 0); //max 100mS, no error
 		bsp_gpio_set(LV_EN, enable);
 		break;
 	case DPS_HS:
-		debounce_init(&dps_mon_hs, 15, 0);
+		debounce_t_init(&dps_mon_hs, 100, 0);
 		bsp_gpio_set(HS_EN, enable);
 		break;
 	case DPS_VS:
+		#if DPS_HV_V4 == 1
+		bsp_gpio_set(VS_UP, enable);
+		bsp_gpio_set(VS_DN, !enable);
+		#else
 		bsp_gpio_set(VS_EN, enable);
+		#endif
 		break;
 	case DPS_HV:
-		debounce_init(&dps_mon_hv, 15, 0);
+		debounce_t_init(&dps_mon_hv, 1000, 0);
 		bsp_gpio_set(HV_EN, enable);
 		break;
 	case DPS_IS:
@@ -432,6 +544,7 @@ int dps_gain(int dps, int gain, int execute)
 		}
 		break;
 	case DPS_HV:
+#ifndef DPS_HV_V4
 		if(execute) {
 			dps_hv_g = gain;
 			bsp_gpio_set(HV_VS, gain & 0x01);
@@ -445,6 +558,7 @@ int dps_gain(int dps, int gain, int execute)
 				dps_hv_g = gain;
 			}
 		}
+#endif
 		break;
 	case DPS_VS:
 	case DPS_LV:
@@ -603,11 +717,20 @@ static int cmd_power_func(int argc, char *argv[])
 				else if(!strcmp(argv[2], "UPDN")) {
 					ecode = dps_hv_start();
 					if(!ecode) {
+						vm_wait(10);
 						ecode = dps_hv_stop();
 						if(!ecode) {
 							ecode = dps_hv_start();
 							if(!ecode) {
+								vm_wait(10);
 								ecode = dps_hv_stop();
+								if(!ecode) {
+									ecode = dps_hv_start();
+									if(!ecode) {
+										vm_wait(10);
+										ecode = dps_hv_stop();
+									}
+								}
 							}
 						}
 					}
